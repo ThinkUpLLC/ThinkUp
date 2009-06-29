97d556d18fab94fe613ad54587267ef891bfbf20
ginatrapani <ginatrapani@gmail.com>
2009-06-28 17:28:08 -0700

    Prepping for OAuth support

--- common/config.inc.php
+++ common/config.inc.php
@@ -2,15 +2,20 @@
 
 /** APPLICATION CONFIG **/
 $TWITALYTIC_CFG['debug'] = true;
-
 $TWITALYTIC_CFG['timeZone'] = 'TZ=US/Pacific';
-$TWITALYTIC_CFG['bitly_api_key'] = 'yourkey';
-$TWITALYTIC_CFG['bitly_login'] = 'yourbitlylogin';
+
+// Get the following by registering your instance with Twitter at http://twitter.com/oauth_clients/
+$TWITALYTIC_CFG['oauth_consumer_key'] = 'yourconsumerkey';
+$TWITALYTIC_CFG['oauth_consumer_secret'] = 'yourconsumersecret';
 
 $TWITALYTIC_CFG['app_title'] = 'Twitalytic';
 $TWITALYTIC_CFG['log_location'] = '/Users/gina/Sites/twitalytic/automated/logs/';
 $TWITALYTIC_CFG['site_root_path'] = '/';
 $TWITALYTIC_CFG['smarty_path'] = '/usr/local/php5/lib/php/smarty/libs/';
+
+// Get the following here: http://bit.ly
+$TWITALYTIC_CFG['bitly_api_key'] = 'yourkey';
+$TWITALYTIC_CFG['bitly_login'] = 'yourbitlylogin';
 
 /** DATABASE CONFIG **/
 $TWITALYTIC_CFG['db_host'] = "localhost";
