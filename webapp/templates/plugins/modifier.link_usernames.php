c6a9592c44ee6ffd76dd6470c2a883712c91762e
Gina Trapani <gina@ginatrapani.org>
2009-07-17 12:50:58 -0700

    Bugfix: Correct @username links when followed by colon, enclosed in parens, or have a 's after them

--- webapp/templates/plugins/modifier.link_usernames.php
+++ webapp/templates/plugins/modifier.link_usernames.php
@@ -20,13 +20,18 @@
  * @return string
  */
 function smarty_modifier_link_usernames($text) {
-	//TODO: Find a more elegant (perhaps regex-based) way to do this
+	//TODO: Find a more elegant way to do this that's totally regex-based, not loving this explod/implode approach
 	global $cfg, $i; //icky but necessary
 	$words = explode(" ", $text);
+	$pattern = '/^@[a-zA-Z0-9_]+/';
 	for($k = 0; $k < count($words); $k++) {
 		if ( substr($words[$k], 0, 1) == '@' ) {
-			$words[$k] = '<a href="'.$cfg->site_root_path.'user/?u='.substr($words[$k],1).'&i='.$i->twitter_username.'">'.$words[$k].'</a>';
-		} 
+			preg_match($pattern, $words[$k], $matches);
+			$words[$k] = '<a href="'.$cfg->site_root_path.'user/?u='.substr($matches[0],1).'&i='.$i->twitter_username.'">'.$words[$k].'</a>';
+		} else if ( substr($words[$k], 0, 2) == '(@' ) { //for usersnames in parentheses
+			preg_match($pattern, substr($words[$k], 1, strlen($words[$k])), $matches);
+			$words[$k] = '<a href="'.$cfg->site_root_path.'user/?u='.substr($matches[0],1).'&i='.$i->twitter_username.'">'.$words[$k].'</a>';
+		}
 	}
 	return implode($words, ' ');
 }
