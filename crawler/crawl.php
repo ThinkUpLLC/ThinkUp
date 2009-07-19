2f995ca76d3b3689e617524dcfad14d2b9491425
Gina Trapani <gina@ginatrapani.org>
2009-07-18 20:54:17 -0700

    Crawler now runs in a continuous loop; no more cron

--- crawler/crawl.php
+++ crawler/crawl.php
@@ -1,51 +1,53 @@
-<?php   # invoke this hourly in cron
+<?php   
 require_once('config.crawler.inc.php');
 ini_set("include_path", ini_get("include_path").":".$INCLUDE_PATH);
 require_once("init.php");
 
-// Instantiate and initialize needed objects
-$db = new Database();
-$conn = $db->getConnection();
-$id = new InstanceDAO();
-$oid = new OwnerInstanceDAO();
+while (true) {
+	// Instantiate and initialize needed objects
+	$db = new Database();
+	$conn = $db->getConnection();
+	$id = new InstanceDAO();
+	$oid = new OwnerInstanceDAO();
 
-$instances = $id->getAllInstancesStalestFirst();
-foreach ($instances as $i) {
-	$crawler = new Crawler($i);
-	$cfg = new Config($i->twitter_username, $i->twitter_user_id);
-	$logger = new Logger($i->twitter_username);
-	$tokens = $oid->getOAuthTokens($i->id);
-	$api = new CrawlerTwitterAPIAccessorOAuth($tokens['oauth_access_token'], $tokens['oauth_access_token_secret'], $cfg, $i);
-	$api -> init($logger);
+	$instances = $id->getAllInstancesStalestFirst();
+	foreach ($instances as $i) {
+		$crawler = new Crawler($i);
+		$cfg = new Config($i->twitter_username, $i->twitter_user_id);
+		$logger = new Logger($i->twitter_username);
+		$tokens = $oid->getOAuthTokens($i->id);
+		$api = new CrawlerTwitterAPIAccessorOAuth($tokens['oauth_access_token'], $tokens['oauth_access_token_secret'], $cfg, $i);
+		$api -> init($logger);
 
-	if ( $api->available_api_calls_for_crawler > 0 ) {
+		if ( $api->available_api_calls_for_crawler > 0 ) {
 	
-		$crawler->fetchInstanceUserInfo($cfg, $api, $logger);
+			$crawler->fetchInstanceUserInfo($cfg, $api, $logger);
 
-		$crawler->fetchInstanceUserTweets($cfg, $api, $logger);
+			$crawler->fetchInstanceUserTweets($cfg, $api, $logger);
 	
-		$crawler->fetchInstanceUserReplies($cfg, $api, $logger);
+			$crawler->fetchInstanceUserReplies($cfg, $api, $logger);
 
-		$crawler->fetchInstanceUserFriends($cfg, $api, $logger);
+			$crawler->fetchInstanceUserFriends($cfg, $api, $logger);
 
-		$crawler->fetchInstanceUserFollowers($cfg, $api, $logger);
+			$crawler->fetchInstanceUserFollowers($cfg, $api, $logger);
 
-		$crawler->fetchStrayRepliedToTweets($cfg, $api, $logger);
+			$crawler->fetchStrayRepliedToTweets($cfg, $api, $logger);
 
-		$crawler->fetchUnloadedFollowerDetails($cfg, $api, $logger);
+			$crawler->fetchUnloadedFollowerDetails($cfg, $api, $logger);
 
-		$crawler->fetchFriendTweetsAndFriends($cfg, $api, $logger);
+			$crawler->fetchFriendTweetsAndFriends($cfg, $api, $logger);
 
-		// TODO: Get direct messages
-		// TODO: Gather favorites data
+			// TODO: Get direct messages
+			// TODO: Gather favorites data
 
-		$crawler->cleanUpFollows($cfg, $api, $logger);
+			$crawler->cleanUpFollows($cfg, $api, $logger);
 		
-		// Save instance
-		$id->save($crawler->instance,  $crawler->owner_object->tweet_count, $logger, $api);
-	} 
-	$logger->close();			# Close logging
-}
+			// Save instance
+			$id->save($crawler->instance,  $crawler->owner_object->tweet_count, $logger, $api);
+		} 
+		$logger->close();			# Close logging
+	}
 
-if ( isset($conn) ) $db->closeConnection($conn); // Clean up
+	if ( isset($conn) ) $db->closeConnection($conn); // Clean up
+}
 ?>
