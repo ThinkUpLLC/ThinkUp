c18fa80423906d8482a4bd4aa0e1176cae9b5a10
Gina Trapani <ginatrapani@gmail.com>
2010-02-15 15:15:47 -0800

    [OPTIONAL DB MIGRATION] Change your table prefix from the former default, ta_, to current default, tt_

---
+++ db_migrations/2010-02-14-update-table-prefixes.sql
@@ -0,0 +1,9 @@
+RENAME TABLE ta_follows TO tt_follows;
+RENAME TABLE ta_tweets TO tt_tweets;
+RENAME TABLE ta_users TO tt_users;
+RENAME TABLE ta_instances TO tt_instances;
+RENAME TABLE ta_owners TO tt_owners;
+RENAME TABLE ta_owner_instances TO tt_owner_instances;
+RENAME TABLE ta_tweet_errors TO tt_tweet_errors;
+RENAME TABLE ta_user_errors TO tt_user_errors;
+RENAME TABLE ta_links TO tt_links;
