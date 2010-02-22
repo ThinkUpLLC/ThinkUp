06922c09864358b58fbbc418dc18705e674b56a3
Gina Trapani <ginatrapani@gmail.com>
2010-02-22 14:48:47 -0800

    Fixed logout bug with new Session object

--- webapp/session/logout.php
+++ webapp/session/logout.php
@@ -1,4 +1,11 @@
 <?php
+
+// set up
+chdir("..");
+require_once ('config.webapp.inc.php');
+ini_set("include_path", ini_get("include_path").PATH_SEPARATOR.$INCLUDE_PATH);
+require_once ("init.php");
+
 session_start(); 
 $session = new Session();
 $session->logout();
