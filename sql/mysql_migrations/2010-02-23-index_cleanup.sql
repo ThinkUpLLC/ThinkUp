62b3abc16c6a21bb9cce362fd018eb21568fbbfe
Gina Trapani <ginatrapani@gmail.com>
2010-02-22 18:06:01 -0800

    [DB MIGRATION REQ'D] Fixed index alteration migration

--- sql/mysql_migrations/2010-02-23-index_cleanup.sql
+++ sql/mysql_migrations/2010-02-23-index_cleanup.sql
@@ -1,3 +1,4 @@
-ALTER TABLE `tt_follows` DROP KEY `follower_id_user_id` (`follower_id`,`user_id`);
+ALTER TABLE `tt_follows` DROP INDEX `follower_id_user_id`;
 ALTER TABLE `tt_tweets` ADD KEY `in_reply_to_user_id` (`in_reply_to_user_id`);
+
 
