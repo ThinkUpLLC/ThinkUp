176a334f90cf378c057bb189c474877a0057aba5
Gina Trapani <ginatrapani@gmail.com>
2010-02-23 00:34:31 -0800

    Removed session state from public timeline for more efficient caching

--- webapp/public.php
+++ webapp/public.php
@@ -3,7 +3,7 @@
 require_once ('config.webapp.inc.php');
 ini_set("include_path", ini_get("include_path").PATH_SEPARATOR.$INCLUDE_PATH);
 require_once ("init.php");
-session_start();
+
 $db = new Database($THINKTANK_CFG);
 $conn = $db->getConnection();
 $cfg = new Config();
