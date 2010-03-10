b1b2045eae5d78f8e44a2f07dfdc7e6ea57d82be
Gina Trapani <ginatrapani@gmail.com>
2010-03-10 01:00:16 -0800

    Missing semi-colons

--- sql/mysql_migrations/2010-03-10-more_pluggability_alterations.sql
+++ sql/mysql_migrations/2010-03-10-more_pluggability_alterations.sql
@@ -1,16 +1,16 @@
-RENAME TABLE `tt_tweets` TO `tt_posts` 
-ALTER TABLE  `tt_posts` CHANGE  `status_id`  `post_id` BIGINT( 11 ) NOT NULL
-ALTER TABLE  `tt_posts` CHANGE  `tweet_text`  `post_text` VARCHAR( 160 ) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL
-ALTER TABLE  `tt_posts` CHANGE  `tweet_html`  `post_html` VARCHAR( 255 ) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL
-ALTER TABLE  `tt_posts` CHANGE  `in_reply_to_status_id`  `in_reply_to_post_id` BIGINT( 11 ) NULL DEFAULT NULL
-ALTER TABLE  `tt_posts` CHANGE  `in_retweet_of_status_id`  `in_retweet_of_post_id` BIGINT( 11 ) NULL DEFAULT NULL
+RENAME TABLE `tt_tweets` TO `tt_posts`; 
+ALTER TABLE  `tt_posts` CHANGE  `status_id`  `post_id` BIGINT( 11 ) NOT NULL;
+ALTER TABLE  `tt_posts` CHANGE  `tweet_text`  `post_text` VARCHAR( 160 ) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL;
+ALTER TABLE  `tt_posts` CHANGE  `tweet_html`  `post_html` VARCHAR( 255 ) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL;
+ALTER TABLE  `tt_posts` CHANGE  `in_reply_to_status_id`  `in_reply_to_post_id` BIGINT( 11 ) NULL DEFAULT NULL;
+ALTER TABLE  `tt_posts` CHANGE  `in_retweet_of_status_id`  `in_retweet_of_post_id` BIGINT( 11 ) NULL DEFAULT NULL;
 ALTER TABLE  `tt_posts` DROP `post_html`;
 
 
-ALTER TABLE  `tt_links` CHANGE  `status_id`  `post_id` BIGINT( 11 ) NULL DEFAULT NULL
+ALTER TABLE  `tt_links` CHANGE  `status_id`  `post_id` BIGINT( 11 ) NULL DEFAULT NULL;
 
-RENAME TABLE `tt_tweet_errors` TO `tt_post_errors` 
-ALTER TABLE  `tt_post_errors` CHANGE  `status_id`  `post_id` BIGINT( 20 ) NOT NULL
+RENAME TABLE `tt_tweet_errors` TO `tt_post_errors`; 
+ALTER TABLE  `tt_post_errors` CHANGE  `status_id`  `post_id` BIGINT( 20 ) NOT NULL;
 
 CREATE TABLE IF NOT EXISTS `tt_replies` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
