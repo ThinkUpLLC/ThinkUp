a415c15977670b07484dcc2b9592f8d9c8711a83
Dash30 <customerservice@dash30.com>
2010-03-14 08:46:21 -0400

    modified logout to bounce user to public timeline rather than login prompt.

--- webapp/session/logout.php
+++ webapp/session/logout.php
@@ -9,5 +9,6 @@ require_once ("init.php");
 session_start(); 
 $session = new Session();
 $session->logout();
-header("Location: login.php?smsg=You+have+successfully+logged+out.");
+//header("Location: login.php?smsg=You+have+successfully+logged+out.");
+header("Location: ../index.php?smsg=You+have+successfully+logged+out.")
 ?> 
