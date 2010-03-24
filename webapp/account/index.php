7a8d6161539334492589da5a078dd6af5d2c7d44
Gina Trapani <ginatrapani@gmail.com>
2010-03-24 00:10:30 -0700

    Added Facebook plugin options to config; Fixed account page default Twitter plugin account listing

--- webapp/account/index.php
+++ webapp/account/index.php
@@ -1,4 +1,4 @@
-<?php
+<?php 
 // set up
 chdir("..");
 require_once ('config.webapp.inc.php');
@@ -8,27 +8,27 @@ require_once ("init.php");
 session_start();
 $session = new Session();
 if (!$session->isLoggedIn()) {
-	header("Location: ../index.php");
+    header("Location: ../index.php");
 }
 
 $od = new OwnerDAO($db);
 
 
 if (isset($_POST['changepass']) && $_POST['changepass'] == 'Change Password') {
-	$originalpass = $od->getPass($_SESSION['user']);
-	$origpass = $originalpass['pwd'];
-	if (!$session->pwdCheck($_POST['oldpass'], $origpass)) {
-		$errormsg = "Old password does not match or empty.";
-	} elseif ($_POST['pass1'] != $_POST['pass2']) {
-		$errormsg = "New passwords did not match. Your password has not been changed.";
-	} elseif (strlen($_POST['pass1']) < 5) {
-		$errormsg = "New password must be at least 5 characters. Your password has not been changed.";
-	} else {
-		$cryptpass = $session->pwdcrypt($_POST['pass1']);
-		$od->updatePassword($_SESSION['user'], $cryptpass);
-		$successmsg = "Your password has been updated.";
-
-	}
+    $originalpass = $od->getPass($_SESSION['user']);
+    $origpass = $originalpass['pwd'];
+    if (!$session->pwdCheck($_POST['oldpass'], $origpass)) {
+        $errormsg = "Old password does not match or empty.";
+    } elseif ($_POST['pass1'] != $_POST['pass2']) {
+        $errormsg = "New passwords did not match. Your password has not been changed.";
+    } elseif (strlen($_POST['pass1']) < 5) {
+        $errormsg = "New password must be at least 5 characters. Your password has not been changed.";
+    } else {
+        $cryptpass = $session->pwdcrypt($_POST['pass1']);
+        $od->updatePassword($_SESSION['user'], $cryptpass);
+        $successmsg = "Your password has been updated.";
+        
+    }
 }
 
 $s = new SmartyThinkTank();
@@ -44,25 +44,27 @@ $owner = $od->getByEmail($_SESSION['user']);
 $s->assign('cfg', $cfg);
 $s->assign('owner', $owner);
 
-if ( $owner->is_admin ) {
-	$owners = $od->getAllOwners();
-	foreach ($owners as $o) {
-		$instances = $id->getByOwner($o, true);
-		$o->setInstances($instances);
-	}
-	$s->assign('owners', $owners);
+if ($owner->is_admin) {
+    $owners = $od->getAllOwners();
+    foreach ($owners as $o) {
+        $instances = $id->getByOwner($o, true);
+        $o->setInstances($instances);
+    }
+    $s->assign('owners', $owners);
 }
 
 $cmi = $webapp->getConfigMenu();
-$s-> assign('config_menu', $cmi);
+$s->assign('config_menu', $cmi);
 if (isset($_GET['p'])) {
-	$webapp->configuration($_GET['p']);
-	array_push( $s->template_dir, 'plugins/'.$_GET['p']);
-	$s->assign('body', $THINKTANK_CFG['source_root_path'].'common//plugins//'.$_GET['p'].'//templates/'.$_GET['p'].'.account.index.tpl');
-} else  {
-	//default to the first plugin on the stack
-	$s->assign('body', $THINKTANK_CFG['source_root_path'].'common//plugins//'.$cmi[0][0].'//templates/'.$cmi[0][0].'.account.index.tpl');
+    $active_plugin = $_GET['p'];
+} else {
+    //default to the first plugin on the stack
+    $active_plugin = $cmi[0][0];
 }
+
+$webapp->configuration($active_plugin);
+array_push($s->template_dir, 'plugins/'.$active_plugin);
+$s->assign('body', $THINKTANK_CFG['source_root_path'].'common//plugins//'.$active_plugin.'//templates/'.$active_plugin.'.account.index.tpl');
 
 /* End plugin-specific configuration handling */
 
@@ -72,10 +74,10 @@ $db->closeConnection($conn);
 
 
 if (isset($errormsg)) {
-	$s->assign('errormsg', $errormsg);
+    $s->assign('errormsg', $errormsg);
 }
 if (isset($successmsg)) {
-	$s->assign('successmsg', $successmsg);
+    $s->assign('successmsg', $successmsg);
 }
 
 $s->display('account.index.tpl');
