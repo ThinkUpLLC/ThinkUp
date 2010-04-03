ccd5a62be67b052f238b98f62c818cefa95880cd
Gina Trapani <ginatrapani@gmail.com>
2010-04-03 12:14:26 -0700

    Bugfix: check for the existence of  first

--- webapp/crawler/crawl.php
+++ webapp/crawler/crawl.php
@@ -4,7 +4,7 @@ require_once ("common/init.php");
 
 $authorized = false;
 
-if ($argc > 1) { // check for CLI credentials
+if (isset($argc) && $argc > 1) { // check for CLI credentials
     $session = new Session();
     $username = $argv[1];
     $pw = $argv[2];
