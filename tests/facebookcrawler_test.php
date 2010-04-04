74b1318da2fcd144d919d8afeb8065f182e90266
Gina Trapani <ginatrapani@gmail.com>
2010-04-03 17:09:24 -0700

    Fixed bad include paths in registration and forgot password pages; Added tests for those pages; Standardized Login/Sign In/Log In on Log In everywhere; Code formatting

--- tests/facebookcrawler_test.php
+++ tests/facebookcrawler_test.php
@@ -4,7 +4,7 @@ require_once (dirname(__FILE__).'/simpletest/web_tester.php');
 
 require_once (dirname(__FILE__).'/config.tests.inc.php');
 
-
+ini_set("include_path", ini_get("include_path").PATH_SEPARATOR.$INCLUDE_PATH);
 require_once ("classes/class.ThinkTankTestCase.php");
 require_once ("common/class.User.php");
 require_once ("common/class.Instance.php");
