52b9e548f1a47be4d0a233d62189c614df43ca91
Russell Smith <russell.smith@ukd1.co.uk>
2010-04-12 21:08:22 +0100

    Fix for warning being echo'd

--- webapp/common/class.Mailer.php
+++ webapp/common/class.Mailer.php
@@ -2,7 +2,7 @@
 class Mailer {
 
     public static function mail($to, $subject, $message) {
-        $mailheader = "From: \"Auto-Response\" <notifications@$host>\r\n";
+        $mailheader = "From: \"Auto-Response\" <notifications@host>\r\n";
         $mailheader .= "X-Mailer: PHP/".phpversion();
         mail($to, $subject, $message, $mailheader);
     }
