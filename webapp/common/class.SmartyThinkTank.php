88d84b2834658a0302263acb9614ee74d0b51ce7
Gina Trapani <ginatrapani@gmail.com>
2010-05-10 18:35:04 -0700

    Webapp directory cleanup * Consolidated templates, compilation, and cache directories into one folder * Renamed it from templates to view (for Model-View-Controller)

--- webapp/common/class.SmartyThinkTank.php
+++ webapp/common/class.SmartyThinkTank.php
@@ -4,10 +4,10 @@ class SmartyThinkTank extends Smarty {
     function SmartyThinkTank() {
         global $THINKTANK_CFG;
         $this->Smarty();
-        $this->template_dir = array('templates');
-        $this->compile_dir = 'templates_c/';
-        $this->plugins_dir = array('plugins', 'templates/plugins/');
-        $this->cache_dir = 'templates_c/cache';
+        $this->template_dir = array('view');
+        $this->compile_dir = 'view/compiled_view/';
+        $this->plugins_dir = array('plugins', 'view/plugins/');
+        $this->cache_dir = 'view/compiled_view/cache';
         $this->cache_lifetime = 300;
         $this->caching = $THINKTANK_CFG['cache_pages'];
         $this->assign('app_name', $THINKTANK_CFG['app_title']);
