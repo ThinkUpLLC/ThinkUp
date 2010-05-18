30b0c762feb8c1afb1cc79f5638d6c7ec4435310
mwilkie <mark@bitterpill.org>
2010-05-17 20:33:17 -0400

    logger singleton, ie: Logger::getInstance()

--- tests/TestOfLogging.php
+++ tests/TestOfLogging.php
@@ -1,4 +1,4 @@
-<?php
+<?php 
 require_once dirname(__FILE__).'/config.tests.inc.php';
 require_once $SOURCE_ROOT_PATH.'extlib/simpletest/autorun.php';
 require_once $SOURCE_ROOT_PATH.'extlib/simpletest/web_tester.php';
@@ -13,28 +13,50 @@ class TestOfLogging extends UnitTestCase {
     function TestOfLogging() {
         $this->UnitTestCase('Log class test');
     }
-
+    
     function setUp() {
+        global $THINKTANK_CFG;
+        // delete our log file if it exists
+        if (file_exists($THINKTANK_CFG['log_location'])) {
+            unlink($THINKTANK_CFG['log_location']);
+        }
     }
-
+    
     function tearDown() {
+        global $THINKTANK_CFG;
+        // delete our log file if it exists
+        if (file_exists($THINKTANK_CFG['log_location'])) {
+            unlink($THINKTANK_CFG['log_location']);
+        }
     }
-
+    
     function testCreatingNewLogger() {
         global $THINKTANK_CFG;
         $logger = new Logger($THINKTANK_CFG['log_location']);
         $logger->logStatus('Should write this to the log', get_class($this));
         $this->assertTrue(file_exists($THINKTANK_CFG['log_location']), 'File created');
-
+        
         $messages = file($THINKTANK_CFG['log_location']);
         $this->assertWantedPattern('/Should write this to the log/', $messages[sizeof($messages) - 1]);
-
+        
         $logger->setUsername('ginatrapani');
         $logger->logStatus('Should write this to the log with a username', get_class($this));
         $this->assertWantedPattern('/ginatrapani | TestOfLogging:Should write this to the log/', $messages[sizeof($messages) - 1]);
-
+        
         $logger->close();
-
+    }
+    
+    function testNewLoggerSingleton() {
+        global $THINKTANK_CFG;
+        $logger = Logger::getInstance();
+        $logger->logStatus('Singleton logger should write this to the log', get_class($this));
+        $this->assertTrue(file_exists($THINKTANK_CFG['log_location']), 'File created');
+        $messages = file($THINKTANK_CFG['log_location']);
+        $this->assertWantedPattern('/Singleton logger should write this to the log/', $messages[sizeof($messages) - 1]);
+        $logger->setUsername('single-ton');
+        $logger->logStatus('Should write this to the log with a username', get_class($this));
+        $this->assertWantedPattern('/single-ton | TestOfLogging:Singleton logger should write this to the log/', $messages[sizeof($messages) - 1]);
+        $logger->close();
     }
 }
 ?>
--- webapp/model/class.Logger.php
+++ webapp/model/class.Logger.php
@@ -1,32 +1,43 @@
-<?php
+<?php 
 class Logger {
+    // our singleton instance
+    private static $instance;
     var $log;
-    var $twitter_username;
-
+    var $network_username;
+    
     function __construct($location) {
         $this->log = $this->openFile($location, 'a'); # Append to any prior file
     }
-
-    function setUsername($uname) {
-        $this->twitter_username = $uname;
+    
+    // The singleton method
+    public static function getInstance() {
+        global $THINKTANK_CFG;
+        if (!isset(self::$instance)) {
+            self::$instance = new Logger($THINKTANK_CFG['log_location']);
+        }
+        return self::$instance;
     }
-
+    
+    function setUsername($uname) {
+        $this->network_username = $uname;
+    }
+    
     function logStatus($status_message, $classname) {
-        $status_signature = date("Y-m-d H:i:s", time())." | ".(string) number_format(round(memory_get_usage() / 1024000, 2), 2)." MB | $this->twitter_username | $classname:";
+        $status_signature = date("Y-m-d H:i:s", time())." | ".(string) number_format(round(memory_get_usage() / 1024000, 2), 2)." MB | $this->network_username | $classname:";
         if (strlen($status_message) > 0) {
             $this->writeFile($this->log, $status_signature.$status_message); # Write status to log
         }
     }
-
+    
     private function addBreaks() {
         $this->writeFile($this->log, ""); # Add a little whitespace
     }
-
+    
     function close() {
         $this->addBreaks();
         $this->closeFile($this->log);
     }
-
+    
     function openFile($filename, $type) {
         if (array_search($type, array('w', 'a')) < 0) {
             $type = 'w';
@@ -34,20 +45,17 @@ class Logger {
         $filehandle = fopen($filename, $type);// or die("can't open file $filename");
         return $filehandle;
     }
-
+    
     function writeFile($filehandle, $message) {
         return fwrite($filehandle, $message."\n");
     }
-
+    
     function closeFile($filehandle) {
         return fclose($filehandle);
     }
-
+    
     function deleteFile($filename) {
         return unlink($filename);
     }
-
-
 }
-
 ?>
