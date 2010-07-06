d48b970f29f900eae3a4e27ed11890755c3abccf
Gina Trapani <ginatrapani@gmail.com>
2010-07-06 16:35:28 -0700

    Issue #132: Bugfix for ExpandURLs crawl hang

--- webapp/plugins/expandurls/model/class.ExpandURLsPlugin.php
+++ webapp/plugins/expandurls/model/class.ExpandURLsPlugin.php
@@ -1,5 +1,14 @@
 <?php
+/**
+ * ExpandURLs Crawler Plugin
+ *
+ * @author Gina Trapani <ginatrapani[at]gmail[dot]com>
+ *
+ */
 class ExpandURLsPlugin implements CrawlerPlugin {
+    /**
+     * Run when the crawler does
+     */
     public function crawl() {
         $logger = Logger::getInstance();
         $ldao = DAOFactory::getDAO('LinkDAO');
@@ -20,12 +29,17 @@ class ExpandURLsPlugin implements CrawlerPlugin {
 
     public function renderConfiguration($owner) {
         //TODO: Write controller class, echo its results
-
+        //Set the number of links to expand per run in the options panel
     }
 
-    //Thanks to Probably Programming
-    //http://probablyprogramming.com/2009/04/11/untiny-that-url/
-    function untinyurl($tinyurl, $ldao) {
+    /**
+     * Expand a given short URL
+     * Thanks to Probably Programming
+     * http://probablyprogramming.com/2009/04/11/untiny-that-url/
+     * @param str $tinyurl Shortened URL
+     * @param LinkDao $ldao
+     */
+    private function untinyurl($tinyurl, $ldao) {
         $logger = Logger::getInstance();
         $url = parse_url($tinyurl);
         $host = $url['host'];
@@ -47,10 +61,14 @@ class ExpandURLsPlugin implements CrawlerPlugin {
             $request = "HEAD {$url} HTTP/1.0\r\nHost: {$host}\r\nConnection: Close\r\n\r\n";
 
             fwrite($sock, $request);
+
             $response = '';
-            while (!feof($sock)) {
+            $start = null;
+            $timeout = 5000; //seconds
+            while(!self::safe_feof($sock, $start) && (microtime(true) - $start) < $timeout) {
                 $response .= fgets($sock, 128);
             }
+
             $lines = explode("\r\n", $response);
             foreach ($lines as $line) {
                 if (strpos(strtolower($line), 'location:') === 0) {
@@ -61,5 +79,16 @@ class ExpandURLsPlugin implements CrawlerPlugin {
             return $tinyurl;
         }
     }
+
+    /**
+     * Safe wrapper for the feof function that implements a timeout.
+     * See Example #1:
+     * http://php.net/manual/en/function.feof.php
+     * @param socket $fp Open socket
+     * @param mixed $start Int or null
+     */
+    private function safe_feof($fp, &$start = null) {
+        $start = microtime(true);
+        return feof($fp);
+    }
 }
-?>
