01bf6d780c64dc31dd376a68fb25488e54c10a7c
Gina Trapani <ginatrapani@gmail.com>
2010-07-14 00:25:55 -0700

    Bugfix: Show non-logged-in user requesting private post page, public post page

--- tests/TestOfPostController.php
+++ tests/TestOfPostController.php
@@ -7,12 +7,16 @@ require_once $SOURCE_ROOT_PATH.'tests/classes/class.ThinkTankUnitTestCase.php';
 require_once $SOURCE_ROOT_PATH.'webapp/controller/interface.Controller.php';
 require_once $SOURCE_ROOT_PATH.'webapp/controller/class.ThinkTankController.php';
 require_once $SOURCE_ROOT_PATH.'webapp/controller/class.ThinkTankAuthController.php';
+require_once $SOURCE_ROOT_PATH.'webapp/controller/class.PublicTimelineController.php';
 require_once $SOURCE_ROOT_PATH.'webapp/controller/class.PostController.php';
 require_once $SOURCE_ROOT_PATH.'extlib/Smarty-2.6.26/libs/Smarty.class.php';
 require_once $SOURCE_ROOT_PATH.'webapp/model/class.SmartyThinkTank.php';
 require_once $SOURCE_ROOT_PATH.'webapp/model/class.Post.php';
+require_once $SOURCE_ROOT_PATH.'webapp/model/class.Instance.php';
 require_once $SOURCE_ROOT_PATH.'webapp/model/class.Link.php';
 require_once $SOURCE_ROOT_PATH.'webapp/model/class.DAOFactory.php';
+require_once $SOURCE_ROOT_PATH.'webapp/model/class.Profiler.php';
+require_once $SOURCE_ROOT_PATH.'webapp/model/class.Session.php';
 try {
     $db = new Database($THINKTANK_CFG);
     $conn = $db->getConnection();
@@ -31,16 +35,16 @@ class TestOfPostController extends ThinkTankUnitTestCase {
     /**
      * Constructor
      */
-    function __construct() {
+    public function __construct() {
         $this->UnitTestCase('PostController class test');
     }
 
     /**
      * Add test post to database
      */
-    function setUp(){
+    public function setUp(){
         parent::setUp();
-        $q = "INSERT INTO tt_posts (post_id, author_user_id, author_username, author_fullname, author_avatar, 
+        $q = "INSERT INTO tt_posts (post_id, author_user_id, author_username, author_fullname, author_avatar,
         post_text, source, pub_date, reply_count_cache, retweet_count_cache) VALUES (1001, 13, 'ev', 'Ev Williams', 
         'avatar.jpg', 'This is a test post', 'web', '2006-01-01 00:05:00', ".rand(0, 4).", 5);";
         $this->db->exec($q);
@@ -49,7 +53,7 @@ class TestOfPostController extends ThinkTankUnitTestCase {
     /**
      * Test constructor
      */
-    function testConstructor() {
+    public function testConstructor() {
         $controller = new PostController(true);
         $this->assertTrue(isset($controller), 'constructor test');
     }
@@ -57,17 +61,17 @@ class TestOfPostController extends ThinkTankUnitTestCase {
     /**
      * Test controller when user is not logged in
      */
-    function testControlNotLoggedIn() {
+    public function testControlNotLoggedIn() {
         $controller = new PostController(true);
         $results = $controller->go();
 
-        $this->assertEqual('You must be logged in to do this', $results);
+        $this->assertTrue(strpos($results, 'Public Timeline') > 0);
     }
 
     /**
      * Test controller when user is logged in, but there's no Post ID on the query string
      */
-    function testControlLoggedInNoPostID() {
+    public function testControlLoggedInNoPostID() {
         $_SESSION['user'] = 'me@example.com';
 
         $controller = new PostController(true);
@@ -79,7 +83,7 @@ class TestOfPostController extends ThinkTankUnitTestCase {
     /**
      * Test controller when user is logged in and there is a valid Post ID on the query string
      */
-    function testControlLoggedInWithPostID() {
+    public function testControlLoggedInWithPostID() {
         $_SESSION['user'] = 'me@example.com';
         $_GET["t"] = '1001';
 
@@ -92,7 +96,7 @@ class TestOfPostController extends ThinkTankUnitTestCase {
     /**
      * Test controller when logged in but there's a numeric but nonexistent Post ID
      */
-    function testControlLoggedInWithNumericButNonExistentPostID(){
+    public function testControlLoggedInWithNumericButNonExistentPostID(){
         $_SESSION['user'] = 'me@example.com';
         $_GET["t"] = '11';
 
@@ -105,7 +109,7 @@ class TestOfPostController extends ThinkTankUnitTestCase {
     /**
      * Test controller when logged in but a non-numeric post ID
      */
-    function testControlLoggedInWithNonNumericPostID(){
+    public function testControlLoggedInWithNonNumericPostID(){
         $_SESSION['user'] = 'me@example.com';
         $_GET["t"] = 'notapostID45';
 
