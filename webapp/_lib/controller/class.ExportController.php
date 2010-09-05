dac031704a1fc582576e7bf7a3324725e64c4b8f
Michael Louis Thaler <michael.louis.thaler@gmail.com>
2010-09-05 17:38:09 -0400

    Tests that should work
    For some reason I can't get the new FixtureBuilder to work...

--- webapp/_lib/controller/class.ExportController.php
+++ webapp/_lib/controller/class.ExportController.php
@@ -44,7 +44,9 @@ class ExportController extends ThinkUpAuthController {
                     $this->addErrorMessage('Insufficient privileges');
                     return $this->generateView();
                 } else {
-                    switch ($_GET['type']) {
+                    $type = isset($_GET['type']) ? $_GET['type'] : 'posts';
+
+                    switch ($type) {
                     case 'replies':
                         $this->exportReplies();
                     break;
@@ -68,7 +70,6 @@ class ExportController extends ThinkUpAuthController {
     protected function exportAllPosts() {
         $pd = DAOFactory::getDAO('PostDAO');
         $posts_it = $pd->getAllPostsByUsernameIterator($_GET['u'], $_GET['n']);
-        // get object var names
         $vars = array_keys(get_class_vars('Post'));
 
         self::outputCSV($posts_it, $vars);
@@ -79,16 +80,27 @@ class ExportController extends ThinkUpAuthController {
         $replies = $pd->getRepliesToPostIterator($_GET['post_id'], $_GET['n']);
         $heading = array_keys(get_class_vars('Post'));
 
-        $data = array();
-        foreach ($replies as $reply) {
-            $data[] = get_object_vars($reply);
-        }
-
-        self::outputCSV($data, $heading);
+        self::outputCSV($replies, $heading);
     }
 
+    /**
+     * Sends an associative array to the browser as a .csv spreadsheet.
+     *
+     * Flushes the output buffer on each line, to avoid clogging it with memory. An unfortunate side effect of this is
+     * it means we can't count up the total size of the spreadsheet and put it in the HTTP headers (this would allow
+     * browsers to display a progress bar as the spreadsheet is downloaded). The only way we'd be able to work around
+     * this would be writing the data to a temp file first and then sending it to the user at the end; allowing this
+     * as an optional paramater would be a possible future enhancement.
+     *
+     * @param array $data An associative array of data.
+     * @param array $column_labels The first line of the CSV, by convention interpreted as column labels.
+     */
     public static function outputCSV($data, $column_labels) {
-        ob_end_clean();
+        // check for contents before clearing the buffer to silence PHP notices
+        if (ob_get_contents()) {
+            ob_end_clean();
+        }
+
         if( ! headers_sent() ) { // this is so our test don't barf on us
             header('Content-Type: text/csv');
             header('Content-Disposition: attachment; filename="export.csv"');
