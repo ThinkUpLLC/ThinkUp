5e73045777d920ec54657180393a15c6143a4828
Gina Trapani <ginatrapani@gmail.com>
2010-09-07 11:28:30 -0700

    Issue #168: Fix php path on shared hosting environments where exec() is not allowed

--- webapp/_lib/controller/class.CrawlerWebController.php
+++ webapp/_lib/controller/class.CrawlerWebController.php
@@ -11,12 +11,14 @@ class CrawlerWebController extends ThinkUpAuthController {
         Utils::defineConstants();
         $this->setPageTitle("ThinkUp Crawler");
         $this->setViewTemplate('crawler.run-top.tpl');
+        $whichphp = exec('which php');
+        $php_path =  (!empty($whichphp))?$whichphp:'php';
         $this->addSuccessMessage('ThinkUp has just started to collect your posts. This is going to take a little '.
         'while, but if you want to see the technical details of what\'s going on, there\'s a log below. ');
         $this->addInfoMessage('<b>Hint</b><br > If you prefer using the command line, use this command to set up a '.
         'cron job that runs hourly to update your posts:<br />'.
         '<code style="font-family:Courier">cd '.THINKUP_WEBAPP_PATH.
-        'crawler/;export THINKUP_PASSWORD=yourpassword; '.exec('which php').' crawl.php '.
+        'crawler/;export THINKUP_PASSWORD=yourpassword; '.$php_path.' crawl.php '.
         $this->getLoggedInUser().'</code><br />'.
         '(Be sure to change yourpassword to your real password!)');
         echo $this->generateView();
