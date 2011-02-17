03b3997c125c3ef7e004f8235131dea439908146
Eric Van Johnson <eric@esolves.com>
2011-02-16 22:18:56 -0800

    Added tweaks to the EC2 User Script. Patch server, install packages, config apache

--- extras/scripts/ec2-install-userdata/ec2-install-userdata.sh
+++ extras/scripts/ec2-install-userdata/ec2-install-userdata.sh
@@ -15,7 +15,7 @@ sudo apt-get -y install unzip htop mysql-client
 sudo apt-get -y install curl libcurl3 libcurl3-dev php5-curl php5-mcrypt php5-gd --fix-missing
 sudo apt-get -y install sendmail
 
-# Opitional "Apache Best Practice"
+# Optional "Apache Best Practice"
 # Disable directory indexing 
 #a2dismod autoindex
 
