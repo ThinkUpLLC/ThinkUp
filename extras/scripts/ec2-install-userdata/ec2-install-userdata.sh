edb3a2658e1d4c95d547460b83410b16c2c0716d
Eric Van Johnson <eric@esolves.com>
2011-02-20 20:56:04 -0800

    Created Optional Section for configuration

--- extras/scripts/ec2-install-userdata/ec2-install-userdata.sh
+++ extras/scripts/ec2-install-userdata/ec2-install-userdata.sh
@@ -10,20 +10,33 @@ exec > >(tee /var/log/user-data.log|logger -t user-data -s 2>/dev/console) 2>&1
 sudo apt-get update && apt-get upgrade -y
 sudo apt-get -y install apache2 php5-mysql libapache2-mod-php5 
 
-sudo DEBIAN_FRONTEND=noninteractive apt-get install -q -y mysql-server 
-sudo apt-get -y install unzip htop mysql-client
+sudo apt-get -y install unzip  
 sudo apt-get -y install curl libcurl3 libcurl3-dev php5-curl php5-mcrypt php5-gd --fix-missing
 sudo apt-get -y install sendmail
 
-# Optional "Apache Best Practice"
-# Disable directory indexing 
+# Install MySQL directly on instance
+sudo DEBIAN_FRONTEND=noninteractive apt-get install -q -y mysql-server 
+
+# RDS - Would you rather us RDS instead? Comment out the line above and uncomment the line below
+#sudo apt-get -y install mysql-client
+
+
+# Optional Section (not necessary but nice to haves)
+## Packages  
+## phpmyadmin => Web based MySQL client powered by PHP
+## htop => enhanced version of top
+## byobu => screen wrapper that can be configure to track approx. cost of EC2 instance
+## aptitude => high-level interface to the package manager
+## Uncomment line below to install, remove any packages you don't want
+#sudo apt-get -y install  phpmyadmin htop byobu aptitude
+
+# #Apache Best Practice" Configuration
+# Disable directory indexing (uncomment line below)
 #a2dismod autoindex
 
 # restart apache to init php packages
 sudo service apache2 restart
 
-# not necessary but nice to have
-#sudo apt-get -y install phpmyadmin
 
 wget https://github.com/downloads/ginatrapani/ThinkUp/thinkup-0.8.zip --no-check-certificate
 sudo unzip -d /var/www/ thinkup-0.8.zip
@@ -35,6 +48,7 @@ sudo touch /var/www/thinkup/config.inc.php
 sudo chown www-data /var/www/thinkup/config.inc.php
 
 # create database
+
 mysqladmin -u root password NEWPASSWORDHERE 
 mysqladmin -h localhost -u root -pNEWPASSWORDHERE create thinkup
 
