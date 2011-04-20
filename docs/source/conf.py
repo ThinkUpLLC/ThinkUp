316ac1c066c9d5f8ba76f0813a4f5856fd011cc0
Gina Trapani <ginatrapani@gmail.com>
2011-04-19 23:35:35 -0700

    Fixed docs favicon

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -115,7 +115,7 @@ html_logo = 'thinkup-logo_200.png'
 # The name of an image file (within the static path) to use as favicon of the
 # docs.  This file should be a Windows icon file (.ico) being 16x16 or 32x32
 # pixels large.
-html_favicon = 'thinkup_favicon.png'
+html_favicon = 'favicon.ico'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
