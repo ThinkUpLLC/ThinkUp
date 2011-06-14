eb6cc996b5a9cbc6733c879be67f6d1a23975986
Gina Trapani <ginatrapani@gmail.com>
2011-06-14 15:43:01 -0700

    Clean up Smarty plugins and modifiers

--- webapp/_lib/view/plugins/modifier.relative_datetime.php
+++ webapp/_lib/view/plugins/modifier.relative_datetime.php
@@ -9,23 +9,23 @@
  *
  * This file is part of ThinkUp (http://thinkupapp.com).
  *
- * ThinkUp is free software: you can redistribute it and/or modify it under the terms of the GNU General Public 
- * License as published by the Free Software Foundation, either version 2 of the License, or (at your option) any 
+ * ThinkUp is free software: you can redistribute it and/or modify it under the terms of the GNU General Public
+ * License as published by the Free Software Foundation, either version 2 of the License, or (at your option) any
  * later version.
  *
- * ThinkUp is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied 
- * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more 
+ * ThinkUp is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied
+ * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
  * details.
  *
- * You should have received a copy of the GNU General Public License along with ThinkUp.  If not, see 
+ * You should have received a copy of the GNU General Public License along with ThinkUp.  If not, see
  * <http://www.gnu.org/licenses/>.
-*/
+ */
 /**
  * Smarty plugin
  * @package Smarty
  * @subpackage plugins
  */
- 
+
 /**
  * Smarty relative date / time plugin
  *
@@ -42,39 +42,27 @@
  * @param string
  * @return string
  */
-function smarty_modifier_relative_datetime($timestamp)
-{
+function smarty_modifier_relative_datetime($timestamp) {
     if(!$timestamp){
         return 'N/A';
     }
- 
+
     $timestamp = (int)strtotime($timestamp);
     $difference = time() - $timestamp;
     $periods = array("sec", "min", "hour", "day", "week","month", "year", "decade");
     $lengths = array("60","60","24","7","4.35","12","10");
     $total_lengths = count($lengths);
- 
-   /* if ($difference > 0) { // this was in the past
-        $ending = "ago";
-    } else { // this was in the future
-        $difference = -$difference;
-        $ending = " ago";
-    }*/
-    //return;
- 
+
     for($j = 0; $difference > $lengths[$j] && $total_lengths > $j; $j++) {
         $difference /= $lengths[$j];
     }
- 
+
     $difference = round($difference);
     if($difference != 1) {
         $periods[$j].= "s";
     }
- 
+
     $text = "$difference $periods[$j] $ending";
- 
+
     return $text;
 }
-
-
-?>
--- webapp/_lib/view/plugins/modifier.truncate_for_select.php
+++ webapp/_lib/view/plugins/modifier.truncate_for_select.php
@@ -9,22 +9,21 @@
  *
  * This file is part of ThinkUp (http://thinkupapp.com).
  *
- * ThinkUp is free software: you can redistribute it and/or modify it under the terms of the GNU General Public 
- * License as published by the Free Software Foundation, either version 2 of the License, or (at your option) any 
+ * ThinkUp is free software: you can redistribute it and/or modify it under the terms of the GNU General Public
+ * License as published by the Free Software Foundation, either version 2 of the License, or (at your option) any
  * later version.
  *
- * ThinkUp is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied 
- * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more 
+ * ThinkUp is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied
+ * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
  * details.
  *
- * You should have received a copy of the GNU General Public License along with ThinkUp.  If not, see 
+ * You should have received a copy of the GNU General Public License along with ThinkUp.  If not, see
  * <http://www.gnu.org/licenses/>.
-*/
-/**
+ *
  * @author Gina Trapani <ginatrapani[at]gmail[dot]com>
  * @license http://www.gnu.org/licenses/gpl.html
  * @copyright 2009-2011 Gina Trapani
-*/
+ */
 /*
  * Smarty plugin
  * -------------------------------------------------------------
@@ -36,21 +35,21 @@
  *           appending the $etc string.
  * -------------------------------------------------------------
  */
-function smarty_modifier_truncate_for_select($string, $length = 50, $etc = '...',
-                                  $break_words = false)
-{
-    if ($length == 0)
+function smarty_modifier_truncate_for_select($string, $length = 50, $etc = '...', $break_words = false) {
+    if ($length == 0) {
         return '';
+    }
 
     if (strlen($string) > $length) {
         $length -= strlen($etc);
         $fragment = substr($string, 0, $length+1);
-        if ($break_words)
+        if ($break_words) {
             $fragment = substr($fragment, 0, -1);
-        else
+        } else {
             $fragment = preg_replace('/\s+(\S+)?$/', '', $fragment);
+        }
         return $fragment.$etc;
-    } else
+    } else {
         return $string;
+    }
 }
-?>
