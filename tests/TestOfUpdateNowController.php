12439cc70110e2f87c3769914f6979a12926df18
mwilkie <mark@bitterpill.org>
2011-06-30 00:41:57 -0400

    Decouple api key from owner password * added api_key to owner table * added db migration to gen key based on the old sha1 key generator * updated owner dao and tests to support api_key * updated ThinkUpAuthAPIController and tests to use new owner->api_key * updated account config interface, controllers and tests to allow viewing and reseting of the api key * updated 'update now' and rss controller and tests to gen URLs with new api_key * added link to update view to the account config view for key viewing/config access Closes #410

---
+++ tests/TestOfUpdateNowController.php
@@ -0,0 +1,56 @@
+<?php
+/**
+ *
+ * ThinkUp/tests/TestOfUpdateNowController.php
+ *
+ * Copyright (c) 2009-2011 Mark Wilkie
+ *
+ * LICENSE:
+ *
+ * This file is part of ThinkUp (http://thinkupapp.com).
+ *
+ * ThinkUp is free software: you can redistribute it and/or modify it under the terms of the GNU General Public
+ * License as published by the Free Software Foundation, either version 2 of the License, or (at your option) any
+ * later version.
+ *
+ * ThinkUp is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied
+ * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License along with ThinkUp.  If not, see
+ * <http://www.gnu.org/licenses/>.
+ *
+ * Test of RSSController
+ *
+ * @license http://www.gnu.org/licenses/gpl.html
+ * @copyright 2009-2011 Mark Wilkie
+ * @author Mark Wilkie <mwilkie[at]gmail[dot]com>
+ *
+ */
+require_once dirname(__FILE__).'/init.tests.php';
+require_once THINKUP_ROOT_PATH.'webapp/_lib/extlib/simpletest/autorun.php';
+require_once THINKUP_ROOT_PATH.'webapp/config.inc.php';
+
+class TestOfUpdateNowController extends ThinkUpUnitTestCase {
+
+
+    public function testLoadProperRSSUrl() {
+        $builder = $this->buildData();
+        $this->simulateLogin('me@example.com', true, true);
+        $controller = new UpdateNowController(true);
+        $this->assertTrue(isset($controller));
+        $result = $controller->control();
+        $this->assertPattern('/rss.php\?un=me@example.com&as=c9089f3c9adaf0186f6ffb1ee8d6501c/', $result);
+    }
+
+    private function buildData() {
+        $owner_builder = FixtureBuilder::build('owners', array(
+            'id' => 1, 
+            'email' => 'me@example.com', 
+            'pwd' => 'XXX', 
+            'is_activated' => 1,
+            'api_key' => 'c9089f3c9adaf0186f6ffb1ee8d6501c'
+            ));
+            return array($owner_builder);
+    }
+}
