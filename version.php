750469a0000fe195f05c704ff082ae01d2b172bd
Andy Baio <andy@waxy.org>
2011-08-09 12:10:37 -0700

    Updated to beta 14

--- version.php
+++ version.php
@@ -1,6 +1,6 @@
 <?php
 echo 'ThinkUpAppVersion.serverResponse([{';
-$current_version = '0.13';
+$current_version = '0.14';
 if (isset($_GET['v'])) {
 	$install_version = $_GET['v'];
 	if (version_compare($current_version, $install_version, '>')) {
