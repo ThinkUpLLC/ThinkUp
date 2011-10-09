50a9404dfc475ef930e889c461827fbc4494067f
Gina Trapani <ginatrapani@gmail.com>
2011-10-09 12:25:36 -0700

    [DB MIGRATION REQ'D] Defensively rewrite links_post_key migration * Avoid 'Duplicate entry' failure when adding the unique key to the tu_links table by replacing the ALTER statement with CREATE TABLE + INSERT IGNORE
    Developers who already successfully ran the 2011-10-07 version will get this error: "Column already exists: 1060 Duplicate column name 'post_key'"
    To resolve it, run the following SQL:
    
    INSERT INTO tu_completed_migrations (migration) VALUES ('2011-10-09_links_post_key-0');
    INSERT INTO tu_completed_migrations (migration) VALUES ('2011-10-09_links_post_key-1');
    INSERT INTO tu_completed_migrations (migration) VALUES ('2011-10-09_links_post_key-2');
    INSERT INTO tu_completed_migrations (migration) VALUES ('2011-10-09_links_post_key-3');
    INSERT INTO tu_completed_migrations (migration) VALUES ('2011-10-09_links_post_key-4');
    INSERT INTO tu_completed_migrations (migration) VALUES ('2011-10-09_links_post_key-5');
    INSERT INTO tu_completed_migrations (migration) VALUES ('2011-10-09_links_post_key-6');
    INSERT INTO tu_completed_migrations (migration) VALUES ('2011-10-09_links_post_key-7');
    INSERT INTO tu_completed_migrations (migration) VALUES ('2011-10-09_links_post_key-8');
    INSERT INTO tu_completed_migrations (migration) VALUES ('2011-10-09_links_post_key-9');
    INSERT INTO tu_completed_migrations (migration) VALUES ('2011-10-09_links_post_key-10');
    INSERT INTO tu_completed_migrations (migration) VALUES ('2011-10-09_links_post_key-11');
    INSERT INTO tu_completed_migrations (migration) VALUES ('2011-10-09_links_post_key-12');

---
+++ webapp/install/sql/mysql_migrations/2011-10-09_links_post_key.sql
@@ -0,0 +1,31 @@
+--
+-- Change tu_links foreign key to post_key (tu_posts.id) from post_id, network
+--
+
+ALTER TABLE  tu_links ADD  post_key INT( 11 ) NOT NULL
+COMMENT  'Internal ID of the post in which this link appeared.' AFTER  clicks;
+
+UPDATE tu_links, tu_posts SET tu_links.post_key = tu_posts.id 
+WHERE tu_links.post_id = tu_posts.post_id AND tu_links.network = tu_posts.network;
+
+ALTER TABLE  tu_links DROP INDEX  post_id;
+
+ALTER TABLE  tu_links DROP INDEX  url;
+
+DROP TABLE IF EXISTS tu_links_b16;
+
+CREATE TABLE tu_links_b16 LIKE tu_links;
+
+ALTER TABLE  tu_links_b16 ADD INDEX  post_key (  post_key );
+
+ALTER TABLE  tu_links_b16 ADD UNIQUE INDEX url (  url ,  post_key );
+
+INSERT IGNORE INTO tu_links_b16 (SELECT * FROM tu_links);
+
+ALTER TABLE  tu_links_b16 DROP  post_id, DROP  network ;
+
+RENAME TABLE tu_links to tu_links_b15;
+
+RENAME TABLE tu_links_b16 TO tu_links;
+
+DROP TABLE tu_links_b15;
