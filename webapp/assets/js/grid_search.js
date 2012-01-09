d57c843ebe257fa7dabf0b34d03aebd54fff7ac0
Gina Trapani <ginatrapani@gmail.com>
2012-01-09 00:04:45 -0500

    Fixed grid search Uncaught TypeError, closes #1187

--- webapp/assets/js/grid_search.js
+++ webapp/assets/js/grid_search.js
@@ -4,49 +4,51 @@
 var TUGridSearch = function() {
 
     /**
-     * @var boolean Enable for console logging
-     */
+	 * @var boolean Enable for console logging
+	 */
     this.DEBUG = false;
 
     this.searchString = "";
     
     this.loading = false;
 
+    var self = this;
+
     this.keyup = function(e) {
         if (e.keyCode == 27) { 
-            tu_grid_search.close_iframe();
+            self.close_iframe();
         }
     };
 
     /**
-     * Init grid search
-     */
+	 * Init grid search
+	 */
     this.init = function() {
         // register on submit event on our form
         $(document).ready(function() {
-            if (tu_grid_search.DEBUG) {
+            if (self.DEBUG) {
                 console.log("Grid Search initialized...");
             }
             $('#grid_search_icon').click(function() {
-                tu_grid_search.load_iframe();
+                self.load_iframe();
             });
             $('#close_grid_search').click(function() {
-                tu_grid_search.close_iframe();
+                self.close_iframe();
             });
             if(document.location.search.match(/search=/)) {
-                tu_grid_search.load_iframe();
+                self.load_iframe();
             }
         });
-        //tu_grid_search.load_iframe();
+        // self.load_iframe();
     }
 
     /**
-     * @param Object
-     *            {success: true|false, posts: [a posts array]};
-     */
+	 * @param Object
+	 *            {success: true|false, posts: [a posts array]};
+	 */
     this.populate_grid = function(obj) {
-        if (tu_grid_search.DEBUG) { console.debug(obj.posts.length + ' posts'); }
-        if (tu_grid_search.DEBUG) { console.debug(obj.limit + ' limit'); }
+        if (self.DEBUG) { console.debug(obj.posts.length + ' posts'); }
+        if (self.DEBUG) { console.debug(obj.limit + ' limit'); }
         if((obj.posts.length - obj.limit) == 1) {
             $('#max_rows').html(obj.limit);
             $('#overlimit').show();
@@ -105,9 +107,9 @@ var TUGridSearch = function() {
                 return value;
             }
         } ];
-        //if( parent.GRID_TYPE == 2) {
+        // if( parent.GRID_TYPE == 2) {
         columns[3].width = 400;
-        //}
+        // }
 
         var options = {
             enableCellNavigation : false,
@@ -116,16 +118,16 @@ var TUGridSearch = function() {
 
         this.dataView.beginUpdate();
         this.dataView.setItems(obj.posts);
-        this.dataView.setFilter(tu_grid_search.myFilter);
+        this.dataView.setFilter(self.myFilter);
         this.dataView.endUpdate();
-        $("#grid_search_count").html(this.add_commas(tu_grid_search.dataView.rows.length));
+        $("#grid_search_count").html(self.add_commas(self.dataView.rows.length));
         $('#myGrid').show();
         var grid = new Slick.Grid($("#myGrid"), this.dataView.rows, columns, options);
         
         this.dataView.onRowCountChanged.subscribe(function(args) {
             grid.updateRowCount();
             grid.render();
-            $("#grid_search_count").html(this.add_commas(tu_grid_search.dataView.rows.length));
+            $("#grid_search_count").html(self.add_commas(self.dataView.rows.length));
         });
         
         this.dataView.onRowsChanged.subscribe(function(rows) {
@@ -137,38 +139,38 @@ var TUGridSearch = function() {
             Slick.GlobalEditorLock.cancelCurrentEdit();
             var select_element = $('#grid_search_input');
             this.value = select_element.val();
-            tu_grid_search.searchString = this.value;
-            tu_grid_search.dataView.refresh();
+            self.searchString = this.value;
+            self.dataView.refresh();
         });
 
         $("#grid_export").click(function(e) {
             $('#grid_search_form').hide();
             $('#grid_search_spinner').show();
-            $('#grid_export_data').val( $.toJSON(tu_grid_search.dataView.rows) );
+            $('#grid_export_data').val( $.toJSON(self.dataView.rows) );
             $('#grid_export_form').submit();
             $('#grid_search_spinner').hide();
             $('#grid_search_form').show();
         });
 
-        //if search arg, filter...
+        // if search arg, filter...
         if(match_array = document.location.search.match(/search=(.*?)&/)) {
             search_query = decodeURIComponent(unescape(match_array[1])).replace(/\+/g, ' ');
-            if (tu_grid_search.DEBUG) { console.debug("search param defined: %s", search_query); }
+            if (self.DEBUG) { console.debug("search param defined: %s", search_query); }
             $('#grid_search_input').val(search_query);
             $('#grid_search_input').focus();
             this.value = search_query;
-            tu_grid_search.searchString = this.value;
-            tu_grid_search.dataView.refresh();
+            self.searchString = this.value;
+            self.dataView.refresh();
         }
     }
 
     /**
-     * search filter
-     */
+	 * search filter
+	 */
     this.myFilter = function (item) {
         if(item['id'] == -1 || item['text'] == null) { return false; }
-        if (tu_grid_search.searchString != "" && 
-        item["text"].toLowerCase().indexOf(tu_grid_search.searchString.toLowerCase()) == -1) {
+        if (self.searchString != "" && 
+        item["text"].toLowerCase().indexOf(self.searchString.toLowerCase()) == -1) {
             return false;
         } else {
             return true;
@@ -176,17 +178,17 @@ var TUGridSearch = function() {
     }
 
     /**
-     * 
-     */
+	 * 
+	 */
     this.load_iframe = function(nolimit) {
         nolimit = nolimit ? true : false;
         // close grid search with escape key
         $(document).keyup( this.keyup );
 
-        if(tu_grid_search.loading) { return; };
+        if(self.loading) { return; };
         // close top 20 words if needed
         if(typeof(tu_word_freq) != 'undefined') { tu_word_freq.close(); };
-        tu_grid_search.loading = true;
+        self.loading = true;
         if(window.GRID_TYPE && GRID_TYPE==1) {
             window.scroll(0,0);
             $('#screen').css({ opacity: 0.7, "width":$(document).width(),"height":$(document).height()});
@@ -210,15 +212,15 @@ var TUGridSearch = function() {
             $('#grid_iframe').attr('src',
                     path + 'assets/html/grid.html?' + query_string + '&nolimit=' + nolimit
                     + '&cb=' + (new Date()).getTime());
-            if (tu_grid_search.DEBUG) {
+            if (self.DEBUG) {
                 console.debug("loading grid search iframes %s",  $('#grid_iframe').attr('src') );
             }
-            tu_grid_search.loading = false;
+            self.loading = false;
         });
     }
     /**
-     * 
-     */
+	 * 
+	 */
     this.close_iframe = function() {
         var path = typeof (site_root_path) != 'undefined' ? site_root_path : '';
         $('#grid_iframe').attr('src', path + '/assets/img/ui-bg_glass_65_ffffff_1x400.png');
@@ -236,12 +238,12 @@ var TUGridSearch = function() {
     }
     
     /**
-     * load xss script with post data callback
-     */
+	 * load xss script with post data callback
+	 */
     this.get_data = function() {
         $('#myGrid').hide();
         var url = '../../post/grid.php' + document.location.search;
-        if (tu_grid_search.DEBUG) { console.debug('Getting data with url: ' + url); }
+        if (self.DEBUG) { console.debug('Getting data with url: ' + url); }
         script = document.createElement('script');
         script.setAttribute("src", url);
         script.setAttribute("type", "text/javascript");
@@ -249,8 +251,8 @@ var TUGridSearch = function() {
     }
     
     /**
-     * Format numeric string with commas
-     */
+	 * Format numeric string with commas
+	 */
     this.add_commas = function(nStr) {
         nStr += '';
         x = nStr.split('.');
