bf3dbdf64819bba0b5ebbcd368420093ca0b2ae3
Andrew Stewart <andrew@averagestudios.com>
2012-01-26 14:55:21 -0800

    Changed indentation to four spaces from two

--- webapp/_lib/model/class.Loader.php
+++ webapp/_lib/model/class.Loader.php
@@ -33,146 +33,146 @@
  */
 class Loader {
 
-  /**
-   * Lookup paths for classes and interfaces
-   * @var array
-   */
-  private static $lookup_path;
+    /**
+     * Lookup paths for classes and interfaces
+     * @var array
+     */
+    private static $lookup_path;
 
-  /**
-   * Classes whose filename doesn't follow the convention
-   * @var array
-   */
-  private static $special_classes;
+    /**
+     * Classes whose filename doesn't follow the convention
+     * @var array
+     */
+    private static $special_classes;
 
-  /**
-   * Register
-   *
-   * Registers the autoloader to enable lazy loading
-   *
-   * @param array $paths Array of additional lookup path strings
-   * @return bool
-   */
-  public static function register(Array $paths) {
-    self::setLookupPath($paths);
-    return spl_autoload_register(array(__CLASS__, "load"));
-  }
-
-  /**
-   * Unregister
-   *
-   * Unregisters the autoloader script, disabling lazy loading
-   *
-   * @return bool
-   */
-  public static function unregister() {
-    self::$lookup_path = null;
-    self::$special_classes = null;
-    return spl_autoload_unregister(array(__CLASS__, "load"));
-  }
-
-  /**
-   * Set Lookup Path
-   *
-   * Establishes lookup paths, including additional paths if provided
-   *
-   * @param array $paths Array of additional lookup path strings
-   */
-  private static function setLookupPath(Array $paths = null) {
-    // check required constants
-    defined("THINKUP_ROOT_PATH") || define("THINKUP_ROOT_PATH", dirname(dirname(dirname(dirname(__FILE__)))) . "/");
-    defined("THINKUP_WEBAPP_PATH") || define("THINKUP_WEBAPP_PATH", dirname(dirname(dirname(__FILE__))) . "/");
-
-    // set default lookup paths
-    self::$lookup_path = array(
-      THINKUP_WEBAPP_PATH . "_lib/model/",
-      THINKUP_WEBAPP_PATH . "_lib/controller/",
-      THINKUP_WEBAPP_PATH . "_lib/model/exceptions/"
-    );
-
-    // set default lookup path for special classes
-    self::$special_classes = array(
-      "Smarty" => THINKUP_WEBAPP_PATH . "_lib/extlib/Smarty-2.6.26/libs/Smarty.class.php"
-    );
-
-    if (isset($paths)) {
-      foreach($paths as $path) self::$lookup_path[] = $path;
-    }
-  }
-
-  /**
-   * Add Path
-   *
-   * Adds another path to crawl for class files
-   *
-   * @param string $path
-   */
-  public static function addPath($path) {
-    if (!isset(self::$lookup_path)) self::register();
-    self::$lookup_path[] = $path;
-  }
-
-  /**
-   * Get Lookup Path
-   *
-   * Gets the array of lookup paths
-   *
-   * @return array
-   */
-  public static function getLookupPath() {
-    return self::$lookup_path;
-  }
-
-  /**
-   * Get Special Classes
-   *
-   * Gets the array of special class paths
-   *
-   * @return array
-   */
-  public static function getSpecialClasses() {
-    return self::$special_classes;
-  }
-
-  /**
-   * Load
-   *
-   * The method registered to run on _autoload. When a class is instantiated, this
-   * method will be called to look up the class file if the class is not present.
-   * The second instantiation of the same class wouldn't call this method.
-   *
-   * @param string $class
-   * @param bool
-   */
-  public static function load($class) {
-    // check if class is already in scope
-    if (class_exists($class, false)) return;
-
-    // if class is a standard ThinkUp object or interface
-    foreach(self::$lookup_path as $path) {
-      $filename = $path . "class." . $class . ".php";
-      if (file_exists($filename)) {
-        require_once($filename);
-        return;
-      }
-
-      $filename = $path . "interface." . $class . ".php";
-      if (file_exists($filename)) {
-        require_once($filename);
-        return;
-      }
-
-      $filename = $path . $class . ".php";
-      if (file_exists($filename)) {
-        require_once($filename);
-        return;
-      }
+    /**
+     * Register
+     *
+     * Registers the autoloader to enable lazy loading
+     *
+     * @param array $paths Array of additional lookup path strings
+     * @return bool
+     */
+    public static function register(Array $paths) {
+        self::setLookupPath($paths);
+        return spl_autoload_register(array(__CLASS__, "load"));
     }
 
-    // if class is a special class
-    if(array_key_exists($class, self::$special_classes)) {
-      require_once(self::$special_classes[$class]);
-      return;
+    /**
+     * Unregister
+     *
+     * Unregisters the autoloader script, disabling lazy loading
+     *
+     * @return bool
+     */
+    public static function unregister() {
+        self::$lookup_path = null;
+        self::$special_classes = null;
+        return spl_autoload_unregister(array(__CLASS__, "load"));
     }
-  }
+
+    /**
+     * Set Lookup Path
+     *
+     * Establishes lookup paths, including additional paths if provided
+     *
+     * @param array $paths Array of additional lookup path strings
+     */
+    private static function setLookupPath(Array $paths = null) {
+        // check required constants
+        defined("THINKUP_ROOT_PATH") || define("THINKUP_ROOT_PATH", dirname(dirname(dirname(dirname(__FILE__)))) . "/");
+        defined("THINKUP_WEBAPP_PATH") || define("THINKUP_WEBAPP_PATH", dirname(dirname(dirname(__FILE__))) . "/");
+
+        // set default lookup paths
+        self::$lookup_path = array(
+            THINKUP_WEBAPP_PATH . "_lib/model/",
+            THINKUP_WEBAPP_PATH . "_lib/controller/",
+            THINKUP_WEBAPP_PATH . "_lib/model/exceptions/"
+        );
+
+        // set default lookup path for special classes
+        self::$special_classes = array(
+            "Smarty" => THINKUP_WEBAPP_PATH . "_lib/extlib/Smarty-2.6.26/libs/Smarty.class.php"
+        );
+
+        if (isset($paths)) {
+            foreach($paths as $path) self::$lookup_path[] = $path;
+        }
+    }
+
+    /**
+     * Add Path
+     *
+     * Adds another path to crawl for class files
+     *
+     * @param string $path
+     */
+    public static function addPath($path) {
+        if (!isset(self::$lookup_path)) self::register();
+        self::$lookup_path[] = $path;
+    }
+
+    /**
+     * Get Lookup Path
+     *
+     * Gets the array of lookup paths
+     *
+     * @return array
+     */
+    public static function getLookupPath() {
+        return self::$lookup_path;
+    }
+
+    /**
+     * Get Special Classes
+     *
+     * Gets the array of special class paths
+     *
+     * @return array
+     */
+    public static function getSpecialClasses() {
+        return self::$special_classes;
+    }
+
+    /**
+     * Load
+     *
+     * The method registered to run on _autoload. When a class is instantiated, this
+     * method will be called to look up the class file if the class is not present.
+     * The second instantiation of the same class wouldn't call this method.
+     *
+     * @param string $class
+     * @param bool
+     */
+    public static function load($class) {
+        // check if class is already in scope
+        if (class_exists($class, false)) return;
+
+        // if class is a standard ThinkUp object or interface
+        foreach(self::$lookup_path as $path) {
+            $filename = $path . "class." . $class . ".php";
+            if (file_exists($filename)) {
+                require_once($filename);
+                return;
+            }
+
+            $filename = $path . "interface." . $class . ".php";
+            if (file_exists($filename)) {
+                require_once($filename);
+                return;
+            }
+
+            $filename = $path . $class . ".php";
+            if (file_exists($filename)) {
+                require_once($filename);
+                return;
+            }
+        }
+
+        // if class is a special class
+        if(array_key_exists($class, self::$special_classes)) {
+            require_once(self::$special_classes[$class]);
+            return;
+        }
+    }
 }
