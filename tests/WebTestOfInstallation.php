fabbd8d5132fa5bc3c48f9c90fdfb0bfde3d0a66
Gina Trapani <ginatrapani@gmail.com>
2012-02-13 10:15:41 -0800

    Add regresstion test for custom folder installation location

--- tests/WebTestOfInstallation.php
+++ tests/WebTestOfInstallation.php
@@ -53,6 +53,15 @@ class WebTestOfInstallation extends ThinkUpBasicWebTestCase {
         'cd thinkup;chmod -R 777 data;');
     }
 
+    public function setUpCustomFolderInstallation() {
+        //Clean up files from test installation
+        exec('rm -rf webapp/test_installer/*');
+        exec('cp build/thinkup.zip webapp/test_installer/.;'.
+        'cd webapp/test_installer/;'.
+        'unzip thinkup.zip;mv thinkup mythinkupfolder; chmod -R 777 mythinkupfolder;'.
+        'cd mythinkupfolder;chmod -R 777 data;');
+    }
+
     public function tearDown() {
         global $THINKUP_CFG;
         //Clean up test installation files
@@ -140,6 +149,49 @@ class WebTestOfInstallation extends ThinkUpBasicWebTestCase {
         $this->click("Settings");
         $this->assertTitle('Configure Your Account | ThinkUp');
         $this->assertText('admin');
+    }
+
+    public function testSuccessfulInstallationInCustomFolder() {
+        self::setUpCustomFolderInstallation();
+        require THINKUP_WEBAPP_PATH.'config.inc.php';
+
+        //Config file doesn't exist
+        $this->assertFalse(file_exists($THINKUP_CFG['source_root_path'].
+          '/webapp/test_installer/mythinkupfolder/config.inc.php'));
+
+        //sleep(1000);
+        //Start installation process
+        $this->get($this->url.'/test_installer/mythinkupfolder/');
+        $this->assertTitle("ThinkUp");
+        $this->assertText('ThinkUp\'s configuration file does not exist! Try installing ThinkUp.');
+        $this->clickLink("installing ThinkUp.");
+        $this->assertText('Great! Your system has everything it needs to run ThinkUp.');
+        $this->clickLinkById('nextstep');
+
+        //sleep(1000);
+        $this->assertText('Create Your ThinkUp Account');
+        $this->setField('full_name', 'ThinkUp J. User');
+        $this->setField('site_email', 'user@example.com');
+        $this->setField('password', 'secret12345');
+        $this->setField('confirm_password', 'secret12345');
+        $this->setField('timezone', 'America/Los_Angeles');
+
+        $this->setField('db_host', $THINKUP_CFG['db_host']);
+        $this->setField('db_name', $this->test_database_name);
+        $this->setField('db_user', $THINKUP_CFG['db_user']);
+        $this->setField('db_passwd', $THINKUP_CFG['db_password']);
+        $this->setField('db_socket', $THINKUP_CFG['db_socket']);
+        $this->setField('db_prefix', $THINKUP_CFG['table_prefix']);
+        $this->clickSubmitByName('Submit');
+
+        //$this->showSource();
+        //sleep(1000);
+        $this->assertText('ThinkUp has been installed successfully. Check your email account; an account activation '.
+        'message has been sent.');
+
+        //Config file has been written
+        $this->assertTrue(file_exists($THINKUP_CFG['source_root_path'].
+        '/webapp/test_installer/mythinkupfolder/config.inc.php'));
     }
 
     public function testFieldLevelMessagesForInvalidInputs() {
