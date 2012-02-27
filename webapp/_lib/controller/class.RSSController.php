0bb107a58aaf86f9118dd6716b55c8234c964724
Gina Trapani <ginatrapani@gmail.com>
2012-02-27 10:37:57 -0800

    Restore RSS feed crawler trigger, closes #1255

--- webapp/_lib/controller/class.RSSController.php
+++ webapp/_lib/controller/class.RSSController.php
@@ -57,7 +57,7 @@ class RSSController extends ThinkUpAuthAPIController {
             $crawler_last_run = strtotime($freshest_instance->crawler_last_run);
         }
         if ($freshest_instance && $crawler_last_run < time() - $rss_crawler_refresh_rate*60) {
-            $crawler_run_url = $base_url.'run.php?'.sprintf('un=%s&as=%s', $email, $owner->api_key);
+            $crawler_run_url = $base_url.'crawler/run.php?'.sprintf('un=%s&as=%s', $email, $owner->api_key);
             $ch = curl_init();
             curl_setopt($ch, CURLOPT_URL, $crawler_run_url);
             curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 5); // seconds
