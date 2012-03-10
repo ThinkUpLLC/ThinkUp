14057123df6b03b4cbb60c4f046d2e8abb53f708
Gina Trapani <ginatrapani@gmail.com>
2012-03-10 01:21:31 -0800

    Add web updater test override

--- version_download.php
+++ version_download.php
@@ -1,8 +1,14 @@
 <?php
 /**
- * Redirect request to location of the latest user package download.
+ * Return current version and download location in JSON format, with override for web updater tests.
  */
 require_once('version.inc.php');
 header('Content-type: application/json');
+
+// Test override
+if (isset($_GET['version']) && isset($_GET['url'])) {
+	$current_version = $_GET['version'];
+	$current_version_download_link = $_GET['url'];
+}
 ?>
 {"version":"<?= $current_version ?>", "url":"<?= $current_version_download_link ?>"}
