3a01fb97ce7cc81ff9b3485e631ac25ad6638c5b
Gina Trapani <ginatrapani@gmail.com>
2012-03-23 18:04:13 -0400

    Update beta version notifier

--- version.beta.inc.php
+++ version.beta.inc.php
@@ -1,4 +1,4 @@
 <?php
 //Edit this file on each new release
-$current_version = "1.0.4-b2";
-$current_version_download_link = "http://thinkupapp.com/downloads/beta/thinkup_1.0.4-b2.zip";
+$current_version = "1.0.4-beta.2";
+$current_version_download_link = "https://github.com/downloads/ginatrapani/ThinkUp/thinkup_1.0.4-beta.2.zip";
