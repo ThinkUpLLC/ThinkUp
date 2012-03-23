adcf30b154490eecfcfd5cf98b29df99912c6565
Gina Trapani <ginatrapani@gmail.com>
2012-03-23 16:51:50 -0400

    Add beta download channel and flat JSON-based version updater

---
+++ version.beta.inc.php
@@ -0,0 +1,4 @@
+<?php
+//Edit this file on each new release
+$current_version = "1.0.4-b2";
+$current_version_download_link = "http://thinkupapp.com/downloads/beta/thinkup_1.0.4-b2.zip";
---
+++ version_download_beta.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Return current version and download location in JSON format, with override for web updater tests.
+ */
+require_once('version.beta.inc.php');
+header('Content-type: application/json');
+
+// Test override
+if (isset($_GET['version']) && isset($_GET['url'])) {
+	$current_version = $_GET['version'];
+	$current_version_download_link = $_GET['url'];
+}
+?>
+{"version":"<?= $current_version ?>", "url":"<?= $current_version_download_link ?>"}
