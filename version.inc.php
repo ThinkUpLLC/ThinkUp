bfd49553230206509f965664c1f36753e5ed6969
Gina Trapani <ginatrapani@gmail.com>
2012-05-07 14:11:56 -0700

    Version 1.0.6

--- version.inc.php
+++ version.inc.php
@@ -1,4 +1,4 @@
 <?php
 //Edit this file on each new release
-$current_version = "1.0.5";
-$current_version_download_link = "https://github.com/downloads/ginatrapani/ThinkUp/thinkup_1.0.5.zip";
+$current_version = "1.0.6";
+$current_version_download_link = "https://github.com/downloads/ginatrapani/ThinkUp/thinkup_1.0.6.zip";
