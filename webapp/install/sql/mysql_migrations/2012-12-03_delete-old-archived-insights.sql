1712ccfd1e8497e8433c30acae9915e055b303a6
Gina Trapani <ginatrapani@gmail.com>
2012-12-03 18:21:28 -0800

    Add Response Rates chart to retweet spike insight

--- webapp/install/sql/mysql_migrations/2012-12-03_delete-old-archived-insights.sql
+++ webapp/install/sql/mysql_migrations/2012-12-03_delete-old-archived-insights.sql
@@ -1,1 +1,7 @@
+-- Clear out existing archived_posts_ insights and use new format
 DELETE FROM tu_insights WHERE SUBSTR( slug, 1, LENGTH(  'archived_posts_' ) ) =  'archived_posts_';
+
+-- Clear out existing retweet spike insights and regenerate using new data format
+DELETE FROM tu_insights WHERE SUBSTR( slug, 1, LENGTH(  'avg_retweet_count_last_' ) ) =  'avg_retweet_count_last_';
+DELETE FROM tu_insights WHERE SUBSTR( slug, 1, LENGTH(  'retweet_spike_' ) ) =  'retweet_spike_';
+DELETE FROM tu_insights WHERE SUBSTR( slug, 1, LENGTH(  'retweet_high_' ) ) =  'retweet_high_';
