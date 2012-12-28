ba67f98899a11d214f327ca7587ab6b1b976eecf
Gina Trapani <ginatrapani@gmail.com>
2012-12-27 22:55:28 -0800

    Test fixes

--- webapp/plugins/insightsgenerator/tests/TestOfStyleStatsInsight.php
+++ webapp/plugins/insightsgenerator/tests/TestOfStyleStatsInsight.php
@@ -87,7 +87,7 @@ class TestOfStyleStatsInsight extends ThinkUpUnitTestCase {
         $this->assertEqual($result->slug, 'style_stats');
         $this->assertEqual($result->prefix, 'Your posting style:');
         $this->assertEqual($result->filename, 'stylestats');
-        $this->assertPattern('/7 of your posts this week were photos, none were links, none were quotations, and '.
+        $this->assertPattern('/of your posts this week were photos, none were links, none were quotations, and '.
         'none were questions./', $result->text);
         //sleep(1000);
     }
