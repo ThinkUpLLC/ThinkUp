69d2c875fb7eaf51e8d882171756fc448056aba3
Gina Trapani <ginatrapani@gmail.com>
2013-01-01 16:06:41 -0800

    Version 2.0-beta.1

--- version.beta.inc.php
+++ version.beta.inc.php
@@ -1,4 +1,4 @@
 <?php
 //Edit this file on each new release
-$current_version = "1.2.1";
-$current_version_download_link = "https://thinkup.com/downloads/thinkup-1.2.1.zip";
+$current_version = "2.0-beta.1";
+$current_version_download_link = "https://thinkup.com/downloads/beta/thinkup-2.0-beta.1.zip";
