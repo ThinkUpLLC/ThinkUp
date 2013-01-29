0c45aa1c33f83e34b2711e4afbc124792df87149
Kurt Zenisek <KZeni1@gmail.com>
2013-01-29 11:49:19 -0600

    Refresh Data link on Insights now pulls Developer Log setting. The refresh data link in the status bar of the Insights view was ignoring the enable developer log setting (defaulting to not being enabled). I noticed this was a quick fix of adding this variable for the Insights controller (in addition to the Dashboard controller). Closes #1474

--- webapp/_lib/controller/class.InsightStreamController.php
+++ webapp/_lib/controller/class.InsightStreamController.php
@@ -42,6 +42,7 @@ class InsightStreamController extends ThinkUpController {
         $this->setViewTemplate('insights.tpl');
         $this->addToView('enable_bootstrap', true);
         $this->addToView('enable_geoencoder_iframe', true);
+        $this->addToView('developer_log', $config->getValue('is_log_verbose'));
 
         if ($this->shouldRefreshCache() ) {
             if (isset($_GET['u']) && isset($_GET['n']) && isset($_GET['d']) && isset($_GET['s'])) {
