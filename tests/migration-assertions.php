d2cfc1a2f0bccd354d6e4b90b9180289f76d5eb4
Gina Trapani <ginatrapani@gmail.com>
2013-02-24 01:34:06 -0800

    Version 1.3

--- tests/migration-assertions.php
+++ tests/migration-assertions.php
@@ -932,7 +932,14 @@ $MIGRATIONS = array(
 
      /* 1.2.1 */
     '1.2.1' => array(
+        'zip_url' => 'https://thinkup.com/downloads/thinkup-1.2.1.zip',
+        'migrations' => 0,
+     ),
+
+     /* 1.3 */
+    '1.3' => array(
         'zip_url' => 'file://./build/thinkup.zip',
         'migrations' => 0,
      ),
+
 );
