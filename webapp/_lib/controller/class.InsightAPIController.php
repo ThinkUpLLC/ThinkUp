561685975731f60cf0c3617acf38ae320c0f72f3
nilakshdas <nilakshdas@gmail.com>
2013-04-20 19:22:33 +0530

    Issue #1551 - Updated InsightMySQLDAO to serve insights in time range

--- webapp/_lib/controller/class.InsightAPIController.php
+++ webapp/_lib/controller/class.InsightAPIController.php
@@ -31,36 +31,6 @@
  */
 class InsightAPIController extends ThinkUpAuthAPIController {
     /**
-     * The network to query. Defaults to twitter.
-     * @var str
-     */
-    public $network = 'twitter';
-    /**
-     * The user ID to use. No default value. In requests that require user data, either this or username must be set.
-     * @var int
-     */
-    public $user_id;
-    /**
-     * The username to use. No default value. In requests that require user data, either this or user ID must be set.
-     * @var str
-     */
-    public $username;
-    /**
-     * Internal unique ID.
-     * @var int
-     */
-    public $insight_id;
-    /**
-     * Prefix to the text content of the alert.
-     * @var str
-     */
-    public $prefix;
-    /**
-     * ThinkUp API Key to access the insight.
-     * @var str
-     */
-    private $api_key;
-    /**
      * The number of results to return. Defaults to 20.
      * @var int
      */
@@ -71,19 +41,8 @@ class InsightAPIController extends ThinkUpAuthAPIController {
      */
     public $page = 1;
     /**
-     * What to order the results by. Does not work on all calls. Defaults to "default". Different calls handle this
-     * value differently.
-     * @var str
-     */
-    public $order_by = 'date';
-    /**
-     * The direction to order the results in. Defaults to DESC for descending order.
-     * @var str DESC or ASC
-     */
-    public $direction = 'DESC';
-    /**
-     * In time range API calls, this is the starting date. Can be a Unix timestamp or a valid time string. Defaults to
-     * 0 which represents midnight on Jan 1st 1970.
+     * In time range API calls, this is the starting date. Can be a Unix timestamp or a valid time string.
+     * Defaults to 0 which represents midnight on Jan 1st 1970.
      * @var mixed 
      */
     public $from = 0;
@@ -91,12 +50,12 @@ class InsightAPIController extends ThinkUpAuthAPIController {
      * In time range API calls, this is the end date. Can be a Unix timestamp or a valid time string.
      * @var mixed
      */
-    public $until_time;
+    public $until = null;
     /**
-     * Whether or not to trim the user to just the user ID. Defaults to false.
-     * @var bool
+     * ThinkUp API Key to access the insight.
+     * @var str
      */
-    public $trim_user = false;
+    private $api_key;
     /**
      * A User object set when either the user_id or username variables are set. If you are using User data at any point
      * in this class, you should use this object.
@@ -107,10 +66,6 @@ class InsightAPIController extends ThinkUpAuthAPIController {
      * @var InsightDAO
      */
     private $insight_dao;
-    /**
-     * @var UserDAO
-     */
-    private $user_dao;
     /**
      * Constructor
      *
@@ -124,28 +79,6 @@ class InsightAPIController extends ThinkUpAuthAPIController {
         /*
          * START READ IN OF QUERY STRING VARS
          */
-        if (isset($_GET['network'])) {
-            $this->network = $_GET['network'];
-        }
-        if (isset($_GET['user_id'])) {
-            if (is_numeric($_GET['user_id'])) {
-                $this->user_id = $_GET['user_id'];
-            }
-        }
-        if (isset($_GET['username'])) {
-            $this->username = $_GET['username'];
-        }
-        if (isset($_GET['insight_id'])) {
-            if (is_numeric($_GET['insight_id'])) {
-                $this->insight_id = $_GET['insight_id'];
-            }
-        }
-        if (isset($_GET['prefix'])) {
-            $this->prefix = $_GET['prefix'];
-        }
-        if (isset($_GET['api_key'])) {
-            $this->api_key = $_GET['api_key'];
-        }
         if (isset($_GET['count'])) {
             if (is_numeric($_GET['count'])) {
                 $this->count = (int) $_GET['count'] > 200 ? 200 : (int) $_GET['count'];
@@ -156,59 +89,18 @@ class InsightAPIController extends ThinkUpAuthAPIController {
                 $this->page = (int) $_GET['page'];
             }
         }
-        if (isset($_GET['order_by'])) {
-            $this->order_by = $this->parseOrderBy($_GET['order_by']);
-        }
-        if (isset($_GET['direction'])) {
-            $this->direction = $_GET['direction'] == 'DESC' ? 'DESC' : 'ASC';
-        }
         if (isset($_GET['from'])) {
             $this->from = $_GET['from'];
         }
         if (isset($_GET['until'])) {
             $this->until = $_GET['until'];
         }
-        if (isset($_GET['trim_user'])) {
-            $this->trim_user = $this->isTrue($_GET['trim_user']);
+        if (isset($_GET['api_key'])) {
+            $this->api_key = $_GET['api_key'];
         }
         /*
          * END READ IN OF QUERY STRING VARS
          */
-    }
-
-    /**
-     * Convert the order_by option to database column.
-     *
-     * For example, 'date' gets converted into the appropriate database colum name: 'pub_date'.
-     *
-     * @param string $order_by The value from $_GET['order_by']
-     * @return string A valid database column.
-     */
-    private function parseOrderBy($order_by) {
-        switch ($order_by) {
-            case 'date': $order_by = 'date';
-            break;
-
-            default: $order_by = $this->order_by;
-            break;
-        }
-
-        return $order_by;
-    }
-
-    /**
-     * Determine whether the given value represents true or not. Used for the boolean $_GET values such as
-     * trim_user and include_entities.
-     *
-     * @param string $var The value to determine.
-     * @return bool True if $var is 't', 'true' or '1'.
-     */
-    private function isTrue($var) {
-        if (isset($var) && !is_null($var)) {
-            return $var == 'true' || $var == 't' || $var == '1';
-        } else {
-            return false;
-        }
     }
 
     public function control() {
@@ -234,33 +126,19 @@ class InsightAPIController extends ThinkUpAuthAPIController {
             throw new APIDisabledException();
         }
 
-        // fetch the correct PostDAO and UserDAO from the DAOFactory
-        $this->post_dao = DAOFactory::getDAO('InsightDAO');
-        $this->user_dao = DAOFactory::getDAO('UserDAO');
+        // Fetch the correct InsightDAO from the DAOFactory
+        $this->insight_dao = DAOFactory::getDAO('InsightDAO');
 
-        /*
-         * Use the information gathered from the query string to retrieve a
-         * User object. This will be the standard object with which to get
-         * User information from in API calls.
-         */
-        if ($this->user_id != null) {
-            $this->user = $this->user_dao->getDetails($this->user_id, $this->network);
-        } else if ($this->username != null) {
-            $this->user = $this->user_dao->getUserByName($this->username, $this->network);
-        } else {
-            $this->user = null;
-        }
         //Privacy checks
         $email = $this->getLoggedInUser();
         $owner = parent::getOwner($email);
-        if (!(isset($_GET['api_key']) && $this->api_key == $owner->api_key)) {
+        if ($this->api_key != $owner->api_key) {
             $m = 'An insight request requires a valid ThinkUp API Key to be specified.';
             throw new APIOAuthException($m);
         }
 
-        $data = $this->insight_dao->getAllInstanceInsights($count,$page);
-
-        // Insights based on parameters entered by the user to be added.
+        $data = $this->insight_dao->getInsightsForInstancesInRange($this->from, $this->until,
+        $this->count, $this->page, false);
 
         $this->setJsonData($data);
         return $this->generateView();
