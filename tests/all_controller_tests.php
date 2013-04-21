33ce61ee28eef243591dd97079ac410503706ff7
nilakshdas <nilakshdas@gmail.com>
2013-04-21 23:24:17 +0530

    Fixes #1551 - Added HTML 5 notifications feature

--- tests/all_controller_tests.php
+++ tests/all_controller_tests.php
@@ -41,7 +41,6 @@ $controller_test_list = array(
 "TestOfCrawlerAuthController",
 "TestOfDashboardController",
 "TestOfInsightStreamController",
-"TestOfInsightAPIController",
 "TestOfThinkUpEmbedController",
 "TestOfThreadJSController",
 "TestOfExportController",
