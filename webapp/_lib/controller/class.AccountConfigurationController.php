ThinkUpLLC/ThinkUp

commit 9e3494d3740a0e1bcdfa24acb9a7f8987f57b269

Disallow phrases in Twitter saved search closes #1598

webapp/_lib/controller/class.AccountConfigurationController.php
@@ -241,22 +241,28 @@ class AccountConfigurationController extends ThinkUpAuthController {
             $instance_id = $_POST['instance_id'];
             $new_hashtag_name=$_POST['new_hashtag_name'];
 
-            $instance_dao = DAOFactory::getDAO('InstanceDAO');
-            $instance = $instance_dao->get($instance_id);
-            if ( isset($instance) ) {
-                $hashtag = $hashtag_dao->getHashtag($new_hashtag_name, $instance->network);
-                if (!isset($hashtag)) {
-                    $hashtag_id = $hashtag_dao->insertHashtag($new_hashtag_name, $instance->network);
-                    $row_inserted = $instancehashtag_dao->insert($instance_id, $hashtag_id);
-                    $message = "Saved search for " . $new_hashtag_name . ".";
-                    $this->addSuccessMessage($message,'account');
+            //Check if $new_hashtag_name is an individual word (no spaces)
+            if (strpos(" ", $new_hashtag_name) !== false) {
+                $instance_dao = DAOFactory::getDAO('InstanceDAO');
+                $instance = $instance_dao->get($instance_id);
+                if ( isset($instance) ) {
+                    $hashtag = $hashtag_dao->getHashtag($new_hashtag_name, $instance->network);
+                    if (!isset($hashtag)) {
+                        $hashtag_id = $hashtag_dao->insertHashtag($new_hashtag_name, $instance->network);
+                        $row_inserted = $instancehashtag_dao->insert($instance_id, $hashtag_id);
+                        $message = "Saved search for " . $new_hashtag_name . ".";
+                        $this->addSuccessMessage($message,'account');
+                    } else {
+                        $row_inserted = $instancehashtag_dao->insert($instance_id, $hashtag->id);
+                        $message = "Saved search for " . $new_hashtag_name . ".";
+                        $this->addSuccessMessage($message,'account');
+                    }
                 } else {
-                    $row_inserted = $instancehashtag_dao->insert($instance_id, $hashtag->id);
-                    $message = "Saved search for " . $new_hashtag_name . ".";
-                    $this->addSuccessMessage($message,'account');
+                    $this->addErrorMessage('Instance doesn\'t exist.','account');
                 }
             } else {
-                $this->addErrorMessage('Instance doesn\'t exist.','account');
+                $this->addErrorMessage('You can only search for an individual keyword or hashtag, not a phrase. '.
+                'Please try again.','account');
             }
         }
         $this->view_mgr->clear_all_cache();

webapp/plugins/twitter/view/twitter.account.hashtag.tpl
@@ -57,7 +57,11 @@
       <div class="controls">
         {insert name="csrf_token"}
         <input type="hidden" name="instance_id" value="{$instance->id}">
-        <input name="new_hashtag_name" type="new_hashtag_name" id="new_hashtag_name">
+        <input name="new_hashtag_name" type="new_hashtag_name" id="new_hashtag_name" {literal}pattern="^[\S]*$"{/literal}  required 
+        data-validation-required-message="<i class='icon-exclamation-sign'></i>Please enter a keyword or hashtag."
+        data-validation-pattern-message="<i class='icon-exclamation-sign'></i>Enter an individual keyword or hashtag, not a phrase.">
+        <span class="help-block"></span>
+        
       </div>
     </div>
     <div class="control-group">

commit 4be776e02a74d46ec917522c31fc6c2db867c3d5

Fix bug introduced in 9e3494d3740a0e1bcdfa24acb9a7f8987f57b269

webapp/_lib/controller/class.AccountConfigurationController.php
@@ -242,7 +242,7 @@ class AccountConfigurationController extends ThinkUpAuthController {
             $new_hashtag_name=$_POST['new_hashtag_name'];
 
             //Check if $new_hashtag_name is an individual word (no spaces)
-            if (strpos(" ", $new_hashtag_name) !== false) {
+            if (strpos($new_hashtag_name, " ") === false) {
                 $instance_dao = DAOFactory::getDAO('InstanceDAO');
                 $instance = $instance_dao->get($instance_id);
                 if ( isset($instance) ) {

tests/TestOfAccountConfigurationController.php
@@ -834,7 +834,8 @@ class TestOfAccountConfigurationController extends ThinkUpUnitTestCase {
         $_POST['csrf_token'] = parent::CSRF_TOKEN;
 
         $controller = new AccountConfigurationController(true);
-        $controller->go();
+        $results = $controller->go();
+        $this->debug($results);
 
         $hashtag = $hashtag_dao->getHashtag($_POST['new_hashtag_name'], 'twitter');
         $this->assertNotNull($hashtag);