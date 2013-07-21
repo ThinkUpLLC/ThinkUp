80eaf607e4efecff1b1848c3e56ce1d3df4bf897
John Eckman <eckman.john@gmail.com>
2013-07-21 16:39:20 -0400

    Foursquare plugin: Fix photo processing Foursquare API sometimes returns suffix and prefix, sometimes just photo Closes #1650

--- webapp/plugins/foursquare/model/class.FoursquareCrawler.php
+++ webapp/plugins/foursquare/model/class.FoursquareCrawler.php
@@ -292,8 +292,12 @@ class FoursquareCrawler {
                 // The author full name is the name they gave foursquare
                 $post['author_fullname'] = $user->response->user->firstName." ".$user->response->user->lastName;
                 // The avatar is the one they have set on foursquare
-                $post["author_avatar"] = $user->response->user->photo->prefix . "100x100" .
-                $user->response->user->photo->suffix;
+                if (isset($user->response->user->photo->prefix) && isset($user->response->user->photo->suffix)) {
+                    $post["author_avatar"] = $user->response->user->photo->prefix . "100x100" .
+                    $user->response->user->photo->suffix;
+                } elseif (isset($user->response->user->photo)) { //sometimes just photo is set, not prefix and suffix
+                    $post["author_avatar"] = $user->response->user->photo;
+                }
 
                 // The author user id is there foursquare user ID
                 $post['author_user_id'] = $user->response->user->id;
@@ -368,9 +372,12 @@ class FoursquareCrawler {
                         // The author full name is the name they gave foursquare
                         $comment_store['author_fullname'] = $comment->user->firstName." ".$comment->user->lastName;
                         // The avatar is the one they have set on foursquare
-                        $comment_store["author_avatar"] = $comment->user->photo->prefix . "100x100" .
-                        $comment->user->photo->suffix;
-
+                        if (isset($comment->user->photo->prefix) && isset($comment->user->photo->suffix)) {
+                            $comment_store["author_avatar"] = $comment->user->photo->prefix . "100x100" .
+                            $comment->user->photo->suffix;
+                        } elseif (isset($comment->user->photo)) { //sometimes just photo is set, not prefix and suffix
+                            $comment["author_avatar"] = $comment->user->photo;
+                        }
                         // The author user id is there foursquare user ID
                         $comment_store['author_user_id'] = $comment->user->id;
                         // The date they posted the comment
