6a7e222f129c75fdd8028dee1ffbebdbd1459e11
Aaron Kalair <aaronkalair@gmail.com>
2013-07-22 10:46:59 +0100

    Subscriber change insight

---
+++ webapp/plugins/insightsgenerator/insights/subscriberchange.php
@@ -0,0 +1,87 @@
+<?php
+/*
+ Plugin Name: Subscriber Change
+ Description: How videos affected your subscriber count
+ */
+
+/**
+ *
+ * ThinkUp/webapp/plugins/insightsgenerator/insights/subscriberchange.php
+ *
+ * Copyright (c) 2012-2013 Aaron Kalair
+ *
+ * LICENSE:
+ *
+ * This file is part of ThinkUp (http://thinkup.com).
+ *
+ * ThinkUp is free software: you can redistribute it and/or modify it under the terms of the GNU General Public
+ * License as published by the Free Software Foundation, either version 2 of the License, or (at your option) any
+ * later version.
+ *
+ * ThinkUp is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied
+ * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License along with ThinkUp.  If not, see
+ * <http://www.gnu.org/licenses/>.
+ *
+ * @license http://www.gnu.org/licenses/gpl.html
+ * @copyright 2012-2013 Aaron Kalair
+ * @author Aaron Kalair <aaronkalair [at] gmail [dot] com>
+ */
+
+class SubscriberChangeInsight extends InsightPluginParent implements InsightPlugin {
+
+    public function generateInsight(Instance $instance, $last_weeks_posts, $number_days) {
+        parent::generateInsight($instance, $last_week_of_posts, $number_days);
+        $this->logger->logInfo("Begin generating insight", __METHOD__.','.__LINE__);
+        $filename = basename(__FILE__, ".php");
+
+        $video_dao = DAOFactory::getDAO('VideoDAO');
+
+        // Get the users subscriber count for comparing with later
+        $user_dao = DAOFactory::getDAO('UserDAO');
+        $user = $user_dao->getDetails($instance->network_user_id, 'youtube');
+        $subscriber_count = $user->follower_count;
+
+        foreach($last_weeks_posts as $post) {
+
+            if($post->network == 'youtube') {
+                $video =  $video_dao->getVideoByID($post->post_id, 'youtube');
+                $simplified_post_date = date('Y-m-d', strtotime($video->pub_date));
+            } else {
+                break;
+            }
+
+            $gain_or_loss = $video->subscribers_gained - $video->subscribers_lost;
+            // if we lost subscribers then we will be doing subscriber_count minus a negative number, so adding
+            $total_before_video = $subscriber_count - $gain_or_loss;
+            $percent_change = round((abs($gain_or_loss) / $total_before_video) * 100,2);
+            $prefix = ($gain_or_loss < 0) ? 'They\'re fleeing' : 'They\'re sticking around';
+            $verb = ($gain_or_loss < 0) ? ' decreased' : ' increased';
+            $text = $video->post_text.$verb." your subscriber count by ".$percent_change."%";
+            $subscriber_count = intval($subscriber_count);
+            $total_before_video = intval($total_before_video);
+            $chart = "{cols: [{label: 'metric', type: 'string'}, {label: 'Subscribers', type: 'number'}],";
+            $chart .= "rows: [{c:[{v:'Before'}, {v:$total_before_video} ] },";
+            $chart .= "{c:[{v:'After'}, {v:$subscriber_count} ] } ] }";
+
+            if($percent_change >=50) {
+                $this->insight_dao->insertInsight('subscriber_change'.$video->id, $instance->id,
+                $simplified_post_date, $prefix, $text, $filename, Insight::EMPHASIS_HIGH,
+                serialize(array($chart,$video)));
+            } elseif($percent_change >=25 ) {
+                $this->insight_dao->insertInsight('subscriber_change'.$video->id, $instance->id,
+                $simplified_post_date, $prefix, $text, $filename, Insight::EMPHASIS_MED,
+                serialize(array($chart,$video)));
+            } elseif($percent_change >=10 ) {
+                $this->insight_dao->insertInsight('subscriber_change'.$video->id, $instance->id,
+                $simplified_post_date, $prefix, $text, $filename, Insight::EMPHASIS_LOW,
+                serialize(array($chart,$video)));
+            }
+        }
+    }
+}
+
+$insights_plugin_registrar = PluginRegistrarInsights::getInstance();
+$insights_plugin_registrar->registerInsightPlugin('SubscriberChangeInsight');
---
+++ webapp/plugins/insightsgenerator/tests/TestOfSubscriberChangeInsight.php
@@ -0,0 +1,230 @@
+<?php
+/**
+ *
+ * ThinkUp/webapp/plugins/insightsgenerator/tests/TestOfSubscriberChangeInsight.php
+ *
+ * Copyright (c) 2012-2013 Aaron Kalair
+ *
+ * LICENSE:
+ *
+ * This file is part of ThinkUp (http://thinkup.com).
+ *
+ * ThinkUp is free software: you can redistribute it and/or modify it under the terms of the GNU General Public
+ * License as published by the Free Software Foundation, either version 2 of the License, or (at your option) any
+ * later version.
+ *
+ * ThinkUp is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied
+ * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License along with ThinkUp.  If not, see
+ * <http://www.gnu.org/licenses/>.
+ *
+ * Test of SubscriberChangeInsight
+ *
+ * Test for the SubscriberChangeInsight class.
+ *
+ * @license http://www.gnu.org/licenses/gpl.html
+ * @copyright 2012-2013 Gina Trapani
+ * @author Aaron Kalair <aaronkalair[at]gmail[dot]com>
+ */
+
+require_once dirname(__FILE__) . '/../../../../tests/init.tests.php';
+require_once THINKUP_WEBAPP_PATH.'_lib/extlib/simpletest/autorun.php';
+require_once THINKUP_WEBAPP_PATH.'_lib/extlib/simpletest/web_tester.php';
+require_once THINKUP_ROOT_PATH. 'webapp/plugins/insightsgenerator/model/class.InsightPluginParent.php';
+require_once THINKUP_ROOT_PATH. 'webapp/plugins/insightsgenerator/insights/subscriberchange.php';
+
+class TestOfSubscriberChangeInsight extends ThinkUpUnitTestCase {
+
+    public function setUp(){
+        parent::setUp();
+    }
+
+    public function tearDown() {
+        parent::tearDown();
+    }
+
+    public function testHighIncrease() {
+        $user = self::buildData();
+        // Insert a video that would have raised the subscriber count by >= 50%
+        $post_builder = FixtureBuilder::build('posts', array('id'=>1, 'post_id'=>'1', 'author_username'=>'ev',
+        'post_text'=>'My Great Video', 'pub_date'=>'-1d', 'network'=>'youtube'));
+        $video_builder = FixtureBuilder::build('videos', array('id'=>1, 'post_key'=>'1', 'subscribers_gained'=>9,
+        'subscribers_lost'=>0, 'average_view_percentage'=>10));
+
+        $instance = new Instance();
+        $instance->id = 1;
+        $instance->network = 'youtube';
+        $instance->network_username = 'ev';
+        $instance->network_user_id = 1;
+
+        $insight = new SubscriberChangeInsight();
+        $posts[] = new Post($post_builder->columns);
+        $insight->generateInsight($instance, $posts, 7);
+
+        // Assert that insight got inserted
+        $insight_dao = new InsightMySQLDAO();
+        $today = date ('Y-m-d');
+        $result = $insight_dao->getInsight('subscriber_change1', 1, date('Y-m-d', strtotime('-1 day')));
+        $this->assertNotNull($result);
+        $this->assertIsA($result, "Insight");
+        $this->assertEqual($result->prefix, "They're sticking around");
+        $this->assertEqual($result->text, "My Great Video increased your subscriber count by 81.82%");
+        $this->assertEqual($result->emphasis, 2);
+    }
+
+    public function testMediumIncrease() {
+        $user = self::buildData();
+        // Insert a video that would have raised the subscriber count by >= 25%
+        $post_builder = FixtureBuilder::build('posts', array('id'=>1, 'post_id'=>'1', 'author_username'=>'ev',
+        'post_text'=>'My Great Video', 'pub_date'=>'-1d', 'network'=>'youtube'));
+        $video_builder = FixtureBuilder::build('videos', array('id'=>1, 'post_key'=>'1', 'subscribers_gained'=>4,
+        'subscribers_lost'=>0, 'average_view_percentage'=>10));
+
+        $instance = new Instance();
+        $instance->id = 1;
+        $instance->network = 'youtube';
+        $instance->network_username = 'ev';
+        $instance->network_user_id = 1;
+
+        $insight = new SubscriberChangeInsight();
+        $posts[] = new Post($post_builder->columns);
+        $insight->generateInsight($instance, $posts, 7);
+
+        // Assert that insight got inserted
+        $insight_dao = new InsightMySQLDAO();
+        $today = date ('Y-m-d');
+        $result = $insight_dao->getInsight('subscriber_change1', 1, date('Y-m-d', strtotime('-1 day')));
+        $this->assertNotNull($result);
+        $this->assertIsA($result, "Insight");
+        $this->assertEqual($result->prefix, "They're sticking around");
+        $this->assertEqual($result->text, "My Great Video increased your subscriber count by 25%");
+        $this->assertEqual($result->emphasis, 1);
+    }
+
+    public function testLowIncrease() {
+        $user = self::buildData();
+        // Insert a video that would have raised the subscriber count by >= 10%
+        $post_builder = FixtureBuilder::build('posts', array('id'=>1, 'post_id'=>'1', 'author_username'=>'ev',
+        'post_text'=>'My Great Video', 'pub_date'=>'-1d', 'network'=>'youtube'));
+        $video_builder = FixtureBuilder::build('videos', array('id'=>1, 'post_key'=>'1', 'subscribers_gained'=>3,
+        'subscribers_lost'=>0, 'average_view_percentage'=>10));
+
+        $instance = new Instance();
+        $instance->id = 1;
+        $instance->network = 'youtube';
+        $instance->network_username = 'ev';
+        $instance->network_user_id = 1;
+
+        $insight = new SubscriberChangeInsight();
+        $posts[] = new Post($post_builder->columns);
+        $insight->generateInsight($instance, $posts, 7);
+
+        // Assert that insight got inserted
+        $insight_dao = new InsightMySQLDAO();
+        $today = date ('Y-m-d');
+        $result = $insight_dao->getInsight('subscriber_change1', 1, date('Y-m-d', strtotime('-1 day')));
+        $this->assertNotNull($result);
+        $this->assertIsA($result, "Insight");
+        $this->assertEqual($result->prefix, "They're sticking around");
+        $this->assertEqual($result->text, "My Great Video increased your subscriber count by 17.65%");
+        $this->assertEqual($result->emphasis, 0);
+    }
+
+    public function testHighDecrease() {
+        $user = self::buildData();
+        // Insert a video that would have lowered the subscriber count by >= 50%
+        $post_builder = FixtureBuilder::build('posts', array('id'=>1, 'post_id'=>'1', 'author_username'=>'ev',
+        'post_text'=>'My Great Video', 'pub_date'=>'-1d', 'network'=>'youtube'));
+        $video_builder = FixtureBuilder::build('videos', array('id'=>1, 'post_key'=>'1', 'subscribers_gained'=>0,
+        'subscribers_lost'=>20, 'average_view_percentage'=>10));
+
+        $instance = new Instance();
+        $instance->id = 1;
+        $instance->network = 'youtube';
+        $instance->network_username = 'ev';
+        $instance->network_user_id = 1;
+
+        $insight = new SubscriberChangeInsight();
+        $posts[] = new Post($post_builder->columns);
+        $insight->generateInsight($instance, $posts, 7);
+
+        // Assert that insight got inserted
+        $insight_dao = new InsightMySQLDAO();
+        $today = date ('Y-m-d');
+        $result = $insight_dao->getInsight('subscriber_change1', 1, date('Y-m-d', strtotime('-1 day')));
+        $this->assertNotNull($result);
+        $this->assertIsA($result, "Insight");
+        $this->assertEqual($result->prefix, "They're fleeing");
+        $this->assertEqual($result->text, "My Great Video decreased your subscriber count by 50%");
+        $this->assertEqual($result->emphasis, 2);
+    }
+
+    public function testMediumDecrease() {
+        $user = self::buildData();
+        // Insert a video that would have lowered the subscriber count by >= 50%
+        $post_builder = FixtureBuilder::build('posts', array('id'=>1, 'post_id'=>'1', 'author_username'=>'ev',
+        'post_text'=>'My Great Video', 'pub_date'=>'-1d', 'network'=>'youtube'));
+        $video_builder = FixtureBuilder::build('videos', array('id'=>1, 'post_key'=>'1', 'subscribers_gained'=>0,
+        'subscribers_lost'=>9, 'average_view_percentage'=>10));
+
+        $instance = new Instance();
+        $instance->id = 1;
+        $instance->network = 'youtube';
+        $instance->network_username = 'ev';
+        $instance->network_user_id = 1;
+
+        $insight = new SubscriberChangeInsight();
+        $posts[] = new Post($post_builder->columns);
+        $insight->generateInsight($instance, $posts, 7);
+
+        // Assert that insight got inserted
+        $insight_dao = new InsightMySQLDAO();
+        $today = date ('Y-m-d');
+        $result = $insight_dao->getInsight('subscriber_change1', 1, date('Y-m-d', strtotime('-1 day')));
+        $this->assertNotNull($result);
+        $this->assertIsA($result, "Insight");
+        $this->assertEqual($result->prefix, "They're fleeing");
+        $this->assertEqual($result->text, "My Great Video decreased your subscriber count by 31.03%");
+        $this->assertEqual($result->emphasis, 1);
+    }
+
+    public function testLowDecrease() {
+        $user = self::buildData();
+        // Insert a video that would have lowered the subscriber count by >= 50%
+        $post_builder = FixtureBuilder::build('posts', array('id'=>1, 'post_id'=>'1', 'author_username'=>'ev',
+        'post_text'=>'My Great Video', 'pub_date'=>'-1d', 'network'=>'youtube'));
+        $video_builder = FixtureBuilder::build('videos', array('id'=>1, 'post_key'=>'1', 'subscribers_gained'=>0,
+        'subscribers_lost'=>4, 'average_view_percentage'=>10));
+
+        $instance = new Instance();
+        $instance->id = 1;
+        $instance->network = 'youtube';
+        $instance->network_username = 'ev';
+        $instance->network_user_id = 1;
+
+        $insight = new SubscriberChangeInsight();
+        $posts[] = new Post($post_builder->columns);
+        $insight->generateInsight($instance, $posts, 7);
+
+        // Assert that insight got inserted
+        $insight_dao = new InsightMySQLDAO();
+        $today = date ('Y-m-d');
+        $result = $insight_dao->getInsight('subscriber_change1', 1, date('Y-m-d', strtotime('-1 day')));
+        $this->assertNotNull($result);
+        $this->assertIsA($result, "Insight");
+        $this->assertEqual($result->prefix, "They're fleeing");
+        $this->assertEqual($result->text, "My Great Video decreased your subscriber count by 16.67%");
+        $this->assertEqual($result->emphasis, 0);
+    }
+
+
+    private function buildData() {
+        $builders[] = FixtureBuilder::build('users', array('user_id'=>'1', 'user_name'=>'ev',
+        'full_name'=>'Ev Williams', 'avatar'=>'avatar.jpg', 'is_protected'=>0, 'follower_count'=>20,
+        'network'=>'youtube'));
+        return $builders;
+    }
+
+}
