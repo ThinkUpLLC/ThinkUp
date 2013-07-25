dd2d7267ec292f0db605e023a1dc41ff32812bc5
Aaron Kalair <aaronkalair@gmail.com>
2013-07-25 17:01:17 +0100

    Added video table and supporting classes

---
+++ tests/TestOfVideoMySQLDAO.php
@@ -0,0 +1,226 @@
+<?php
+/**
+ *
+ * ThinkUp/tests/TestOfVideoMySQLDAO.php
+ *
+ * Copyright (c) 2009-2013 Aaron Kalair
+ *
+ * LICENSE:
+ *
+ * This file is part of ThinkUp (http://thinkup.com).
+ *
+ * ThinkUp is free software: you can redistribute it and/or modify it under the terms of the GNU General Public
+ * License as published by the Free Software Foundation, either version 2 of the License, or (at your option) any
+ * later version.
+ *
+ * ThinkUp is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied
+ * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License along with ThinkUp.  If not, see
+ * <http://www.gnu.org/licenses/>.
+ *
+ *
+ * Test of VideoMySQL DAO implementation
+ * @license http://www.gnu.org/licenses/gpl.html
+ * @copyright 2009-2013 Aaron Kalair
+ * @author Aaron Kalair <aaronkalair[at]gmail[dot]com>
+ *
+ */
+require_once dirname(__FILE__).'/init.tests.php';
+require_once THINKUP_WEBAPP_PATH.'_lib/extlib/simpletest/autorun.php';
+require_once THINKUP_WEBAPP_PATH.'config.inc.php';
+
+
+class TestOfVideoMySQLDAO extends ThinkUpUnitTestCase {
+
+    /**
+     * VideoMySQLDAO
+     */
+    protected $dao;
+
+    public function setUp() {
+        $this->dao = new VideoMySQLDAO();
+        parent::setUp();
+        $config = Config::getInstance();
+        $this->builders = self::buildData();
+    }
+
+    public function tearDown() {
+        $this->builders = null;
+        parent::tearDown();
+    }
+
+    protected function buildData() {
+        $builders = array();
+        // Add a video to the database
+        $builders[] = FixtureBuilder::build('posts', array(
+            'id' => 1,
+            'post_id' => 'tfgdf4es',
+            'author_user_id' => '115383827382290096528',
+            'author_username' => 'Aaron Kalair',
+            'author_fullname' => 'Aaron Kalair',
+            'author_avatar' => 'avatar.jpg',
+            'post_text' => 'A random YouTube video',
+            'source' => '',
+            'pub_date' => '2013-03-05 16:45:00',
+            'reply_count_cache' => 204,
+            'network' => 'youtube',
+            'is_protected'=>0
+            ));
+        $builders[] = FixtureBuilder::build('videos', array(
+            'internal_post_id' => 1,
+            'description' => 'This is a youtube video ',
+            'likes' => 10,
+            'dislikes' => 20,
+            'views' => 5452,
+            'minutes_watched' => 125,
+            'average_view_duration' => 50000124,
+            'average_view_percentage' => 41,
+            'favorites_added' => 204,
+            'favorites_removed' => 52,
+            'shares'=>0,
+            'subscribers_gained' =>0,
+            'subscribers_lost' => 2,
+
+            ));
+        return $builders;
+    }
+
+    /**
+     * Test constructor
+     */
+    public function testConstructor() {
+        $dao = new VideoMySQLDAO();
+        $this->assertTrue(isset($dao));
+    }
+
+    public function testAddPost(){
+
+        $video_attributes['description'] = "Watch my youtube video";
+        $video_attributes['post_text'] = "My Great Video";
+        $video_attributes['likes'] = 200;
+        $video_attributes['dislikes'] = 25;
+        $video_attributes['views'] = 54465;
+        $video_attributes['pub_date'] = '2013-12-12 12:01:54';
+        $video_attributes['post_id'] = 'G7fdAh4';
+        $video_attributes['location'] = 'England';
+        $video_attributes['place'] = 'England';
+        $video_attributes['geo'] = '54.342, -5.65';
+        $video_attributes['reply_count_cache'] = 20;
+        $video_attributes['is_protected'] = 0;
+        $video_attributes['favorites_added'] = 12;
+        $video_attributes['favorites_removed'] = 2;
+        $video_attributes['shares'] = 52;
+        $video_attributes['subscribers_gained'] = 59;
+        $video_attributes['subscribers_lost'] = 1;
+        $video_attributes['minutes_watched'] = 850;
+        $video_attributes['average_view_duration'] = 50000;
+        $video_attributes['average_view_percentage'] = 25;
+        $video_attributes['author_user_id'] = '4544554545710124';
+        $video_attributes['author_username'] = 'Aaron Kalair';
+        $video_attributes['author_fullname'] = 'Aaron Kalair';
+        $video_attributes['author_avatar'] = 'http://www.google.com/avatars/mgfgfd';
+        $video_attributes['source'] = '';
+        $video_attributes['network'] = 'youtube';
+
+        $this->dao->addVideo($video_attributes);
+
+        // Check all the values were added
+        $video = $this->dao->getVideoByID('G7fdAh4', 'youtube');
+        $this->assertTrue(isset($video));
+        $this->assertEqual($video->post_id, 'G7fdAh4');
+        $this->assertEqual($video->description, 'Watch my youtube video');
+        $this->assertEqual($video->post_text, 'My Great Video');
+        $this->assertEqual($video->likes, 200);
+        $this->assertEqual($video->dislikes, 25);
+        $this->assertEqual($video->views, 54465);
+        $this->assertEqual($video->pub_date, '2013-12-12 12:01:54');
+        $this->assertEqual($video->location, 'England');
+        $this->assertEqual($video->place, 'England');
+        $this->assertEqual($video->geo, '54.342, -5.65');
+        $this->assertEqual($video->reply_count_cache, 20);
+        $this->assertEqual($video->is_protected, 0);
+        $this->assertEqual($video->favorites_added, 12);
+        $this->assertEqual($video->favorites_removed, 2);
+        $this->assertEqual($video->shares, 52);
+        $this->assertEqual($video->subscribers_gained, 59);
+        $this->assertEqual($video->subscribers_lost, 1);
+        $this->assertEqual($video->minutes_watched, 850);
+        $this->assertEqual($video->average_view_duration, 50000);
+        $this->assertEqual($video->average_view_percentage, 25);
+        $this->assertEqual($video->author_user_id, '4544554545710124');
+        $this->assertEqual($video->author_username, 'Aaron Kalair');
+        $this->assertEqual($video->author_fullname, 'Aaron Kalair');
+        $this->assertEqual($video->author_avatar, 'http://www.google.com/avatars/mgfgfd');
+        $this->assertEqual($video->source, '');
+        $this->assertEqual($video->network, 'youtube');
+    }
+
+    public function testGetVideoByIDExists() {
+        $video = $this->dao->getVideoByID('tfgdf4es', 'youtube');
+        $this->assertTrue(isset($video));
+        $this->assertEqual($video->post_id, 'tfgdf4es');
+        $this->assertEqual($video->description, 'This is a youtube video ');
+        $this->assertEqual($video->post_text, 'A random YouTube video');
+        $this->assertEqual($video->likes, 10);
+        $this->assertEqual($video->dislikes, 20);
+        $this->assertEqual($video->views, 5452);
+        $this->assertEqual($video->pub_date, '2013-03-05 16:45:00');
+        $this->assertEqual($video->reply_count_cache, 204);
+        $this->assertEqual($video->is_protected, 0);
+        $this->assertEqual($video->favorites_added, 204);
+        $this->assertEqual($video->favorites_removed, 52);
+        $this->assertEqual($video->shares, 0);
+        $this->assertEqual($video->subscribers_gained, 0);
+        $this->assertEqual($video->subscribers_lost, 2);
+        $this->assertEqual($video->minutes_watched, 125);
+        $this->assertEqual($video->average_view_duration, 50000124);
+        $this->assertEqual($video->average_view_percentage, 41);
+        $this->assertEqual($video->author_user_id, '115383827382290096528');
+        $this->assertEqual($video->author_username, 'Aaron Kalair');
+        $this->assertEqual($video->author_fullname, 'Aaron Kalair');
+        $this->assertEqual($video->author_avatar, 'avatar.jpg');
+        $this->assertEqual($video->source, '');
+        $this->assertEqual($video->network, 'youtube');
+    }
+
+    public function testGetVideoByIDDoesNotExist(){
+        $video = $this->dao->getVideoByID(100000001, 'youtube');
+        $this->assertTrue(!isset($video));
+    }
+
+    public function testUpdateVideoCounts(){
+        $video_attributes['post_id'] = 'tfgdf4es';
+        $video_attributes['likes'] = 25;
+        $video_attributes['dislikes'] = 40;
+        $video_attributes['views'] = 8545;
+        $video_attributes['favorites_added'] = 500;
+        $video_attributes['favorites_removed'] = 245;
+        $video_attributes['shares'] = 52;
+        $video_attributes['subscribers_gained'] = 40;
+        $video_attributes['subscribers_lost'] = 100;
+        $video_attributes['minutes_watched'] = 824;
+        $video_attributes['average_view_duration'] = 50004110;
+        $video_attributes['average_view_percentage'] = 65;
+        // Update the videos values
+        $this->assertEqual($this->dao->updateVideoCounts($video_attributes), 1);
+
+        // Check they were updated
+        $video = $this->dao->getVideoByID('tfgdf4es', 'youtube');
+        $this->assertEqual($video->likes, 25);
+        $this->assertEqual($video->dislikes, 40);
+        $this->assertEqual($video->views, 8545);
+        $this->assertEqual($video->favorites_added, 500);
+        $this->assertEqual($video->favorites_removed, 245);
+        $this->assertEqual($video->shares, 52);
+        $this->assertEqual($video->subscribers_gained, 40);
+        $this->assertEqual($video->subscribers_lost, 100);
+        $this->assertEqual($video->minutes_watched, 824);
+        $this->assertEqual($video->average_view_duration, 50004110);
+        $this->assertEqual($video->average_view_percentage, 65);
+
+    }
+
+
+}
---
+++ webapp/_lib/dao/class.VideoMySQLDAO.php
@@ -0,0 +1,136 @@
+<?php
+/**
+ *
+ * ThinkUp/webapp/_lib/model/class.VideoMySQLDAO.php
+ *
+ * Copyright (c) 2009-2013 Aaron Kalair
+ *
+ * LICENSE:
+ *
+ * This file is part of ThinkUp (http://thinkup.com).
+ *
+ * ThinkUp is free software: you can redistribute it and/or modify it under the terms of the GNU General Public
+ * License as published by the Free Software Foundation, either version 2 of the License, or (at your option) any
+ * later version.
+ *
+ * ThinkUp is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied
+ * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License along with ThinkUp.  If not, see
+ * <http://www.gnu.org/licenses/>.
+ *
+ *
+ * Video Data Access Object
+ *
+ * The data access object for retrieving and saving videos in the ThinkUp database
+ *
+ * @license http://www.gnu.org/licenses/gpl.html
+ * @copyright 2009-2013 Aaron Kalair
+ * @author Aaron Kalair <aaronkalair[at]gmail[dot]com>
+ */
+class VideoMySQLDAO extends PostMySQLDAO implements VideoDAO  {
+
+    /**
+     * Fields required for a video.
+     * @var array
+     */
+    var $REQUIRED_VIDEO_FIELDS =  array('likes','dislikes','views','minutes_watched','average_view_duration',
+    'average_view_percentage', 'favorites_added', 'favorites_removed', 'shares', 'subscribers_gained',
+    'subscribers_lost', 'description');
+
+    /**
+     * Checks to see if the $vals array contains all the required fields to insert a post
+     * @param array $vals
+     * @return bool
+     */
+    private function hasAllRequiredFields($vals) {
+        $result = true;
+        foreach ($this->REQUIRED_VIDEO_FIELDS as $field) {
+            if ( !isset($vals[$field]) ) {
+                $this->logger->logError("Missing post $field value", __METHOD__.','.__LINE__);
+                $result = false;
+            }
+        }
+        return $result;
+    }
+
+    public function addVideo($vals) {
+        $res = null;
+        // Check all the fields we need are set
+        if(self::hasAllRequiredFields($vals)) {
+
+            // Insert the values which go into the post table
+            $row_id = parent::addPost($vals);
+
+            // If the post insertion went fine insert the values that go into the videos table
+            if($row_id != null) {
+                //SQL variables to bind
+                $vars = array();
+                //SQL query
+                $q = "INSERT IGNORE INTO #prefix#videos SET ";
+                //Set up required fields
+                foreach ($this->REQUIRED_VIDEO_FIELDS as $field) {
+                    $q .= $field."=:".$field.", ";
+                    $vars[':'.$field] = $vals[$field];
+                }
+
+                // Append the internal post ID
+                $q .= 'internal_post_id=:internal_post_id';
+                $vars[':internal_post_id'] = $row_id;
+
+                // Try to insert the video
+                if ($this->profiler_enabled) { Profiler::setDAOMethod(__METHOD__); }
+                $ps = $this->execute($q, $vars);
+                $res = $this->getInsertId($ps);
+            } else {
+                // If the video was already in the database, update the counts
+                $res = self::updateVideoCounts($vals);
+            }
+        }
+        return $res;
+    }
+
+    public function updateVideoCounts($vals) {
+        $q = "UPDATE #prefix#videos SET ";
+        foreach ($this->REQUIRED_VIDEO_FIELDS as $field) {
+            $q .= $field."=:".$field.", ";
+            $vars[':'.$field] = $vals[$field];
+        }
+
+        // remove the final , and space
+        $q = substr($q, 0, (strlen($q)-2));
+
+        // Find out the internal ID for this post so we know which row to update
+        $post_dao = DAOFactory::getDAO('PostDAO');
+        $post_row = $post_dao->getPost($vals['post_id'], 'youtube');
+        // Set the video to update
+        $q .= " WHERE internal_post_id=:post_id";
+        $vars[':post_id'] = $post_row->id;
+        // Update it
+        $ps = $this->execute($q, $vars);
+        return $this->getUpdateCount($ps);
+    }
+
+    public function getVideoByID($video_id, $network) {
+        $q = "SELECT po.id, po.post_id, po.author_user_id, po.author_username, po.author_fullname, po.author_avatar, ";
+        $q .= "po.post_text, po.is_protected, po.location, po.place, po.geo, po.pub_date, po.reply_count_cache, ";
+        $q .= "po.network, vid.id, vid.internal_post_id, vid.description, vid.likes, vid.dislikes, vid.views, ";
+        $q .= "vid.minutes_watched, vid.average_view_duration, vid.average_view_percentage, vid.favorites_added, ";
+        $q .= "vid.favorites_removed, vid.shares, vid.subscribers_gained, vid.subscribers_lost ";
+        $q .= "FROM #prefix#posts po JOIN #prefix#videos vid ON po.id = vid.internal_post_id ";
+        $q .= "WHERE po.post_id=:video_id AND po.network=:network";
+        $vars = array(
+            ':video_id'=>$video_id,
+            ':network'=>$network
+            );
+        $ps = $this->execute($q, $vars);
+        $post_row = $this->getDataRowAsArray($ps);
+        if($post_row) {
+            return new Video($post_row);
+        }
+        else{
+            return null;
+        }
+    }
+}
