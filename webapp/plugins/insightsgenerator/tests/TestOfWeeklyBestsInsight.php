d4d22034d7c57cc98848437ca1f1b20bd018d231
nilakshdas <nilakshdas@gmail.com>
2013-07-31 05:13:32 +0530

    New Weekly Bests Insight Closes #1622, closes #1661

---
+++ webapp/plugins/insightsgenerator/tests/TestOfWeeklyBestsInsight.php
@@ -0,0 +1,216 @@
+<?php
+/**
+ *
+ * ThinkUp/webapp/plugins/insightsgenerator/tests/TestOfWeeklyBestsInsight.php
+ *
+ * Copyright (c) 2013 Nilaksh Das, Gina Trapani
+ *
+ * LICENSE:
+ *
+ * This file is part of ThinkUp (http://thinkup.com).
+ *
+ * ThinkUp is free software: you can redistribute it and/or modify it under the terms of the GNU General Public
+ * License as published by the Free Software Foundation, either version 2 of the License, or (at your option) any
+ * later version.
+ *
+ * ThinkUp is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied
+ * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License along with ThinkUp.  If not, see
+ * <http://www.gnu.org/licenses/>.
+ *
+ * Test of WeeklyBestsInsight
+ *
+ * Test for the WeeklyBestsInsight class.
+ *
+ * @license http://www.gnu.org/licenses/gpl.html
+ * @copyright 2013 Nilaksh Das, Gina Trapani
+ * @author Nilaksh Das <nilakshdas [at] gmail [dot] com>
+ */
+
+require_once dirname(__FILE__) . '/../../../../tests/init.tests.php';
+require_once THINKUP_WEBAPP_PATH.'_lib/extlib/simpletest/autorun.php';
+require_once THINKUP_WEBAPP_PATH.'_lib/extlib/simpletest/web_tester.php';
+require_once THINKUP_ROOT_PATH. 'webapp/plugins/insightsgenerator/model/class.InsightPluginParent.php';
+require_once THINKUP_ROOT_PATH. 'webapp/plugins/insightsgenerator/insights/weeklybests.php';
+
+class TestOfWeeklyBestsInsight extends ThinkUpUnitTestCase {
+
+    public function setUp(){
+        parent::setUp();
+    }
+
+    public function tearDown() {
+        parent::tearDown();
+    }
+
+    public function testWeeklyBestsInsightForTwitter() {
+        // Get data ready that insight requires
+        $instance = new Instance();
+        $instance->id = 10;
+        $instance->network_username = 'testeriffic';
+        $instance->network = 'twitter';
+
+        $posts = array();
+        $posts[] = new Post(array(
+            'reply_count_cache' => 5,
+            'retweet_count_cache' => 1,
+            'favlike_count_cache' => 3
+        )); // popularity_index = 34
+        $posts[] = new Post(array(
+            'reply_count_cache' => 0,
+            'retweet_count_cache' => 1,
+            'favlike_count_cache' => 15
+        )); // popularity_index = 33
+        $posts[] = new Post(array(
+            'reply_count_cache' => 2,
+            'retweet_count_cache' => 5,
+            'favlike_count_cache' => 1
+        )); // popularity_index = 27
+
+        $insight_plugin = new WeeklyBestsInsight();
+        $insight_plugin->generateInsight($instance, $posts, 3);
+
+        // Assert that insight got inserted
+        $insight_dao = new InsightMySQLDAO();
+        $today = date ('Y-m-d');
+        $result = $insight_dao->getInsight('weekly_best', 10, $today);
+        $this->debug(Utils::varDumpToString($result));
+        $this->assertNotNull($result);
+        $this->assertIsA($result, "Insight");
+        $this->assertPattern('/\@testeriffic\'s most popular post from last week got /', $result->text);
+        $this->assertPattern('/5 replies/', $result->text);
+        $this->assertPattern('/1 retweet/', $result->text);
+        $this->assertPattern('/3 favorites/', $result->text);
+    }
+
+    public function testWeeklyBestsInsightForFacebook() {
+        // Get data ready that insight requires
+        $instance = new Instance();
+        $instance->id = 10;
+        $instance->network_username = 'tester_fb';
+        $instance->network = 'facebook';
+
+        $posts = array();
+        $posts[] = new Post(array(
+            'reply_count_cache' => 8,
+            'favlike_count_cache' => 3
+        )); // popularity_index = 46
+        $posts[] = new Post(array(
+            'reply_count_cache' => 0,
+            'favlike_count_cache' => 15
+        )); // popularity_index = 30
+        $posts[] = new Post(array(
+            'reply_count_cache' => 2,
+            'favlike_count_cache' => 1
+        )); // popularity_index = 12
+
+        $insight_plugin = new WeeklyBestsInsight();
+        $insight_plugin->generateInsight($instance, $posts, 3);
+
+        // Assert that insight got inserted
+        $insight_dao = new InsightMySQLDAO();
+        $today = date ('Y-m-d');
+        $result = $insight_dao->getInsight('weekly_best', 10, $today);
+        $this->debug(Utils::varDumpToString($result));
+        $this->assertNotNull($result);
+        $this->assertIsA($result, "Insight");
+        $this->assertPattern('/tester_fb\'s most popular post from last week got /', $result->text);
+        $this->assertPattern('/8 comments/', $result->text);
+        $this->assertPattern('/3 likes/', $result->text);
+    }
+
+    public function testWeeklyBestsInsightWithOneReply() {
+        // Get data ready that insight requires
+        $instance = new Instance();
+        $instance->id = 10;
+        $instance->network_username = 'testeriffic';
+        $instance->network = 'twitter';
+
+        $posts = array();
+        $posts[] = new Post(array(
+            'reply_count_cache' => 1,
+            'retweet_count_cache' => 0,
+            'favlike_count_cache' => 0
+        )); // popularity_index = 5
+
+        $insight_plugin = new WeeklyBestsInsight();
+        $insight_plugin->generateInsight($instance, $posts, 3);
+
+        // Assert that insight got inserted
+        $insight_dao = new InsightMySQLDAO();
+        $today = date ('Y-m-d');
+        $result = $insight_dao->getInsight('weekly_best', 10, $today);
+        $this->debug(Utils::varDumpToString($result));
+        $this->assertNotNull($result);
+        $this->assertIsA($result, "Insight");
+        $this->assertPattern('/\@testeriffic\'s most popular post from last week got /', $result->text);
+        $this->assertPattern('/1 reply/', $result->text);
+        $this->assertNoPattern('/,/', $result->text);
+        $this->assertNoPattern('/and/', $result->text);
+    }
+
+    public function testWeeklyBestsInsightWithFavorites() {
+        // Get data ready that insight requires
+        $instance = new Instance();
+        $instance->id = 10;
+        $instance->network_username = 'testeriffic';
+        $instance->network = 'twitter';
+
+        $posts = array();
+        $posts[] = new Post(array(
+            'reply_count_cache' => 0,
+            'retweet_count_cache' => 0,
+            'favlike_count_cache' => 3
+        )); // popularity_index = 6
+
+        $insight_plugin = new WeeklyBestsInsight();
+        $insight_plugin->generateInsight($instance, $posts, 3);
+
+        // Assert that insight got inserted
+        $insight_dao = new InsightMySQLDAO();
+        $today = date ('Y-m-d');
+        $result = $insight_dao->getInsight('weekly_best', 10, $today);
+        $this->debug(Utils::varDumpToString($result));
+        $this->assertNotNull($result);
+        $this->assertIsA($result, "Insight");
+        $this->assertPattern('/\@testeriffic\'s most popular post from last week got /', $result->text);
+        $this->assertPattern('/3 favorites/', $result->text);
+        $this->assertNoPattern('/reply/', $result->text);
+        $this->assertNoPattern('/retweet/', $result->text);
+        $this->assertNoPattern('/,/', $result->text);
+        $this->assertNoPattern('/and/', $result->text);
+    }
+
+    public function testWeeklyBestsInsightWithRepliesAndFavorites() {
+        // Get data ready that insight requires
+        $instance = new Instance();
+        $instance->id = 10;
+        $instance->network_username = 'testeriffic';
+        $instance->network = 'twitter';
+
+        $posts = array();
+        $posts[] = new Post(array(
+            'reply_count_cache' => 4,
+            'retweet_count_cache' => 0,
+            'favlike_count_cache' => 5
+        )); // popularity_index = 30
+
+        $insight_plugin = new WeeklyBestsInsight();
+        $insight_plugin->generateInsight($instance, $posts, 3);
+
+        // Assert that insight got inserted
+        $insight_dao = new InsightMySQLDAO();
+        $today = date ('Y-m-d');
+        $result = $insight_dao->getInsight('weekly_best', 10, $today);
+        $this->debug(Utils::varDumpToString($result));
+        $this->assertNotNull($result);
+        $this->assertIsA($result, "Insight");
+        $this->assertPattern('/\@testeriffic\'s most popular post from last week got /', $result->text);
+        $this->assertPattern('/4 replies/', $result->text);
+        $this->assertPattern('/5 favorites/', $result->text);
+        $this->assertNoPattern('/,/', $result->text);
+        $this->assertPattern('/and/', $result->text);
+    }
+}
