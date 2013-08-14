a0dfda4ca6a66c9bb078eb6bae8fb3d6e86a77af
Gina Trapani <ginatrapani@gmail.com>
2013-08-14 15:02:00 -0700

    Correct migration (remove rollback, fix type insertion)

--- webapp/install/sql/mysql_migrations/2013-08-12_merge_group_count_and_count_history.sql
+++ webapp/install/sql/mysql_migrations/2013-08-12_merge_group_count_and_count_history.sql
@@ -1,6 +1,6 @@
 --
 -- Merges the group member count history table with the count history table
 --
-INSERT IGNORE INTO tu_count_history (SELECT member_user_id, NULL, network, 'group_membership_count', date, count FROM tu_group_member_count) #rollback=9;
+INSERT IGNORE INTO tu_count_history (SELECT member_user_id, NULL, network, 'group_memberships', date, count FROM tu_group_member_count);
 
 DROP TABLE IF EXISTS tu_group_member_count;
