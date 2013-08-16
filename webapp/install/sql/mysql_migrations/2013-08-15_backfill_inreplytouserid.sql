2825f026a2b600da53f8ca938a1b22be6f48dd22
Gina Trapani <ginatrapani@gmail.com>
2013-08-16 07:45:04 -0700

    Speed up migration; use in_reply_to_user_id_str

--- webapp/install/sql/mysql_migrations/2013-08-15_backfill_inreplytouserid.sql
+++ webapp/install/sql/mysql_migrations/2013-08-15_backfill_inreplytouserid.sql
@@ -4,15 +4,18 @@
 
 CREATE TABLE `tu_posts_2b8` LIKE tu_posts;
 
-INSERT IGNORE INTO `tu_posts_2b8` (SELECT * FROM tu_posts) #rollback=1;
+--
+-- Fix posts inserted after Jan 1 2013
+--
+INSERT IGNORE INTO `tu_posts_2b8` (SELECT * FROM tu_posts WHERE pub_date >= '2013-01-01') #rollback=1;
 
 UPDATE `tu_posts_2b8` reply 
-JOIN `tu_posts_2b8` post ON (reply.in_reply_to_post_id = post.post_id AND reply.network = post.network) 
+JOIN tu_posts post ON (reply.in_reply_to_post_id = post.post_id AND reply.network = post.network) 
 SET reply.in_reply_to_user_id = post.author_user_id 
 WHERE reply.in_reply_to_user_id is null AND reply.in_reply_to_post_id is not null AND reply.network='twitter';
 
-RENAME TABLE tu_posts TO `tu_posts_2b7`;
+DELETE FROM tu_posts  WHERE pub_date >= '2013-01-01'; 
 
-RENAME TABLE `tu_posts_2b8` TO tu_posts;
+INSERT IGNORE INTO tu_posts (SELECT * FROM `tu_posts_2b8`);
 
-DROP TABLE IF EXISTS `tu_posts_2b7`;
+DROP TABLE IF EXISTS `tu_posts_2b8`;
