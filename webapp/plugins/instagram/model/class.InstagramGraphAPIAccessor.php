4a2eb23e03fb16cf545ec9d4cd571aa379cf103a
Dimosthenis Nikoudis <dnna@dnna.gr>
2013-09-10 14:19:52 +0300

    More autoloading fixes

--- webapp/plugins/instagram/model/class.InstagramGraphAPIAccessor.php
+++ webapp/plugins/instagram/model/class.InstagramGraphAPIAccessor.php
@@ -1,6 +1,6 @@
 <?php
 $version = explode('.', PHP_VERSION); //dont run redis or instagram test for php less than 5.3
 if ($version[0] >= 5 && $version[1] >= 3) { //only run Instagram tests if PHP 5.3
-	require_once dirname(__FILE__) . 'PHP5.3/' . basename(__FILE__);
+	require_once dirname(__FILE__) . '/PHP5.3/' . basename(__FILE__);
 }
 ?>
