f14a55f3582e97338aee7def3b6c4a33a582c20b
Gina Trapani <ginatrapani@gmail.com>
2013-12-16 12:11:25 -0500

    Correct commented-out tests from b0b33a97f6f6db94d9b0266f5fb663175499f275

--- webapp/plugins/insightsgenerator/tests/TestOfInsightsGeneratorPlugin.php
+++ webapp/plugins/insightsgenerator/tests/TestOfInsightsGeneratorPlugin.php
@@ -114,9 +114,6 @@ class TestOfInsightsGeneratorPlugin extends ThinkUpUnitTestCase {
         $this->assertEqual('', $sent);
     }
 
-/*
-    //@TODO Replace PHP 5.2-incompatible calls and uncomment out this test
-    //The strtotime calls in this test and the next one are incompatible with PHP 5.2; commenting out till we fix
     public function testDailySendSetting() {
         unlink(FileDataManager::getDataPath(Mailer::EMAIL));
         $builders = array();
@@ -157,6 +154,7 @@ class TestOfInsightsGeneratorPlugin extends ThinkUpUnitTestCase {
         $this->assertEqual('', $sent, 'Should not send again same day');
     }
 
+/*
     //@TODO Replace PHP 5.2-incompatible calls and uncomment out this test
     //The strtotime calls in this test and the next one are incompatible with PHP 5.2; commenting out till we fix
     public function testWeeklySendSetting() {
@@ -207,8 +205,9 @@ class TestOfInsightsGeneratorPlugin extends ThinkUpUnitTestCase {
         $sent = Mailer::getLastMail();
         $this->assertEqual('', $sent, 'Should not send again same day');
     }
- */
 
+    //@TODO Replace PHP 5.2-incompatible calls and uncomment out this test
+    //The strtotime calls in this test and the next one are incompatible with PHP 5.2; commenting out till we fix
     public function testBothSendSetting() {
         unlink(FileDataManager::getDataPath(Mailer::EMAIL));
         $plugin = new InsightsGeneratorPlugin();
@@ -265,7 +264,7 @@ class TestOfInsightsGeneratorPlugin extends ThinkUpUnitTestCase {
         $this->assertPattern('/29 new lists/', $sent);
         $this->assertPattern('/This week/', $sent);
     }
-
+*/
     public function testMultiUser() {
         unlink(FileDataManager::getDataPath(Mailer::EMAIL));
         $plugin = new InsightsGeneratorPlugin();
