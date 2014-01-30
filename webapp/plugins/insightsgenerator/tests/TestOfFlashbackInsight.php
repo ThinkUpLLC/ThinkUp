3deabbd25fcbe0d7ad74f561e4ef966619c1f18d
Gina Trapani <ginatrapani@gmail.com>
2014-01-30 12:43:22 -0500

    Flashback insight: Add missing period, clean code, add tests

--- webapp/plugins/insightsgenerator/tests/TestOfFlashbackInsight.php
+++ webapp/plugins/insightsgenerator/tests/TestOfFlashbackInsight.php
@@ -20,9 +20,7 @@
  * You should have received a copy of the GNU General Public License along with ThinkUp.  If not, see
  * <http://www.gnu.org/licenses/>.
  *
- * Test of Fave Like Spike Insight
- *
- * Test for FaveLikeSpikeInsight class.
+ * Test of Flashback Insight
  *
  * @license http://www.gnu.org/licenses/gpl.html
  * @copyright 2013 Gina Trapani
@@ -48,5 +46,104 @@ class TestOfFlashbackInsight extends ThinkUpUnitTestCase {
     public function testConstructor() {
         $flashback_insight_plugin = new FlashbackInsight();
         $this->assertIsA($flashback_insight_plugin, 'FlashbackInsight' );
+    }
+
+    public function testFlashbackInsightForTwitter() {
+        // Get data ready that insight requires
+        $builders = self::buildData();
+        $instance = new Instance();
+        $instance->id = 10;
+        $instance->network_user_id = '7654321';
+        $instance->network_username = 'testeriffic';
+        $instance->network = 'twitter';
+        $insight_plugin = new FlashbackInsight();
+        $insight_plugin->generateInsight($instance, $last_week_of_posts, 3);
+        // Assert that insight got inserted
+        $insight_dao = new InsightMySQLDAO();
+        $today = date ('Y-m-d');
+        $result = $insight_dao->getInsight('posts_on_this_day_popular_flashback', 10, $today);
+        $fav_posts = unserialize($result->related_data);
+        $this->debug(Utils::varDumpToString($result));
+        $this->assertNotNull($result);
+        $this->assertIsA($result, "Insight");
+        $this->assertPattern('/On this day&hellip;/', $result->headline);
+        $possible_text = array("This was @testeriffic's most popular tweet <strong>1 year ago</strong>.",
+        "On this day in 2013, this was @testeriffic's most popular tweet.");
+        $this->assertTrue(in_array( $result->text, $possible_text));
+        $this->assertIsA($fav_posts, "array");
+        $this->assertIsA($fav_posts["posts"][0], "Post");
+        $this->assertEqual(count($fav_posts), 1);
+    }
+
+    public function testFlashbackInsightForFacebook() {
+        // Get data ready that insight requires
+        $builders = self::buildData();
+        $instance = new Instance();
+        $instance->id = 10;
+        $instance->network_user_id = '765432fb';
+        $instance->network_username = 'testeriffic';
+        $instance->network = 'facebook';
+        $insight_plugin = new FlashbackInsight();
+        $insight_plugin->generateInsight($instance, $last_week_of_posts, 3);
+        // Assert that insight got inserted
+        $insight_dao = new InsightMySQLDAO();
+        $today = date ('Y-m-d');
+        $result = $insight_dao->getInsight('posts_on_this_day_popular_flashback', 10, $today);
+        $fav_posts = unserialize($result->related_data);
+        $this->debug(Utils::varDumpToString($result));
+        $this->assertNotNull($result);
+        $this->assertIsA($result, "Insight");
+        $this->assertPattern('/On this day&hellip;/', $result->headline);
+        $possible_text = array("This was testeriffic's most popular status update <strong>1 year ago</strong>.",
+        "On this day in 2013, this was testeriffic's most popular status update.");
+        $this->assertTrue(in_array( $result->text, $possible_text));
+        $this->assertIsA($fav_posts, "array");
+        $this->assertIsA($fav_posts["posts"][0], "Post");
+        $this->assertEqual(count($fav_posts), 1);
+    }
+
+    private function buildData() {
+        $builders = array();
+
+        $now = date('Y-m-d H:i:s');
+        $yesterday = date('Y-m-d H:i:s', strtotime('yesterday'));
+
+        $builders[] = FixtureBuilder::build('posts', array('id'=>133, 'post_id'=>133, 'author_user_id'=>'7654321',
+        'author_username'=>'testeriffic', 'author_fullname'=>'Twitter User', 'author_avatar'=>'avatar.jpg',
+        'network'=>'twitter', 'post_text'=>'This is a simple post.', 'source'=>'web',
+        'pub_date'=>'-730d', 'reply_count_cache'=>50, 'is_protected'=>0, 'in_reply_to_post_id'=>null,
+        'in_reply_to_user_id'=>null, 'in_retweet_of_post_id'=>null, 'in_rt_of_user_id'=>null));
+
+        $builders[] = FixtureBuilder::build('posts', array('id'=>134, 'post_id'=>134, 'author_user_id'=>'7654321',
+        'author_username'=>'testeriffic', 'author_fullname'=>'Twitter User', 'author_avatar'=>'avatar.jpg',
+        'network'=>'twitter', 'post_text'=>'This is a post http://t.co/B5LAotKMWY with a link.', 'source'=>'web',
+        'pub_date'=>'-365d', 'reply_count_cache'=>60, 'is_protected'=>0, 'in_reply_to_post_id'=>null,
+        'in_reply_to_user_id'=>null, 'in_retweet_of_post_id'=>null, 'in_rt_of_user_id'=>null));
+
+        $builders[] = FixtureBuilder::build('posts', array('id'=>135, 'post_id'=>135, 'author_user_id'=>'7654321',
+        'author_username'=>'testeriffic', 'author_fullname'=>'Twitter User', 'author_avatar'=>'avatar.jpg',
+        'network'=>'twitter', 'post_text'=>'This is an old post http://t.co/aMHh5XHGfS with a link.', 'source'=>'web',
+        'pub_date'=>'-365d', 'reply_count_cache'=>70, 'is_protected'=>0, 'in_reply_to_post_id'=>null,
+        'in_reply_to_user_id'=>null, 'in_retweet_of_post_id'=>null, 'in_rt_of_user_id'=>null));
+
+        $builders[] = FixtureBuilder::build('posts', array('id'=>136, 'post_id'=>136, 'author_user_id'=>'765432fb',
+        'author_username'=>'testeriffic', 'author_fullname'=>'Twitter User', 'author_avatar'=>'avatar.jpg',
+        'network'=>'facebook', 'post_text'=>'This is a simple post.', 'source'=>'web',
+        'pub_date'=>'-730d', 'reply_count_cache'=>50, 'is_protected'=>0, 'in_reply_to_post_id'=>null,
+        'in_reply_to_user_id'=>null, 'in_retweet_of_post_id'=>null, 'in_rt_of_user_id'=>null));
+
+        $builders[] = FixtureBuilder::build('posts', array('id'=>137, 'post_id'=>137, 'author_user_id'=>'765432fb',
+        'author_username'=>'testeriffic', 'author_fullname'=>'facebook User', 'author_avatar'=>'avatar.jpg',
+        'network'=>'facebook', 'post_text'=>'This is a post http://t.co/B5LAotKMWY with a link.', 'source'=>'web',
+        'pub_date'=>'-365d', 'reply_count_cache'=>60, 'is_protected'=>0, 'in_reply_to_post_id'=>null,
+        'in_reply_to_user_id'=>null, 'in_retweet_of_post_id'=>null, 'in_rt_of_user_id'=>null));
+
+        $builders[] = FixtureBuilder::build('posts', array('id'=>138, 'post_id'=>138, 'author_user_id'=>'765432fb',
+        'author_username'=>'testeriffic', 'author_fullname'=>'facebook User', 'author_avatar'=>'avatar.jpg',
+        'network'=>'facebook', 'post_text'=>'This is an old post http://t.co/aMHh5XHGfS with a link.', 'source'=>'web',
+        'pub_date'=>'-365d', 'reply_count_cache'=>70, 'is_protected'=>0, 'in_reply_to_post_id'=>null,
+        'in_reply_to_user_id'=>null, 'in_retweet_of_post_id'=>null, 'in_rt_of_user_id'=>null));
+
+        return $builders;
     }
 }
