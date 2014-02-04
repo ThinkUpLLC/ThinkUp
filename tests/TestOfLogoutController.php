6ff725cccda89f1df4f87f06502e4d93fa5158db
Gina Trapani <ginatrapani@gmail.com>
2014-02-04 16:29:54 -0500

    Remove REQUEST_URI dependency from login redirection

--- tests/TestOfLogoutController.php
+++ tests/TestOfLogoutController.php
@@ -36,10 +36,7 @@ class TestOfLogoutController extends ThinkUpUnitTestCase {
     public function testLogoutNotLoggedIn() {
         $controller = new LogoutController(true);
         $results = $controller->go();
-        $v_mgr = $controller->getViewManager();
-        $config = Config::getInstance();
-        $this->assertEqual('You must <a href="'.$config->getValue('site_root_path').
-        'session/login.php">log in</a> to do this.', $v_mgr->getTemplateDataItem('error_msg'));
+        $this->assertPattern( '/session\/login.php\?redirect\=/', $controller->redirect_destination);
     }
 
     public function testLogoutWhileLoggedIn() {
