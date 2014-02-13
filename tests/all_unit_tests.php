6b4a81f0b66c8ac2a37b96b7dca060cd467d17be
Chris Moyer <chris@inarow.net>
2014-02-13 18:21:37 -0500

    Turn back on all the tests and php versions.

--- tests/all_unit_tests.php
+++ tests/all_unit_tests.php
@@ -53,9 +53,9 @@ $TOTAL_PASSES = 0;
 $TOTAL_FAILURES = 0;
 $start_time = microtime(true);
 
-//require_once THINKUP_ROOT_PATH.'tests/all_model_tests.php';
+require_once THINKUP_ROOT_PATH.'tests/all_model_tests.php';
 
-//require_once THINKUP_ROOT_PATH.'tests/all_plugin_tests.php';
+require_once THINKUP_ROOT_PATH.'tests/all_plugin_tests.php';
 
 require_once THINKUP_ROOT_PATH.'tests/all_controller_tests.php';
 
