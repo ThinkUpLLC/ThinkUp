76a2630cc19efc881bcadede9e05702166eafae8
Gina Trapani <ginatrapani@gmail.com>
2014-02-21 23:31:52 -0500

    Olympics insight: only run on 2/23/2014

--- webapp/plugins/insightsgenerator/insights/olympics2014.php
+++ webapp/plugins/insightsgenerator/insights/olympics2014.php
@@ -34,61 +34,62 @@
 class Olympics2014Insight extends InsightPluginParent implements InsightPlugin {
 
     public function generateInsight(Instance $instance, $last_week_of_posts, $number_days) {
-        parent::generateInsight($instance, $last_week_of_posts, $number_days);
-        $this->logger->logInfo("Begin generating insight", __METHOD__.','.__LINE__);
-        $hero_image = array(
-            'url' => 'https://www.thinkup.com/assets/images/insights/2014-02/olympics2014.jpg',
-            'alt_text' => 'The Olympic rings in Sochi',
-            'credit' => 'Photo: Atos International',
-            'img_link' => 'http://www.flickr.com/photos/atosorigin/12568057033/'
-        );
+        if (Utils::isTest() || date("Y-m-d") == '2014-02-23') {
+            parent::generateInsight($instance, $last_week_of_posts, $number_days);
+            $this->logger->logInfo("Begin generating insight", __METHOD__.','.__LINE__);
+            $hero_image = array(
+                'url' => 'https://www.thinkup.com/assets/images/insights/2014-02/olympics2014.jpg',
+                'alt_text' => 'The Olympic rings in Sochi',
+                'credit' => 'Photo: Atos International',
+                'img_link' => 'http://www.flickr.com/photos/atosorigin/12568057033/'
+            );
 
-        $post_dao = DAOFactory::getDAO('PostDAO');
-        $last_month_of_posts = $post_dao->getAllPostsByUsernameOrderedBy($instance->network_username,
-        $network=$instance->network, $count=0, $order_by="pub_date", $in_last_x_days = 30,
-        $iterator = false, $is_public = false);
+            $post_dao = DAOFactory::getDAO('PostDAO');
+            $last_month_of_posts = $post_dao->getAllPostsByUsernameOrderedBy($instance->network_username,
+            $network=$instance->network, $count=0, $order_by="pub_date", $in_last_x_days = 30,
+            $iterator = false, $is_public = false);
 
-        if (self::shouldGenerateWeeklyInsight('olympics_2014', $instance, $insight_date='today',
-        $regenerate_existing_insight=true, $day_of_week=0, count($last_month_of_posts))) {
-            $event_count = 0;
-            foreach ($last_month_of_posts as $post) {
-                $event_count += self::countOlympicReferences($post->post_text);
-            }
-            $this->logger->logInfo("There are $event_count Olympic-related mentions", __METHOD__.','.__LINE__);
-
-            if ($event_count > 0) {
-                $headline = "Do they give out medals for " .$this->terms->getNoun('post', InsightTerms::PLURAL) . "?";
-                $insight_text = "$this->username mentioned ";
+            if (self::shouldGenerateWeeklyInsight('olympics_2014', $instance, $insight_date='today',
+            $regenerate_existing_insight=true, $day_of_week=0, count($last_month_of_posts))) {
+                $event_count = 0;
+                foreach ($last_month_of_posts as $post) {
+                    $event_count += self::countOlympicReferences($post->post_text);
+                }
+                $this->logger->logInfo("There are $event_count Olympic-related mentions", __METHOD__.','.__LINE__);
 
                 if ($event_count > 0) {
-                    $this->logger->logInfo("There are event mentions", __METHOD__.','.__LINE__);
+                    $headline = "Do they give out medals for ".$this->terms->getNoun('post', InsightTerms::PLURAL)."?";
+                    $insight_text = "$this->username mentioned ";
 
-                    $insight_text .= "the Olympics ";
-                    if ($event_count > 1) {
-                        $this->logger->logInfo("there is more than one event mention", __METHOD__.','.__LINE__);
-                        $insight_text .= "$event_count times since they started.";
-                        $insight_text .= " That's kind of like winning $event_count gold medals in " .
-                            ucfirst($instance->network) . ", right?";
-                    } else {
-                        $insight_text .= "just as the whole world's attention was focused on the Games.";
-                        $insight_text .= " That's a pretty great way to join a global conversation.";
+                    if ($event_count > 0) {
+                        $this->logger->logInfo("There are event mentions", __METHOD__.','.__LINE__);
+
+                        $insight_text .= "the Olympics ";
+                        if ($event_count > 1) {
+                            $this->logger->logInfo("there is more than one event mention", __METHOD__.','.__LINE__);
+                            $insight_text .= "$event_count times since they started.";
+                            $insight_text .= " That's kind of like winning $event_count gold medals in " .
+                                ucfirst($instance->network) . ", right?";
+                        } else {
+                            $insight_text .= "just as the whole world's attention was focused on the Games.";
+                            $insight_text .= " That's a pretty great way to join a global conversation.";
+                        }
                     }
+                    $my_insight = new Insight();
+                    $my_insight->slug = 'olympics_2014'; //slug to label this insight's content
+                    $my_insight->instance_id = $instance->id;
+                    $my_insight->date = $this->insight_date; //date is often this or $simplified_post_date
+                    $my_insight->headline = $headline; // or just set a string like 'Ohai';
+                    $my_insight->text = $insight_text; // or just set a strong like "Greetings humans";
+                    $my_insight->filename = basename(__FILE__, ".php"); //Same for every insight, must be set this way
+                    $my_insight->emphasis = Insight::EMPHASIS_HIGH; //Optional emphasis, default is Insight::EMPHASIS_LOW
+                    $my_insight->setHeroImage($hero_image);
+
+                    $this->insight_dao->insertInsight($my_insight);
                 }
-                $my_insight = new Insight();
-                $my_insight->slug = 'olympics_2014'; //slug to label this insight's content
-                $my_insight->instance_id = $instance->id;
-                $my_insight->date = $this->insight_date; //date is often this or $simplified_post_date
-                $my_insight->headline = $headline; // or just set a string like 'Ohai';
-                $my_insight->text = $insight_text; // or just set a strong like "Greetings humans";
-                $my_insight->filename = basename(__FILE__, ".php"); //Same for every insight, must be set this way
-                $my_insight->emphasis = Insight::EMPHASIS_HIGH; //Optional emphasis, default is Insight::EMPHASIS_LOW
-                $my_insight->setHeroImage($hero_image);
-
-                $this->insight_dao->insertInsight($my_insight);
             }
+            $this->logger->logInfo("Done generating insight", __METHOD__.','.__LINE__);
         }
-
-        $this->logger->logInfo("Done generating insight", __METHOD__.','.__LINE__);
     }
 
     /**
