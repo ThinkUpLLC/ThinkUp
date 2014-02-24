048eb2139c106e72919a4cddee18543443d07bf8
Chris Moyer <chris@inarow.net>
2014-02-24 16:29:15 +0000

    Ad sessioncache back.

--- tests/all_model_tests.php
+++ tests/all_model_tests.php
@@ -71,7 +71,7 @@ $model_tests->add(new TestOfExportMySQLDAO());
 $model_tests->add(new TestOfPostErrorMySQLDAO());
 $model_tests->add(new TestOfProfiler());
 $model_tests->add(new TestOfSession());
-#$model_tests->add(new TestOfSessionCache());
+$model_tests->add(new TestOfSessionCache());
 $model_tests->add(new TestOfViewManager());
 $model_tests->add(new TestOfUserMySQLDAO());
 $model_tests->add(new TestOfUserErrorMySQLDAO());
