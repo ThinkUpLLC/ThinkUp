5d96a63d24062df4be6bb6bda7055897c523c11e
Chris Moyer <chris@inarow.net>
2014-02-24 14:59:39 +0000

    Turn plugin tests on.

--- tests/all_unit_tests.php
+++ tests/all_unit_tests.php
@@ -59,7 +59,7 @@ $start_time = microtime(true);
 
 //require_once THINKUP_ROOT_PATH.'tests/all_model_tests.php';
 
-//require_once THINKUP_ROOT_PATH.'tests/all_plugin_tests.php';
+require_once THINKUP_ROOT_PATH.'tests/all_plugin_tests.php';
 
 require_once THINKUP_ROOT_PATH.'tests/all_controller_tests.php';
 
