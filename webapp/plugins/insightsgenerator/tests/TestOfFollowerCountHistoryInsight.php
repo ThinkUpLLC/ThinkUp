77eb47788def1ce6b8db09358a4c117c7b2256b3
Chris Moyer <chris@inarow.net>
2014-04-02 22:53:33 +0000

    Actually fix FollowerCountHistory to be date agnostic.  I hope.

--- webapp/plugins/insightsgenerator/tests/TestOfFollowerCountHistoryInsight.php
+++ webapp/plugins/insightsgenerator/tests/TestOfFollowerCountHistoryInsight.php
@@ -168,12 +168,12 @@ class TestOfFollowerCountHistoryInsight extends ThinkUpInsightUnitTestCase {
         $builders = array();
         for ($i=0; $i<7; $i++) {
             $builders[] = FixtureBuilder::build('count_history', array('network_user_id'=>42, 'network'=>'twitter',
-                'type'=>'followers', 'count'=>'9320', 'date' => date('Y-m-d', strtotime('-'.(95+($i*30)).' day'))));
+                'type'=>'followers', 'count'=>'9320', 'date' => date('Y-m-d', strtotime('-'.(3+$i).' month'))));
         }
         $builders[] = FixtureBuilder::build('count_history', array('network_user_id'=>42, 'network'=>'twitter',
-            'type'=>'followers', 'count'=>'9900', 'date' => date('Y-m-d', strtotime('-65 day'))));
+            'type'=>'followers', 'count'=>'9900', 'date' => date('Y-m-d', strtotime('-2 month'))));
         $builders[] = FixtureBuilder::build('count_history', array('network_user_id'=>42, 'network'=>'twitter',
-            'type'=>'followers', 'count'=>'9950', 'date' => date('Y-m-d', strtotime('-35 day'))));
+            'type'=>'followers', 'count'=>'9950', 'date' => date('Y-m-d', strtotime('-1 month'))));
 
         $insight_plugin = new FollowerCountInsight();
         $insight_plugin->generateInsight($this->instance, array(), 3);
