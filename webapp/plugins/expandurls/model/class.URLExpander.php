15ba217a1c570d3f8b37221f4bf11f0c425bcfcb
Gina Trapani <ginatrapani@gmail.com>
2014-05-14 12:17:42 -0400

    Avoid "Update Your Browser" errors from Facebook
    Set ThinkUp’s user agent to Chrome

--- webapp/plugins/expandurls/model/class.URLExpander.php
+++ webapp/plugins/expandurls/model/class.URLExpander.php
@@ -112,6 +112,9 @@ class URLExpander {
         curl_setopt($ch, CURLOPT_URL, $url);
         curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
         curl_setopt($ch, CURLOPT_TIMEOUT, 1); // don't wait more than 1 second
+        $chrome_user_agent = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_2) AppleWebKit/537.36 (KHTML, like Gecko) '.
+            'Chrome/36.0.1944.0 Safari/537.36';
+        curl_setopt($ch, CURLOPT_USERAGENT, $chrome_user_agent);
 
         $html = curl_exec($ch);
         curl_close($ch);
