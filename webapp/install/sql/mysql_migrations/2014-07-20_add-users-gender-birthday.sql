eae178acec450d21a5974f561d4531f654a943d1
ashkerina <anna@example.com>
2014-07-21 13:28:50 -0400

    [DB MIGRATION REQ'D] Add user gender and age fields This commit is only the database migration which adds gender and age fields to the users table. Laying the groundwork for future insights related to gender and age.

---
+++ webapp/install/sql/mysql_migrations/2014-07-20_add-users-gender-birthday.sql
@@ -0,0 +1,22 @@
+--
+-- Add two columns to the users table for storing gender and birthday
+--
+DROP TABLE IF EXISTS tu_users_20b11;
+
+CREATE TABLE tu_users_20b11 LIKE tu_users;
+
+ALTER TABLE tu_users_20b11 ADD gender VARCHAR ( 255 ) DEFAULT NULL COMMENT 'Service user''s gender with room for custom options.' AFTER url ;#rollback=2;
+
+ALTER TABLE tu_users_20b11 ADD birthday date DEFAULT NULL COMMENT 'Service user''s birthday.' AFTER gender;#rollback=3;
+
+INSERT IGNORE INTO tu_users_20b11 (SELECT id, user_id, user_name, full_name, avatar, location, description, url, null, null, is_verified, is_protected, follower_count, friend_count, post_count, last_updated, found_in, last_post, joined, last_post_id, network, favorites_count FROM tu_users)#rollback=4;
+
+RENAME TABLE tu_users TO tu_users_20b10;
+
+RENAME TABLE tu_users_20b11 TO tu_users;
+
+DROP TABLE IF EXISTS tu_users_20b10;
+
+
+
+
