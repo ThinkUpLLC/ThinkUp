2ce773931a6d283fe1e5486b56c2f63151b03cb0
Gina Trapani <ginatrapani@gmail.com>
2014-08-02 22:56:00 -0400

    Move local followers insight into interesting followers insight and check for dupes to avoid repetition

--- webapp/plugins/insightsgenerator/tests/TestOfInterestingFollowersInsight.php
+++ webapp/plugins/insightsgenerator/tests/TestOfInterestingFollowersInsight.php
@@ -415,4 +415,278 @@ class TestOfInterestingFollowersInsight extends ThinkUpInsightUnitTestCase {
         $this->assertIsA($result->related_data['people'], 'Array');
         $this->assertEqual($result->related_data['people'][0]->username,'popular1');
     }
+
+    public function testLocalFollowersInsightWithLocation() {
+        // Get data ready that insight requires
+        $instance = new Instance();
+        $instance->id = 10;
+        $instance->network_user_id = 9654000768;
+        $instance->network_username = 'testuser';
+        $instance->network = 'twitter';
+
+        $builders = array();
+
+        // User
+        $builders[] = FixtureBuilder::build('users', array('user_id'=>'9654000768', 'user_name'=>'testuser',
+        'full_name'=>'Twitter User', 'avatar'=>'avatar.jpg', 'follower_count'=>360, 'is_protected'=>1,
+        'network'=>'twitter', 'description'=>'A test Twitter User', 'location'=>'San Francisco, CA',
+        'friend_count'=>360, 'is_verified'=>0));
+
+        // Followers
+        $builders[] = FixtureBuilder::build('users', array('user_id'=>'9654000769', 'user_name'=>'testfollower1',
+        'full_name'=>'Twitter Follower One', 'avatar'=>'avatar.jpg', 'follower_count'=>360, 'is_protected'=>0,
+        'network'=>'twitter', 'description'=>'A test Twitter Folower', 'location'=>'San Francisco, CA',
+        'friend_count'=>360, 'is_verified'=>0));
+
+        $builders[] = FixtureBuilder::build('users', array('user_id'=>'9654000770', 'user_name'=>'testfollower2',
+        'full_name'=>'Twitter Follower Two', 'avatar'=>'avatar.jpg', 'follower_count'=>360, 'is_protected'=>0,
+        'network'=>'twitter', 'description'=>'A test Twitter Folower', 'location'=>'San Francisco, CA',
+        'friend_count'=>360, 'is_verified'=>0));
+
+        $builders[] = FixtureBuilder::build('users', array('user_id'=>'9654000771', 'user_name'=>'testfollower3',
+        'full_name'=>'Twitter Follower Three', 'avatar'=>'avatar.jpg', 'follower_count'=>360, 'is_protected'=>0,
+        'network'=>'twitter', 'description'=>'A test Twitter Folower', 'location'=>'San Diego, CA',
+        'friend_count'=>360, 'is_verified'=>0));
+
+        // Follows
+        $builders[] = FixtureBuilder::build('follows', array('user_id'=>'9654000768', 'follower_id'=>'9654000769',
+        'last_seen'=>'-0d', 'first_seen'=>'-0d', 'network'=>'twitter'));
+
+        $builders[] = FixtureBuilder::build('follows', array('user_id'=>'9654000768', 'follower_id'=>'9654000770',
+        'last_seen'=>'-0d', 'first_seen'=>'-0d', 'network'=>'twitter'));
+
+        $builders[] = FixtureBuilder::build('follows', array('user_id'=>'9654000768', 'follower_id'=>'9654000771',
+        'last_seen'=>'-0d', 'first_seen'=>'-0d', 'network'=>'twitter'));
+
+        // Initialize and run the insight
+        $insight_plugin = new InterestingFollowersInsight();
+        $insight_plugin->generateInsight($instance, $this->getUser(), $posts=array(), 3);
+
+        //sleep(1000);
+        // Assert that insight got inserted
+        $insight_dao = new InsightMySQLDAO();
+        $today = date ('Y-m-d');
+        $result = $insight_dao->getInsight('local_followers', 10, $today);
+        $this->debug(Utils::varDumpToString($result));
+        $this->assertNotNull($result);
+        $this->assertIsA($result, "Insight");
+        $this->assertPattern('/<strong>2 people<\/strong> in San Francisco, CA followed \@testuser./',
+            $result->headline);
+        $this->assertNoPattern('/avatar.jpg/', $result->header_image);
+    }
+
+    public function testLocalFollowersInsightWithHeaderImage() {
+        // Get data ready that insight requires
+        $instance = new Instance();
+        $instance->id = 10;
+        $instance->network_user_id = 9654000768;
+        $instance->network_username = 'testuser';
+        $instance->network = 'twitter';
+
+        $builders = array();
+
+        // User
+        $builders[] = FixtureBuilder::build('users', array('user_id'=>'9654000768', 'user_name'=>'testuser',
+        'full_name'=>'Twitter User', 'avatar'=>'avatar.jpg', 'follower_count'=>36000, 'is_protected'=>1,
+        'network'=>'twitter', 'description'=>'A test Twitter User', 'location'=>'San Francisco, CA'));
+
+        // Followers
+        $builders[] = FixtureBuilder::build('users', array('user_id'=>'9654000769', 'user_name'=>'testfollower1',
+        'full_name'=>'Twitter Follower One', 'avatar'=>'avatar.jpg', 'follower_count'=>36000, 'is_protected'=>0,
+        'network'=>'twitter', 'description'=>'A test Twitter Folower', 'location'=>'San Francisco, CA'));
+
+        // Follows
+        $builders[] = FixtureBuilder::build('follows', array('user_id'=>'9654000768', 'follower_id'=>'9654000769',
+        'last_seen'=>'-0d', 'first_seen'=>'-0d', 'network'=>'twitter'));
+
+        // Initialize and run the insight
+        $insight_plugin = new InterestingFollowersInsight();
+        $insight_plugin->generateInsight($instance, $this->getUser(), $posts=array(), 3);
+
+        // Assert that insight got inserted
+        $insight_dao = new InsightMySQLDAO();
+        $today = date ('Y-m-d');
+        $result = $insight_dao->getInsight('local_followers', 10, $today);
+        $this->debug(Utils::varDumpToString($result));
+        $this->assertNotNull($result);
+        $this->assertIsA($result, "Insight");
+        $this->assertPattern('/<strong>1 person<\/strong> in San Francisco, CA followed \@testuser./',
+            $result->headline);
+        $this->assertPattern('/avatar.jpg/', $result->header_image);
+        $rendered = $this->getRenderedInsightInHTML($result);
+        $this->assertEqual(1, substr_count($rendered, 'avatar.jpg'));
+    }
+
+    public function testLocalFollowersInsightWithoutLocation() {
+        // Get data ready that insight requires
+        $instance = new Instance();
+        $instance->id = 10;
+        $instance->network_user_id = 9654000768;
+        $instance->network_username = 'testuser';
+        $instance->network = 'twitter';
+
+        $builders = array();
+
+        // User
+        $builders[] = FixtureBuilder::build('users', array('user_id'=>'9654000768', 'user_name'=>'testuser',
+        'full_name'=>'Twitter User', 'avatar'=>'avatar.jpg', 'follower_count'=>36000, 'is_protected'=>1,
+        'network'=>'twitter', 'description'=>'A test Twitter User', 'location'=>''));
+
+        // Followers
+        $builders[] = FixtureBuilder::build('users', array('user_id'=>'9654000769', 'user_name'=>'testfollower1',
+        'full_name'=>'Twitter Follower One', 'avatar'=>'avatar.jpg', 'follower_count'=>36000, 'is_protected'=>0,
+        'network'=>'twitter', 'description'=>'A test Twitter Folower', 'location'=>'San Francisco, CA'));
+
+        $builders[] = FixtureBuilder::build('users', array('user_id'=>'9654000770', 'user_name'=>'testfollower2',
+        'full_name'=>'Twitter Follower Two', 'avatar'=>'avatar.jpg', 'follower_count'=>36000, 'is_protected'=>0,
+        'network'=>'twitter', 'description'=>'A test Twitter Folower', 'location'=>'San Francisco, CA'));
+
+        $builders[] = FixtureBuilder::build('users', array('user_id'=>'9654000771', 'user_name'=>'testfollower3',
+        'full_name'=>'Twitter Follower Three', 'avatar'=>'avatar.jpg', 'follower_count'=>36000, 'is_protected'=>0,
+        'network'=>'twitter', 'description'=>'A test Twitter Folower', 'location'=>''));
+
+        // Follows
+        $builders[] = FixtureBuilder::build('follows', array('user_id'=>'9654000768', 'follower_id'=>'9654000769',
+        'last_seen'=>'-0d', 'first_seen'=>'-0d', 'network'=>'twitter'));
+
+        $builders[] = FixtureBuilder::build('follows', array('user_id'=>'9654000768', 'follower_id'=>'9654000770',
+        'last_seen'=>'-0d', 'first_seen'=>'-0d', 'network'=>'twitter'));
+
+        $builders[] = FixtureBuilder::build('follows', array('user_id'=>'9654000768', 'follower_id'=>'9654000771',
+        'last_seen'=>'-0d', 'first_seen'=>'-0d', 'network'=>'twitter'));
+
+        // Initialize and run the insight
+        $insight_plugin = new InterestingFollowersInsight();
+        $insight_plugin->generateInsight($instance, $this->getUser(), $posts=array(), 3);
+
+        // Assert that insight did NOT get inserted
+        $insight_dao = new InsightMySQLDAO();
+        $today = date ('Y-m-d');
+        $result = $insight_dao->getInsight('local_followers', 10, $today);
+        $this->debug(Utils::varDumpToString($result));
+        $this->assertNull($result);
+    }
+
+    public function test2LocalFollowersInsightWith1LeastLikely() {
+        // Get data ready that insight requires
+        $instance = new Instance();
+        $instance->id = 10;
+        $instance->network_user_id = 9654000768;
+        $instance->network_username = 'testuser';
+        $instance->network = 'twitter';
+
+        $builders = array();
+
+        // User
+        $builders[] = FixtureBuilder::build('users', array('user_id'=>'9654000768', 'user_name'=>'testuser',
+        'full_name'=>'Twitter User', 'avatar'=>'avatar.jpg', 'follower_count'=>360, 'is_protected'=>1,
+        'network'=>'twitter', 'description'=>'A test Twitter User', 'location'=>'San Francisco, CA',
+        'friend_count'=>360, 'is_verified'=>0));
+
+        // Followers
+        $builders[] = FixtureBuilder::build('users', array('user_id'=>'9654000769', 'user_name'=>'testfollower1',
+        'full_name'=>'Twitter Follower One', 'avatar'=>'avatar.jpg', 'follower_count'=>3600000, 'is_protected'=>0,
+        'network'=>'twitter', 'description'=>'A test Twitter Folower', 'location'=>'San Francisco, CA',
+        'friend_count'=>360, 'is_verified'=>0, 'post_count' => 200));
+
+        $builders[] = FixtureBuilder::build('users', array('user_id'=>'9654000770', 'user_name'=>'testfollower2',
+        'full_name'=>'Twitter Follower Two', 'avatar'=>'avatar.jpg', 'follower_count'=>360, 'is_protected'=>0,
+        'network'=>'twitter', 'description'=>'A test Twitter Folower', 'location'=>'San Francisco, CA',
+        'friend_count'=>360, 'is_verified'=>0));
+
+        $builders[] = FixtureBuilder::build('users', array('user_id'=>'9654000771', 'user_name'=>'testfollower3',
+        'full_name'=>'Twitter Follower Three', 'avatar'=>'avatar.jpg', 'follower_count'=>360, 'is_protected'=>0,
+        'network'=>'twitter', 'description'=>'A test Twitter Folower', 'location'=>'San Diego, CA',
+        'friend_count'=>360, 'is_verified'=>0));
+
+        // Follows
+        $builders[] = FixtureBuilder::build('follows', array('user_id'=>'9654000768', 'follower_id'=>'9654000769',
+        'last_seen'=>'-0d', 'first_seen'=>'-0d', 'network'=>'twitter'));
+
+        $builders[] = FixtureBuilder::build('follows', array('user_id'=>'9654000768', 'follower_id'=>'9654000770',
+        'last_seen'=>'-0d', 'first_seen'=>'-0d', 'network'=>'twitter'));
+
+        $builders[] = FixtureBuilder::build('follows', array('user_id'=>'9654000768', 'follower_id'=>'9654000771',
+        'last_seen'=>'-0d', 'first_seen'=>'-0d', 'network'=>'twitter'));
+
+        // Initialize and run the insight
+        $insight_plugin = new InterestingFollowersInsight();
+        $insight_plugin->generateInsight($instance, $this->getUser(), $posts=array(), 3);
+
+        //sleep(1000);
+        // Assert that insight got inserted
+        $insight_dao = new InsightMySQLDAO();
+        $today = date ('Y-m-d');
+        $result = $insight_dao->getInsight('local_followers', 10, $today);
+        $this->debug(Utils::varDumpToString($result));
+        $this->assertNotNull($result);
+        $this->assertIsA($result, "Insight");
+        $this->assertPattern('/<strong>1 person<\/strong> in San Francisco, CA followed \@testuser./',
+            $result->headline);
+        $this->assertPattern('/avatar.jpg/', $result->header_image);
+    }
+
+    public function test2LocalFollowersInsightWith1Verified() {
+        // Get data ready that insight requires
+        $instance = new Instance();
+        $instance->id = 10;
+        $instance->network_user_id = 9654000768;
+        $instance->network_username = 'testuser';
+        $instance->network = 'twitter';
+
+        $builders = array();
+
+        // User
+        $builders[] = FixtureBuilder::build('users', array('user_id'=>'9654000768', 'user_name'=>'testuser',
+        'full_name'=>'Twitter User', 'avatar'=>'avatar.jpg', 'follower_count'=>360, 'is_protected'=>1,
+        'network'=>'twitter', 'description'=>'A test Twitter User', 'location'=>'San Francisco, CA',
+        'friend_count'=>360, 'is_verified'=>0));
+
+        // Followers
+        $builders[] = FixtureBuilder::build('users', array('user_id'=>'9654000769', 'user_name'=>'testfollower1',
+        'full_name'=>'Twitter Follower One', 'avatar'=>'avatar.jpg', 'follower_count'=>3600000, 'is_protected'=>0,
+        'network'=>'twitter', 'description'=>'A test Twitter Folower', 'location'=>'San Francisco, CA',
+        'friend_count'=>360, 'is_verified'=>0));
+
+        $builders[] = FixtureBuilder::build('users', array('user_id'=>'9654000770', 'user_name'=>'testfollower2',
+        'full_name'=>'Twitter Follower Two', 'avatar'=>'avatar.jpg', 'follower_count'=>360, 'is_protected'=>0,
+        'network'=>'twitter', 'description'=>'A test Twitter Folower', 'location'=>'San Francisco, CA',
+        'friend_count'=>360, 'is_verified'=>1));
+
+        $builders[] = FixtureBuilder::build('users', array('user_id'=>'9654000771', 'user_name'=>'testfollower3',
+        'full_name'=>'Twitter Follower Three', 'avatar'=>'avatar.jpg', 'follower_count'=>360, 'is_protected'=>0,
+        'network'=>'twitter', 'description'=>'A test Twitter Folower', 'location'=>'San Diego, CA',
+        'friend_count'=>360, 'is_verified'=>0));
+
+        // Follows
+        $builders[] = FixtureBuilder::build('follows', array('user_id'=>'9654000768', 'follower_id'=>'9654000769',
+        'last_seen'=>'-0d', 'first_seen'=>'-0d', 'network'=>'twitter'));
+
+        $builders[] = FixtureBuilder::build('follows', array('user_id'=>'9654000768', 'follower_id'=>'9654000770',
+        'last_seen'=>'-0d', 'first_seen'=>'-0d', 'network'=>'twitter'));
+
+        $builders[] = FixtureBuilder::build('follows', array('user_id'=>'9654000768', 'follower_id'=>'9654000771',
+        'last_seen'=>'-0d', 'first_seen'=>'-0d', 'network'=>'twitter'));
+
+        // Initialize and run the insight
+        $insight_plugin = new InterestingFollowersInsight();
+        $insight_plugin->generateInsight($instance, $this->getUser(), $posts=array(), 3);
+
+        //sleep(1000);
+        // Assert that insight got inserted
+        $insight_dao = new InsightMySQLDAO();
+        $today = date ('Y-m-d');
+        $result = $insight_dao->getInsight('local_followers', 10, $today);
+        $this->debug(Utils::varDumpToString($result));
+        $this->assertNotNull($result);
+        $this->assertIsA($result, "Insight");
+        $this->assertPattern('/<strong>1 person<\/strong> in San Francisco, CA followed \@testuser./',
+            $result->headline);
+        $this->assertPattern('/avatar.jpg/', $result->header_image);
+    }
+
+    private function getUser($user_id=9654000768, $network = 'twitter') {
+        $user_dao = DAOFactory::getDAO('UserDAO');
+        return $user_dao->getDetails($user_id, $network);
+    }
 }
