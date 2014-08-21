ThinkUpLLC/ThinkUp

commit 5d6c786413924a0cd4d45cf0c618b0ab0086f385

Avoid Mandrill_ValidationError: You must specify a key value due to non UTF8 encoding

webapp/_lib/class.Mailer.php
@@ -76,7 +76,10 @@ class Mailer {
             'global_merge_vars' => array());
 
             foreach ($template_params as $key=>$val) {
-                $message['global_merge_vars'][] = array('name'=>$key, 'content'=>$val);
+                $message['global_merge_vars'][] = array('name'=>$key,
+                    'content'=>
+                    // utf8_encode to avoid Mandrill Error triggered by special characters
+                    utf8_encode($val));
             }
 
             //don't send email when running tests, just write it to the filesystem for assertions

commit c86dd1da9510dc7dc2a75c1b702f6e548a5b45c7

Rollback 5d6c786413924a0cd4d45cf0c618b0ab0086f385 which broke more email special characters than it fixed

webapp/_lib/class.Mailer.php
@@ -77,9 +77,7 @@ class Mailer {
 
             foreach ($template_params as $key=>$val) {
                 $message['global_merge_vars'][] = array('name'=>$key,
-                    'content'=>
-                    // utf8_encode to avoid Mandrill Error triggered by special characters
-                    utf8_encode($val));
+                    'content'=>$val);
             }
 
             //don't send email when running tests, just write it to the filesystem for assertions