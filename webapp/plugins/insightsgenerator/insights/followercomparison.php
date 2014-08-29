70381383fa811e894a71e187542fe545783d0ade
Chris Moyer <chris@inarow.net>
2014-08-29 17:57:44 +0000

    New Follower Comparison insight
    Insight, templates, tests, added to all tests
    
    Closes #2000

---
+++ webapp/plugins/insightsgenerator/insights/followercomparison.php
@@ -0,0 +1,86 @@
+<?php
+/*
+ Plugin Name: Follower Count Comparison
+ Description: See how many friends you could help out by retweeting them.
+ When: Monthly on the 8th for Twitter
+ */
+/**
+ *
+ * ThinkUp/webapp/plugins/insightsgenerator/insights/followercomparison.php
+ *
+ * Copyright (c) 2014 Chris Moyer
+ *
+ * LICENSE:
+ *
+ * This file is part of ThinkUp (http://thinkup.com).
+ *
+ * ThinkUp is free software: you can redistribute it and/or modify it under the terms of the GNU General Public
+ * License as published by the Free Software Foundation, either version 2 of the License, or (at your option) any
+ * later version.
+ *
+ * ThinkUp is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied
+ * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License along with ThinkUp.  If not, see
+ * <http://www.gnu.org/licenses/>.
+ *
+ * @license http://www.gnu.org/licenses/gpl.html
+ * @copyright 2014 Chris Moyer
+ * @author Chris Moyer <chris [at] inarow [dot] net>
+ */
+class FollowerComparisonInsight extends InsightPluginParent implements InsightPlugin {
+    /**
+     * Slug used for this insight
+     */
+    private $slug = 'follower_comparison';
+
+    public function generateInsight(Instance $instance, User $user, $last_week_of_posts, $number_days) {
+        parent::generateInsight($instance, $user, $last_week_of_posts, $number_days);
+        $this->logger->logInfo("Begin generating insight", __METHOD__.','.__LINE__);
+
+        $network = $instance->network;
+        $user_id = $instance->network_user_id;
+        if ($network == 'twitter' && $this->shouldGenerateMonthlyInsight($this->slug, $instance, 'today', false, 8)) {
+
+            $my_count = $user->follower_count;
+            $follow_dao = DAOFactory::getDAO('FollowDAO');
+            $follow_count = $follow_dao->countTotalFriends($user_id, $network);
+            $median = $follow_dao->getMedianFollowerCountOfFriends($user_id, $network);
+            $number_under = $follow_dao->getCountOfFriendsWithFewerFollowers($user_id, $network, $my_count);
+            $rank = $follow_count - $number_under +1;
+            $percentile = (int)sprintf('%d', 100 * ($number_under / $follow_count));
+
+            if ($percentile >= 50 && $my_count > $median) {
+                $insight = new Insight();
+                $insight->slug = $this->slug;
+                $insight->date = $this->insight_date;
+                $insight->instance_id = $instance->id;
+                $insight->filename = basename(__FILE__, ".php");
+                $insight->emphasis = Insight::EMPHASIS_MED;
+                $insight->headline = $this->getVariableCopy(array(
+                    '%username has it good',
+                    'A closer look at %username\'s follower count',
+                    '%username could lend a hand',
+                ));
+                $insight->text = $this->getVariableCopy(array(
+                    "%username has more followers than %percentile% of the %total people %username follows. That means "
+                        . "<strong>%percentoftotal</strong> of %username's friends would reach a bigger audience "
+                        . "if %username retweeted them.",
+                    "%username has more followers than %percentile% of the people %username follows. That means "
+                        . "<strong>%percentoftotal</strong> of %username's friends would reach a bigger audience "
+                        . "if %username retweeted them."
+                ), array(
+                    'total' => number_format($follow_count),
+                    'percentoftotal' => number_format($number_under),
+                    'percentile' => $percentile,
+                ));
+                $this->insight_dao->insertInsight($insight);
+            }
+        }
+        $this->logger->logInfo("Done generating insight", __METHOD__.','.__LINE__);
+    }
+}
+
+$insights_plugin_registrar = PluginRegistrarInsights::getInstance();
+$insights_plugin_registrar->registerInsightPlugin('FollowerComparisonInsight');
---
+++ webapp/plugins/insightsgenerator/tests/TestOfFollowerComparisonInsight.php
@@ -0,0 +1,146 @@
+<?php
+/**
+ *
+ * ThinkUp/webapp/plugins/insightsgenerator/tests/TestOfFollowerComparisonInsight.php
+ *
+ * Copyright (c) Chris Moyer
+ *
+ * LICENSE:
+ *
+ * This file is part of ThinkUp (http://thinkup.com).
+ *
+ * ThinkUp is free software: you can redistribute it and/or modify it under the terms of the GNU General Public
+ * License as published by the Free Software Foundation, either version 2 of the License, or (at your option) any
+ * later version.
+ *
+ * ThinkUp is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied
+ * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License along with ThinkUp.  If not, see
+ * <http://www.gnu.org/licenses/>.
+ *
+ * Test of LOL Count Insight
+ *
+ * @license http://www.gnu.org/licenses/gpl.html
+ * @copyright 2014 Chris Moyer
+ * @author Chris Moyer <chris[at]inarow[dot]net>
+ */
+
+require_once dirname(__FILE__) . '/../../../../tests/init.tests.php';
+require_once THINKUP_WEBAPP_PATH.'_lib/extlib/simpletest/autorun.php';
+require_once THINKUP_WEBAPP_PATH.'_lib/extlib/simpletest/web_tester.php';
+require_once THINKUP_ROOT_PATH. 'webapp/plugins/insightsgenerator/model/class.InsightPluginParent.php';
+require_once THINKUP_ROOT_PATH. 'webapp/plugins/insightsgenerator/insights/followercomparison.php';
+
+class TestOfFollowerComparisonInsight extends ThinkUpInsightUnitTestCase {
+    public function setUp(){
+        parent::setUp();
+
+        $instance = new Instance();
+        $instance->id = 1;
+        $instance->network_user_id = 42;
+        $instance->network_username = 'testy';
+        $instance->network = 'twitter';
+        $this->instance = $instance;
+        $this->slug = 'follower_comparison';
+
+        TimeHelper::setTime(2); // Force one headline for most tests
+    }
+
+    public function tearDown() {
+        parent::tearDown();
+    }
+
+    public function testConstructor() {
+        $insight_plugin = new FollowerComparisonInsight();
+        $this->assertIsA($insight_plugin, 'FollowerComparisonInsight' );
+    }
+
+    public function testSubFifty() {
+        $builders = array();
+        $builders[] = FixtureBuilder::build('users', array('user_id'=>'999', 'follower_count'=>100));
+        $builders[] = FixtureBuilder::build('follows', array('user_id'=>'999', 'follower_id'=>42,
+            'active' => 1, 'last_seen'=>'-1d', 'network'=>'twitter'));
+
+        $builders[] = FixtureBuilder::build('users', array('user_id'=>'888', 'follower_count'=>3000));
+        $builders[] = FixtureBuilder::build('follows', array('user_id'=>'888', 'follower_id'=>42,
+            'active' => 1, 'last_seen'=>'-1d', 'network'=>'twitter'));
+
+        $user = new User(array('follower_count'=>1, 'user_id' => 42, 'network' => 'twitter'));
+
+        $insight_dao = DAOFactory::getDAO('InsightDAO');
+        $insight_plugin = new FollowerComparisonInsight();
+        $insight_plugin->generateInsight($this->instance, $user, array(), 3);
+        $today = date ('Y-m-d');
+        $result = $insight_dao->getInsight($this->slug, $this->instance->id, $today);
+        $this->assertNull($result);
+    }
+
+    public function testOverFifty() {
+        $builders = array();
+        $builders[] = FixtureBuilder::build('users', array('user_id'=>'999', 'follower_count'=>100));
+        $builders[] = FixtureBuilder::build('follows', array('user_id'=>'999', 'follower_id'=>42,
+            'active' => 1, 'last_seen'=>'-1d', 'network'=>'twitter'));
+
+        $builders[] = FixtureBuilder::build('users', array('user_id'=>'888', 'follower_count'=>3000));
+        $builders[] = FixtureBuilder::build('follows', array('user_id'=>'888', 'follower_id'=>42,
+            'active' => 1, 'last_seen'=>'-1d', 'network'=>'twitter'));
+
+        $user = new User(array('follower_count'=>2000, 'user_id' => 42, 'network' => 'twitter'));
+
+        $insight_dao = DAOFactory::getDAO('InsightDAO');
+        $insight_plugin = new FollowerComparisonInsight();
+        $insight_plugin->generateInsight($this->instance, $user, array(), 3);
+        $today = date ('Y-m-d');
+        $result = $insight_dao->getInsight($this->slug, $this->instance->id, $today);
+        $this->assertNotNull($result);
+        $this->assertEqual($result->headline, '@testy could lend a hand');
+        $this->assertEqual($result->text, '@testy has more followers than 50% of the 2 people @testy follows. That '
+            . "means <strong>1</strong> of @testy's friends would reach a bigger audience if @testy retweeted them.");
+
+        $this->debug($this->getRenderedInsightInHTML($result));
+        $this->debug($this->getRenderedInsightInEmail($result));
+    }
+
+    public function testAlternateHeadlines() {
+        $builders = array();
+        for ($i=0; $i<1000; $i++) {
+            $builders[] = FixtureBuilder::build('users', array('user_id'=>1000+$i, 'follower_count'=>10*$i));
+            $builders[] = FixtureBuilder::build('follows', array('user_id'=>1000+$i, 'follower_id'=>42,
+                'active' => 1, 'last_seen'=>'-1d', 'network'=>'twitter'));
+        }
+
+        $user = new User(array('follower_count'=>8000, 'user_id' => 42, 'network' => 'twitter'));
+
+        $insight_dao = DAOFactory::getDAO('InsightDAO');
+        $insight_plugin = new FollowerComparisonInsight();
+
+        for ($i=2; $i<=4; $i++) {
+            TimeHelper::setTime($i);
+            $insight_plugin->generateInsight($this->instance, $user, array(), 3);
+            $today = date ('Y-m-d');
+            $result = $insight_dao->getInsight($this->slug, $this->instance->id, $today);
+            $this->assertNotNull($result);
+            $this->debug($this->getRenderedInsightInHTML($result));
+            $this->debug($this->getRenderedInsightInEmail($result));
+
+            if ($i == 2) {
+                $this->assertEqual($result->headline, '@testy could lend a hand');
+                $this->assertEqual($result->text, '@testy has more followers than 80% of the 1,000 people @testy '
+                    . "follows. That means <strong>800</strong> of @testy's friends would reach a bigger audience if "
+                    . "@testy retweeted them.");
+            } else if ($i == 3) {
+                $this->assertEqual($result->headline, '@testy has it good');
+                $this->assertEqual($result->text, '@testy has more followers than 80% of the people @testy '
+                    . "follows. That means <strong>800</strong> of @testy's friends would reach a bigger audience if "
+                    . "@testy retweeted them.");
+            } else if ($i ==4 ) {
+                $this->assertEqual($result->headline, "A closer look at @testy's follower count");
+                $this->assertEqual($result->text, '@testy has more followers than 80% of the 1,000 people @testy '
+                    . "follows. That means <strong>800</strong> of @testy's friends would reach a bigger audience "
+                    . "if @testy retweeted them.");
+            }
+        }
+    }
+}
