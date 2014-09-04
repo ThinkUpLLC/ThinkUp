5f4c24796daa57ecd674708b2e91f436a7711165
Gina Trapani <ginatrapani@gmail.com>
2014-09-04 16:22:22 -0400

    Weekly bests: New-style headlines

--- webapp/plugins/insightsgenerator/tests/TestOfWeeklyBestsInsight.php
+++ webapp/plugins/insightsgenerator/tests/TestOfWeeklyBestsInsight.php
@@ -46,13 +46,14 @@ class TestOfWeeklyBestsInsight extends ThinkUpInsightUnitTestCase {
         parent::tearDown();
     }
 
-    public function testWeeklyBestsInsightForTwitter() {
+    public function testWeeklyBestsInsightForTwitterV1() {
         // Get data ready that insight requires
         $instance = new Instance();
         $instance->id = 10;
         $instance->network_username = 'testeriffic';
         $instance->network = 'twitter';
         $builders = self::setUpPublicInsight($instance);
+        TimeHelper::setTime(1);
 
         $posts = array();
         $posts[] = new Post(array(
@@ -99,18 +100,11 @@ class TestOfWeeklyBestsInsight extends ThinkUpInsightUnitTestCase {
         $this->debug(Utils::varDumpToString($result));
         $this->assertNotNull($result);
         $this->assertIsA($result, "Insight");
-        $this->assertPattern('/This was \@testeriffic\'s most popular tweet of last week./', $result->headline);
+        $this->assertPattern('/\@testeriffic\'s most popular tweet last week/', $result->headline);
         $this->assertPattern('/5 replies/', $result->text);
         $this->assertPattern('/1 retweet/', $result->text);
         $this->assertPattern('/3 favorites/', $result->text);
 
-        /**
-         * Use this code to output the individual insight's fully-rendered HTML to file.
-         * Then, open the file in your browser to view.
-         *
-         * $ TEST_DEBUG=1 php webapp/plugins/insightsgenerator/tests/TestOfHelloThinkUpInsight.php
-         * -t testHelloThinkUpInsight > webapp/insight.html
-         */
         $controller = new InsightStreamController();
         $_GET['u'] = 'testeriffic';
         $_GET['n'] = 'twitter';
@@ -121,13 +115,6 @@ class TestOfWeeklyBestsInsight extends ThinkUpInsightUnitTestCase {
         //$this->debug($results);
         $this->assertPattern('/This is a really good post/', $results);
 
-        /**
-         * Use this code to output the individual insight's fully-rendered email HTML to file.
-         * Then, open the file in your browser to view.
-         *
-         * $ TEST_DEBUG=1 php webapp/plugins/insightsgenerator/tests/TestOfHelloThinkUpInsight.php
-         * -t testHelloThinkUpInsight > webapp/insight_email.html
-         */
         $email_insight = $this->getRenderedInsightInEmail($result);
         //Uncomment this out to see the email view of insight
         $this->debug($email_insight);
@@ -135,7 +122,83 @@ class TestOfWeeklyBestsInsight extends ThinkUpInsightUnitTestCase {
         $this->assertPattern('/This is a really good post/', $email_insight);
     }
 
-    public function testMonthlyBestInsightForTwitter() {
+    public function testWeeklyBestsInsightForTwitterV2() {
+        // Get data ready that insight requires
+        $instance = new Instance();
+        $instance->id = 10;
+        $instance->network_username = 'testeriffic';
+        $instance->network = 'twitter';
+        $builders = self::setUpPublicInsight($instance);
+        TimeHelper::setTime(2);
+
+        $posts = array();
+        $posts[] = new Post(array(
+            'reply_count_cache' => 5,
+            'retweet_count_cache' => 1,
+            'favlike_count_cache' => 3,
+            'post_text' => 'This is a really good post',
+            'author_username' => $instance->network_username,
+            'author_user_id' => 'abc',
+            'author_avatar' => 'http://example.com/example.jpg',
+            'network' => $instance->network,
+            'pub_date' => date('Y-m-d H:i:s', strtotime('-1 day'))
+        )); // popularity_index = 34
+        $posts[] = new Post(array(
+            'reply_count_cache' => 0,
+            'retweet_count_cache' => 1,
+            'favlike_count_cache' => 15,
+            'post_text' => 'This is an even better post',
+            'author_username' => $instance->network_username,
+            'author_user_id' => 'abc',
+            'author_avatar' => 'http://example.com/example.jpg',
+            'network' => $instance->network,
+            'pub_date' => date('Y-m-d H:i:s', strtotime('-1 day'))
+        )); // popularity_index = 33
+        $posts[] = new Post(array(
+            'reply_count_cache' => 2,
+            'retweet_count_cache' => 5,
+            'favlike_count_cache' => 1,
+            'post_text' => 'This is THE BEST post',
+            'author_username' => $instance->network_username,
+            'author_user_id' => 'abc',
+            'author_avatar' => 'http://example.com/example.jpg',
+            'network' => $instance->network,
+            'pub_date' => date('Y-m-d H:i:s', strtotime('-1 day'))
+        )); // popularity_index = 27
+
+        $insight_plugin = new WeeklyBestsInsight();
+        $insight_plugin->generateInsight($instance, null, $posts, 3);
+
+        // Assert that insight got inserted
+        $insight_dao = new InsightMySQLDAO();
+        $today = date ('Y-m-d');
+        $result = $insight_dao->getInsight('weekly_best', 10, $today);
+        $this->debug(Utils::varDumpToString($result));
+        $this->assertNotNull($result);
+        $this->assertIsA($result, "Insight");
+        $this->assertPattern('/\@testeriffic\'s biggest tweet last week/', $result->headline);
+        $this->assertPattern('/5 replies/', $result->text);
+        $this->assertPattern('/1 retweet/', $result->text);
+        $this->assertPattern('/3 favorites/', $result->text);
+
+        $controller = new InsightStreamController();
+        $_GET['u'] = 'testeriffic';
+        $_GET['n'] = 'twitter';
+        $_GET['d'] = $today;
+        $_GET['s'] = 'weekly_best';
+        $results = $controller->go();
+        //Uncomment this out to see web view of insight
+        //$this->debug($results);
+        $this->assertPattern('/This is a really good post/', $results);
+
+        $email_insight = $this->getRenderedInsightInEmail($result);
+        //Uncomment this out to see the email view of insight
+        $this->debug($email_insight);
+        $this->debug($this->getRenderedInsightInHTML($result));
+        $this->assertPattern('/This is a really good post/', $email_insight);
+    }
+
+    public function testMonthlyBestInsightForTwitterV1() {
         // Get data ready that insight requires
         $instance = new Instance();
         $instance->id = 10;
@@ -193,13 +256,6 @@ class TestOfWeeklyBestsInsight extends ThinkUpInsightUnitTestCase {
         $this->assertPattern('/Behold, @testeriffic\'s most popular tweet of '.date('F', $last_month_time).
             ' '.date('Y', $last_month_time).'./', $result->text);
 
-        /**
-         * Use this code to output the individual insight's fully-rendered HTML to file.
-         * Then, open the file in your browser to view.
-         *
-         * $ TEST_DEBUG=1 php webapp/plugins/insightsgenerator/tests/TestOfHelloThinkUpInsight.php
-         * -t testHelloThinkUpInsight > webapp/insight.html
-         */
         $controller = new InsightStreamController();
         $_GET['u'] = 'testeriffic';
         $_GET['n'] = 'twitter';
@@ -210,13 +266,6 @@ class TestOfWeeklyBestsInsight extends ThinkUpInsightUnitTestCase {
         //$this->debug($results);
         $this->assertPattern('/This is a really good post/', $results);
 
-        /**
-         * Use this code to output the individual insight's fully-rendered email HTML to file.
-         * Then, open the file in your browser to view.
-         *
-         * $ TEST_DEBUG=1 php webapp/plugins/insightsgenerator/tests/TestOfHelloThinkUpInsight.php
-         * -t testHelloThinkUpInsight > webapp/insight_email.html
-         */
         $email_insight = $this->getRenderedInsightInEmail($result);
         //Uncomment this out to see the email view of insight
         $this->debug($email_insight);
@@ -241,13 +290,6 @@ class TestOfWeeklyBestsInsight extends ThinkUpInsightUnitTestCase {
         $this->assertPattern('/This was @testeriffic\'s most popular tweet of '.date('F', $last_month_time).
             ' '.date('Y', $last_month_time).'./', $result->text);
 
-        /**
-         * Use this code to output the individual insight's fully-rendered HTML to file.
-         * Then, open the file in your browser to view.
-         *
-         * $ TEST_DEBUG=1 php webapp/plugins/insightsgenerator/tests/TestOfHelloThinkUpInsight.php
-         * -t testHelloThinkUpInsight > webapp/insight.html
-         */
         $controller = new InsightStreamController();
         $_GET['u'] = 'testeriffic';
         $_GET['n'] = 'twitter';
@@ -258,13 +300,115 @@ class TestOfWeeklyBestsInsight extends ThinkUpInsightUnitTestCase {
         //$this->debug($results);
         $this->assertPattern('/This is a really good post/', $results);
 
-        /**
-         * Use this code to output the individual insight's fully-rendered email HTML to file.
-         * Then, open the file in your browser to view.
-         *
-         * $ TEST_DEBUG=1 php webapp/plugins/insightsgenerator/tests/TestOfHelloThinkUpInsight.php
-         * -t testHelloThinkUpInsight > webapp/insight_email.html
-         */
+        $email_insight = $this->getRenderedInsightInEmail($result);
+        //Uncomment this out to see the email view of insight
+        $this->debug($email_insight);
+        $this->debug($this->getRenderedInsightInHTML($result));
+        $this->assertPattern('/This is a really good post/', $email_insight);
+    }
+
+    public function testMonthlyBestInsightForTwitterV2() {
+        // Get data ready that insight requires
+        $instance = new Instance();
+        $instance->id = 10;
+        $instance->network_username = 'testeriffic';
+        $instance->network = 'twitter';
+        $builders = self::setUpPublicInsight($instance);
+
+        $builders[] = FixtureBuilder::build('posts', array(
+            'reply_count_cache' => 5,
+            'retweet_count_cache' => 1,
+            'favlike_count_cache' => 3,
+            'post_text' => 'This is a really good post',
+            'author_username' => $instance->network_username,
+            'author_user_id' => 'abc',
+            'author_avatar' => 'http://example.com/example.jpg',
+            'network' => $instance->network,
+            'pub_date' => '-1d'));
+
+        $builders[] = FixtureBuilder::build('posts', array(
+            'reply_count_cache' => 0,
+            'retweet_count_cache' => 1,
+            'favlike_count_cache' => 15,
+            'post_text' => 'This is an even better post',
+            'author_username' => $instance->network_username,
+            'author_user_id' => 'abc',
+            'author_avatar' => 'http://example.com/example.jpg',
+            'network' => $instance->network,
+            'pub_date' => '-1d'));
+
+        $builders[] = FixtureBuilder::build('posts', array(
+            'reply_count_cache' => 2,
+            'retweet_count_cache' => 5,
+            'favlike_count_cache' => 1,
+            'post_text' => 'This is THE BEST post',
+            'author_username' => $instance->network_username,
+            'author_user_id' => 'abc',
+            'author_avatar' => 'http://example.com/example.jpg',
+            'network' => $instance->network,
+            'pub_date' => '-1d'));
+
+        TimeHelper::setTime(2);
+        $insight_plugin = new WeeklyBestsInsight();
+        $insight_plugin->generateInsight($instance, null, array(), 3);
+
+        // Assert that insight got inserted
+        $insight_dao = new InsightMySQLDAO();
+        $today = date ('Y-m-d');
+        $result = $insight_dao->getInsight('monthly_best', 10, $today);
+        $this->debug(Utils::varDumpToString($result));
+        $this->assertNotNull($result);
+        $this->assertIsA($result, "Insight");
+        $this->assertPattern('/Happy '.date('F').'!/', $result->headline);
+
+        $last_month_time = strtotime('first day of last month');
+        $this->assertPattern('/This was @testeriffic\'s most popular tweet of '.date('F', $last_month_time).
+            ' '.date('Y', $last_month_time).'./', $result->text);
+
+        $controller = new InsightStreamController();
+        $_GET['u'] = 'testeriffic';
+        $_GET['n'] = 'twitter';
+        $_GET['d'] = $today;
+        $_GET['s'] = 'monthly_best';
+        $results = $controller->go();
+        //Uncomment this out to see web view of insight
+        //$this->debug($results);
+        $this->assertPattern('/This is a really good post/', $results);
+
+        $email_insight = $this->getRenderedInsightInEmail($result);
+        //Uncomment this out to see the email view of insight
+        $this->debug($email_insight);
+        $this->debug($this->getRenderedInsightInHTML($result));
+        $this->assertPattern('/This is a really good post/', $email_insight);
+
+        //Test alternate headline/body
+        TimeHelper::setTime(2);
+        $insight_plugin = new WeeklyBestsInsight();
+        $insight_plugin->generateInsight($instance, null, array(), 3);
+
+        // Assert that insight got inserted
+        $insight_dao = new InsightMySQLDAO();
+        $today = date ('Y-m-d');
+        $result = $insight_dao->getInsight('monthly_best', 10, $today);
+        $this->debug(Utils::varDumpToString($result));
+        $this->assertNotNull($result);
+        $this->assertIsA($result, "Insight");
+        $this->assertPattern('/Happy '.date('F').'!/', $result->headline);
+
+        $last_month_time = strtotime('first day of last month');
+        $this->assertPattern('/This was @testeriffic\'s most popular tweet of '.date('F', $last_month_time).
+            ' '.date('Y', $last_month_time).'./', $result->text);
+
+        $controller = new InsightStreamController();
+        $_GET['u'] = 'testeriffic';
+        $_GET['n'] = 'twitter';
+        $_GET['d'] = $today;
+        $_GET['s'] = 'monthly_best';
+        $results = $controller->go();
+        //Uncomment this out to see web view of insight
+        //$this->debug($results);
+        $this->assertPattern('/This is a really good post/', $results);
+
         $email_insight = $this->getRenderedInsightInEmail($result);
         //Uncomment this out to see the email view of insight
         $this->debug($email_insight);
@@ -279,6 +423,7 @@ class TestOfWeeklyBestsInsight extends ThinkUpInsightUnitTestCase {
         $instance->network_username = 'tester_fb';
         $instance->network = 'facebook';
         $builders = self::setUpPublicInsight($instance);
+        TimeHelper::setTime(1);
 
         $posts = array();
         $posts[] = new Post(array(
@@ -321,7 +466,7 @@ class TestOfWeeklyBestsInsight extends ThinkUpInsightUnitTestCase {
         $this->debug(Utils::varDumpToString($result));
         $this->assertNotNull($result);
         $this->assertIsA($result, "Insight");
-        $this->assertPattern('/This was tester_fb\'s most popular status update of last week/', $result->headline);
+        $this->assertPattern('/tester_fb\'s most popular status update last week/', $result->headline);
         $this->assertPattern('/8 comments/', $result->text);
         $this->assertPattern('/3 likes/', $result->text);
 
@@ -376,7 +521,8 @@ class TestOfWeeklyBestsInsight extends ThinkUpInsightUnitTestCase {
         $this->debug(Utils::varDumpToString($result));
         $this->assertNotNull($result);
         $this->assertIsA($result, "Insight");
-        $this->assertPattern('/testeriffic earned \<strong\>5 comments<\/strong\> and \<strong\>3 likes\<\/strong\>./', $result->text);
+        $this->assertPattern('/testeriffic earned \<strong\>5 comments<\/strong\> and \<strong\>3 likes\<\/strong\>./',
+            $result->text);
         $this->assertPattern('/5 comments/', $result->text);
         $this->assertPattern('/3 likes/', $result->text);
     }
@@ -401,6 +547,7 @@ class TestOfWeeklyBestsInsight extends ThinkUpInsightUnitTestCase {
             'network' => $instance->network,
             'pub_date' => date('Y-m-d H:i:s', strtotime('-1 day'))
         )); // popularity_index = 5
+        TimeHelper::setTime(1);
 
         $insight_plugin = new WeeklyBestsInsight();
         $insight_plugin->generateInsight($instance, null, $posts, 3);
@@ -412,7 +559,7 @@ class TestOfWeeklyBestsInsight extends ThinkUpInsightUnitTestCase {
         $this->debug(Utils::varDumpToString($result));
         $this->assertNotNull($result);
         $this->assertIsA($result, "Insight");
-        $this->assertPattern('/This was \@testeriffic\'s most popular tweet of last week/', $result->headline);
+        $this->assertPattern('/\@testeriffic\'s most popular tweet last week/', $result->headline);
         $this->assertPattern('/1 reply/', $result->text);
         $this->assertNoPattern('/and/', $result->text);
 
@@ -450,6 +597,7 @@ class TestOfWeeklyBestsInsight extends ThinkUpInsightUnitTestCase {
             'network' => $instance->network,
             'pub_date' => date('Y-m-d H:i:s', strtotime('-1 day'))
         )); // popularity_index = 6
+        TimeHelper::setTime(1);
 
         $insight_plugin = new WeeklyBestsInsight();
         $insight_plugin->generateInsight($instance, null, $posts, 3);
@@ -461,7 +609,7 @@ class TestOfWeeklyBestsInsight extends ThinkUpInsightUnitTestCase {
         $this->debug(Utils::varDumpToString($result));
         $this->assertNotNull($result);
         $this->assertIsA($result, "Insight");
-        $this->assertPattern('/This was \@testeriffic\'s most popular tweet of last week/', $result->headline);
+        $this->assertPattern('/\@testeriffic\'s most popular tweet last week/', $result->headline);
         $this->assertPattern('/3 favorites/', $result->text);
         $this->assertNoPattern('/reply/', $result->text);
         $this->assertNoPattern('/retweet/', $result->text);
@@ -501,6 +649,7 @@ class TestOfWeeklyBestsInsight extends ThinkUpInsightUnitTestCase {
             'network' => $instance->network,
             'pub_date' => date('Y-m-d H:i:s', strtotime('-1 day'))
         )); // popularity_index = 30
+        TimeHelper::setTime(1);
 
         $insight_plugin = new WeeklyBestsInsight();
         $insight_plugin->generateInsight($instance, null, $posts, 3);
@@ -512,7 +661,7 @@ class TestOfWeeklyBestsInsight extends ThinkUpInsightUnitTestCase {
         $this->debug(Utils::varDumpToString($result));
         $this->assertNotNull($result);
         $this->assertIsA($result, "Insight");
-        $this->assertPattern('/This was \@testeriffic\'s most popular tweet of last week/', $result->headline);
+        $this->assertPattern('/\@testeriffic\'s most popular tweet last week/', $result->headline);
         $this->assertPattern('/4 replies/', $result->text);
         $this->assertPattern('/5 favorites/', $result->text);
         $this->assertPattern('/and/', $result->text);
