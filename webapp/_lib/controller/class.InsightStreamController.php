853a87b23db82f320932fa7cd1c635b907b7c372
Gina Trapani <ginatrapani@gmail.com>
2014-09-17 23:21:19 -0400

    SSL-enable insight sharing image URL

--- webapp/_lib/controller/class.InsightStreamController.php
+++ webapp/_lib/controller/class.InsightStreamController.php
@@ -138,9 +138,10 @@ class InsightStreamController extends ThinkUpController {
                         'u'=>$insight->instance->network_username,
                         'n'=>$insight->instance->network,
                         'd'=>date('Y-m-d', strtotime($insight->date)),
-                        's'=>$insight->slug
+                        's'=>$insight->slug,
+                        'share'=>1
                         );
-                    $insight_image = "http://shares.thinkup.com/insight?".(http_build_query($insight_params));
+                    $insight_image = "https://shares.thinkup.com/insight?".(http_build_query($insight_params));
                     $twitter_card = "summary_large_image";
                 } else {
                     $insight_image = "https://www.thinkup.com/join/assets/ico/apple-touch-icon-144-precomposed.png";
