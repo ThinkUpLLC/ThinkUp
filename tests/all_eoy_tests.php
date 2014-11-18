bc2a6cdae6bd1cf2c8fca2474dbf30ed5d01bdce
Adam Pash <adam.pash@gmail.com>
2014-11-18 14:20:38 -0500

    Issue #2128: EOY Biggest Fans insight

--- tests/all_eoy_tests.php
+++ tests/all_eoy_tests.php
@@ -36,6 +36,7 @@ $eoy_insight_tests = new TestSuite('EOY insight tests');
 
 //EOY insight tests
 $eoy_insight_tests->add(new TestOfEOYMostFavlikedPostInsight());
+$eoy_insight_tests->add(new TestOfEOYBiggestFansInsight());
 
 $version = explode('.', PHP_VERSION); //dont run redis test for php less than 5.3
 
