211b700b7e2844eca865ac9789c3251617733c14
Anil Dash <anil@dashes.com>
2015-05-04 11:17:43 -0400

    New insight: Star Wars Day

---
+++ webapp/plugins/insightsgenerator/insights/maythefourth.php
@@ -0,0 +1,123 @@
+<?php
+/*
+ Plugin Name: May The Fourth Be With You
+ Description: Did you talk about Star Wars?
+ When: May 4, 2015
+ */
+/**
+ *
+ * ThinkUp/webapp/plugins/insightsgenerator/insights/maythefourth.php
+ *
+ * Copyright (c) 2015 Anil Dash
+ *
+ * LICENSE:
+ *
+ * This file is part of ThinkUp (http://thinkup.com).
+ *
+ * ThinkUp is free software: you can redistribute it and/or modify it under the terms of the GNU General Public
+ * License as published by the Free Software Foundation, either version 2 of the License, or (at your option) any
+ * later version.
+ *
+ * ThinkUp is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied
+ * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License along with ThinkUp.  If not, see
+ * <http://www.gnu.org/licenses/>.
+ *
+ * Copyright (c) 2014-2015 Chris Moyer, Anil Dash
+ *
+ * @author Chris Moyer chris@inarow.net, Anil Dash anil@thinkup.com
+ * @license http://www.gnu.org/licenses/gpl.html
+ * @copyright 2014-2015 Chris Moyer, Anil Dash
+ */
+
+class StarWarsInsight extends InsightPluginParent implements InsightPlugin {
+    /**
+     * Slug for this insight
+     **/
+    var $slug = 'starwars';
+    /**
+     * Date to run this insight
+     */
+    var $run_date = '2015-05-04';
+
+    public function generateInsight(Instance $instance, User $user, $last_week_of_posts, $number_days) {
+        parent::generateInsight($instance, $user, $last_week_of_posts, $number_days);
+        $this->logger->logInfo("Begin generating insight", __METHOD__.','.__LINE__);
+
+        if (Utils::isTest() || date("Y-m-d") == $this->run_date) {
+            $this->logger->logInfo("Should generate insight", __METHOD__.','.__LINE__);
+
+            $hero_image = array(
+                'url' => 'https://www.thinkup.com/assets/images/insights/2015-05/starwars.jpg',
+                'alt_text' => 'RockTrooper',
+                'credit' => 'Photo: JD Hancock',
+                'img_link' => 'https://www.flickr.com/photos/jdhancock/4932301604'
+            );
+
+            $post_dao = DAOFactory::getDAO('PostDAO');
+            $last_year_of_posts = $post_dao->getPostsByUserInRange(
+                $author_id = $instance->network_user_id,
+                $network = $instance->network,
+                $from=date('Y-m-d', strtotime('-1 year')),
+                $until = date('Y-m-d'),
+                $order_by='pub_date', $direction='DESC', $iterator=true, $is_public=false
+            );
+
+            $topics = array(
+                'force' => array("star wars","force awakens", "bb-8", "darth vader", "bb8", "StarWars",
+                    "StarWarsDay"),
+            );
+
+            $matches = array();
+            $matched_posts = array();
+            $matched = false;
+            //print_r($last_year_of_posts);
+            foreach ($last_year_of_posts as $post) {
+                foreach ($topics as $key => $strings) {
+                    foreach ($strings as $string) {
+                        if (preg_match_all('/\b'.strtolower($string).'\b/i', strtolower($post->post_text),
+                            $matches)) {
+                            $matched = true;
+                        }
+                        //DEBUG
+                        // else {
+                        //     $this->logger->logInfo("Didn't find ".$string." in ".$post->post_text,
+                        //         __METHOD__.','.__LINE__);
+                        // }
+                    }
+                }
+                if ($matched) {
+                    $this->logger->logInfo("Matched post ".$post->post_text, __METHOD__.','.__LINE__);
+                    $matched_posts[] = $post;
+                }
+                $matched = false;
+            }
+
+            if (count($matched_posts) > 0) {
+                $headline = "The Force is strong with " . $this->username;
+                $insight_text = $this->username
+                    . " was ready for Star Wars Day. May the fourth be with you... always.";
+
+                $insight = new Insight();
+                $insight->instance_id = $instance->id;
+                $insight->slug = $this->slug;
+                $insight->date = $this->run_date;
+                $insight->headline = $headline;
+                $insight->text = $insight_text;
+                $insight->filename = basename(__FILE__, ".php");
+                $insight->emphasis = Insight::EMPHASIS_HIGH;
+                $insight->setHeroImage($hero_image);
+
+                $matched_posts_sliced = array_slice($matched_posts, 0, 20);
+                $insight->setPosts($matched_posts_sliced);
+                $this->insight_dao->insertInsight($insight);
+            }
+        }
+        $this->logger->logInfo("Done generating insight", __METHOD__.','.__LINE__);
+    }
+}
+
+$insights_plugin_registrar = PluginRegistrarInsights::getInstance();
+$insights_plugin_registrar->registerInsightPlugin('StarWarsInsight');
---
+++ webapp/plugins/insightsgenerator/tests/TestOfMayTheFourth.php
@@ -0,0 +1,130 @@
+<?php
+/**
+ *
+ * ThinkUp/webapp/plugins/insightsgenerator/tests/TestOfStarWarsInsight.php
+ *
+ * Copyright (c) 2012-2015 Gina Trapani
+ *
+ * LICENSE:
+ *
+ * This file is part of ThinkUp (http://thinkup.com).
+ *
+ * ThinkUp is free software: you can redistribute it and/or modify it under the terms of the GNU General Public
+ * License as published by the Free Software Foundation, either version 2 of the License, or (at your option) any
+ * later version.
+ *
+ * ThinkUp is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied
+ * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License along with ThinkUp.  If not, see
+ * <http://www.gnu.org/licenses/>.
+ *
+ * Test for the StarWarsInsight class.
+ *
+ * Copyright (c) 2014-2015 Chris Moyer
+ *
+ * @author Chris Moyer chris@inarow.net
+ * @license http://www.gnu.org/licenses/gpl.html
+ * @copyright 2014-2015 Adam Pash
+ */
+
+require_once dirname(__FILE__) . '/../../../../tests/init.tests.php';
+require_once THINKUP_WEBAPP_PATH.'_lib/extlib/simpletest/autorun.php';
+require_once THINKUP_WEBAPP_PATH.'_lib/extlib/simpletest/web_tester.php';
+require_once THINKUP_ROOT_PATH. 'webapp/plugins/insightsgenerator/model/class.InsightPluginParent.php';
+require_once THINKUP_ROOT_PATH. 'webapp/plugins/insightsgenerator/insights/maythefourth.php';
+
+class TestOfStarWarsInsight extends ThinkUpInsightUnitTestCase {
+
+    public function setUp(){
+        parent::setUp();
+        $instance = new Instance();
+        $instance->id = 10;
+        $instance->network_username = 'Luke';
+        $instance->network_user_id = '18';
+        $instance->network = 'twitter';
+        $this->instance = $instance;
+    }
+
+    public function tearDown() {
+        parent::tearDown();
+    }
+
+    public function testNoStarWars() {
+        $insight_plugin = new StarWarsInsight();
+        $insight_dao = new InsightMySQLDAO();
+        $insight_plugin->generateInsight($this->instance, new User(), array(), 3);
+        $result = $insight_dao->getInsight($insight_plugin->slug, $this->instance->id, date('Y-m-d'));
+        $this->assertNull($result);
+    }
+
+    public function testYesStarWars() {
+        $builders = array();
+        $builders[] = FixtureBuilder::build('posts',
+            array(
+                'pub_date' => '2015-05-04', 'post_id' => 1, 'author_username' => $this->instance->network_username,
+                'author_user_id' => $this->instance->network_user_id, 'network' => $this->instance->network,
+                'post_text' => "I can't believe BB-8 is real!",
+            )
+        );
+        $builders[] = FixtureBuilder::build('posts',
+            array(
+                'pub_date' => '2015-01-07', 'post_id' => 2, 'author_username' => $this->instance->network_username,
+                'author_user_id' => $this->instance->network_user_id, 'network' => $this->instance->network,
+                'post_text' => "omg #StarWars trailer!",
+            )
+        );
+        $builders[] = FixtureBuilder::build('posts',
+            array(
+                'pub_date' => '2015-03-07', 'post_id' => 3, 'author_username' => $this->instance->network_username,
+                'author_user_id' => $this->instance->network_user_id, 'network' => $this->instance->network,
+                'post_text' => "What's up with Darth these days?",
+            )
+        );
+        $builders[] = FixtureBuilder::build('posts',
+            array(
+                'pub_date' => '2014-05-04', 'post_id' => 4, 'author_username' => $this->instance->network_username,
+                'author_user_id' => $this->instance->network_user_id, 'network' => $this->instance->network,
+                'post_text' => "Happy #StarWarsDay yo",
+            )
+        );
+        $builders[] = FixtureBuilder::build('posts',
+            array(
+                'pub_date' => '2014-12-05', 'post_id' => 5, 'author_username' => $this->instance->network_username,
+                'author_user_id' => $this->instance->network_user_id, 'network' => $this->instance->network,
+                'post_text' => "Poor Darth Vader",
+            )
+        );
+        $insight_plugin = new StarWarsInsight();
+        $insight_dao = new InsightMySQLDAO();
+        $insight_plugin->generateInsight($this->instance, new User(), null, 3);
+        $result = $insight_dao->getInsight($insight_plugin->slug, $this->instance->id, $insight_plugin->run_date);
+        $this->assertNotNull($result);
+
+        $this->assertEqual($result->headline, "The Force is strong with @Luke");
+        $this->assertEqual($result->text, "@Luke was ready for Star Wars Day. May the fourth be with you... always.");
+
+        $this->dumpRenderedInsight($result, $this->instance, "Got Star Wars");
+    }
+
+    public function testWordInWord() {
+        $year = date('Y');
+        $builders = array();
+        $builders[] = FixtureBuilder::build('posts',
+            array(
+                'pub_date' => $year.'-02-26', 'post_id' => 1, 'author_username' => $this->instance->network_username,
+                'author_user_id' => $this->instance->network_user_id, 'network' => $this->instance->network,
+                'post_text' => 'Radarthoughts is not a real word.',
+            )
+        );
+        $post->author_username = $this->instance->network_username;
+        $post->author_user_id = $this->instance->network_user_id;
+
+        $insight_plugin = new StarWarsInsight();
+        $insight_dao = new InsightMySQLDAO();
+        $insight_plugin->generateInsight($this->instance, new User(), array($post), 3);
+        $result = $insight_dao->getInsight($insight_plugin->slug, $this->instance->id, date('Y-m-d'));
+        $this->assertNull($result);
+    }
+}
