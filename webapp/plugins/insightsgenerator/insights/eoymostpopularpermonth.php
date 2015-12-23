abd86f088d70df083d537668775f918b72b0de41
Gina Trapani <ginatrapani@gmail.com>
2015-12-23 17:22:11 -0500

    EOY Most Popular per Month: Add Instagram copy

--- webapp/plugins/insightsgenerator/insights/eoymostpopularpermonth.php
+++ webapp/plugins/insightsgenerator/insights/eoymostpopularpermonth.php
@@ -110,6 +110,17 @@ class EOYMostPopularPerMonthInsight extends InsightPluginParent implements Insig
                 return;
             }
 
+            //Populate Instagram photos
+            if ($instance->network == 'instagram') {
+                $photo_dao = DAOFactory::getDAO('PhotoDAO');
+                $popular_photos = array();
+                foreach ($top_posts as $key => $post) {
+                    $post = $photo_dao->getPhoto($post->post_id, 'instagram');
+                    $popular_photos[] = $post;
+                }
+                $top_posts = $popular_photos;
+            }
+
             $copy = array(
                 'twitter' => array(
                     'normal' => array(
@@ -123,6 +134,13 @@ class EOYMostPopularPerMonthInsight extends InsightPluginParent implements Insig
                         'headline' => "%username's biggest posts of each month in %year",
                         'body' => "This year's about to enter the history books. For better or for worse, these were ".
                             "%username's most popular status updates of each month of %qualified_year"
+                    ),
+                ),
+                'instagram' => array(
+                    'normal' => array(
+                        'headline' => "%username's biggest Instagram posts of each month in %year",
+                        'body' => "The calendar is about to flip to a new year. Before it does, check out ".
+                            "%username's most popular Instagram photos and videos of each month of %qualified_year"
                     ),
                 )
             );
